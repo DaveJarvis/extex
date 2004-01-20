6016aa7e96510061f2e281e54024c0cdd4830689
Gerd Neugebauer <gene@gerd-neugebauer.de>
2004-01-20 09:25:56 +0000

    ExpandableCode extracted

--- ExTeX/src/java/de/dante/extex/interpreter/Code.java
+++ ExTeX/src/java/de/dante/extex/interpreter/Code.java
@@ -80,28 +80,6 @@ public interface Code {
             TokenSource source, Typesetter typesetter) throws GeneralException;
 
     /**
-     * This method takes the first token and expands it. The result is placed
-     * on the stack. To expand a token it might be necessary to consume further
-     * tokens. In this case <code>true</code> is returned.
-     * <p>
-     * This means that expandable code does one step of expansion, puts the
-     * result on the stack, and returns <code>true</code>.
-     * Non-expandable code simply return <code>false</code>.
-     * </p>
-     *
-     * @param prefix the prefix flags controlling the expansion
-     * @param context the interpreter context
-     * @param source the token source
-     * @param typesetter the typesetter
-     *
-     * @return <code>true</code> iff the token could be expanded
-     *
-     * @throws GeneralException in case of an error
-     */
-    boolean expand(Flags prefix, Context context,
-            TokenSource source, Typesetter typesetter) throws GeneralException;
-
-    /**
      * ...
      *
      * @param context the interpreter context
