1b45acc502bcc4f770d3c12048233a092956b765
Gerd Neugebauer <gene@gerd-neugebauer.de>
2004-02-10 09:22:09 +0000

    *** empty log message ***

--- ExTeX/src/java/de/dante/extex/ExTeX.java
+++ ExTeX/src/java/de/dante/extex/ExTeX.java
@@ -479,7 +479,7 @@ public class ExTeX {
             initializeStreams(interpreter);
 
             Typesetter typesetter = new TypesetterFactory(config
-                .getConfiguration("Typesetter")).newInstance();
+                .getConfiguration("Typesetter")).newInstance(interpreter.getContext());
 
             DocumentWriter docWriter = new DocumentWriterFactory(config
                 .getConfiguration("DocumentWriter")).newInstance();
@@ -504,15 +504,19 @@ public class ExTeX {
                 : pages == 1 ? "ExTeX.Page" : "ExTeX.Pages"), outname, Integer
                 .toString(pages)));
         } catch (ConfigurationException e) {
+        	e.printStackTrace(); // TODO delete after test
             logger.throwing(this.getClass().getName(), "run", e);
             throw new MainConfigurationException(e);
         } catch (CharacterCodingException e) {
+			e.printStackTrace(); // TODO delete after test
             logger.throwing(this.getClass().getName(), "run", e);
             throw new MainCodingException(e);
         } catch (IOException e) {
+			e.printStackTrace(); // TODO delete after test
             logger.throwing(this.getClass().getName(), "run", e);
             throw new MainIOException(e);
         } catch (GeneralException e) {
+			e.printStackTrace(); // TODO delete after test
             logger.throwing(this.getClass().getName(), "run", e);
             throw new MainException(e);
         }
