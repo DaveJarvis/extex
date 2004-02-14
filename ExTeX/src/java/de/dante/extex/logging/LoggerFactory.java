fe0db5897260b84076dee70d69417afd78ca44b1
Gerd Neugebauer <gene@gerd-neugebauer.de>
2004-02-14 14:21:57 +0000

    *** empty log message ***

--- ExTeX/src/java/de/dante/extex/logging/LoggerFactory.java
+++ ExTeX/src/java/de/dante/extex/logging/LoggerFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004  Gerd Neugebauer
+ * Copyright (C) 2004 Gerd Neugebauer
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
