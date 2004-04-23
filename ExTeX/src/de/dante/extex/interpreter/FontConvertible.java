64c98a7da794adcf589dae2a85e45e718c1a5984
Gerd Neugebauer <gene@gerd-neugebauer.de>
2004-04-23 05:24:20 +0000

    FontConvertable renamed to FontConvertible

--- ExTeX/src/de/dante/extex/interpreter/FontConvertible.java
+++ ExTeX/src/de/dante/extex/interpreter/FontConvertible.java
@@ -29,7 +29,7 @@ import de.dante.util.GeneralException;
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
  * @version $Revision$
  */
-public interface FontConvertable {
+public interface FontConvertible {
 
     /**
      * ...
