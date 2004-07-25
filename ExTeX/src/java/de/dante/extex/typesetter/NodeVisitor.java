c84fc5d4278432d5245927872b58315e2e11079f
Gerd Neugebauer <gene@gerd-neugebauer.de>
2004-07-25 19:03:55 +0000

    typo corrected

--- ExTeX/src/java/de/dante/extex/typesetter/NodeVisitor.java
+++ ExTeX/src/java/de/dante/extex/typesetter/NodeVisitor.java
@@ -138,7 +138,7 @@ import de.dante.util.GeneralException;
  * with the method <tt>visit()</tt> of the Node. The signature allows us to
  * provide two additional arguments and receive a return value. Since we want
  * to do something with the node itself, we use the first free argument for the
- * node and <code>null</codeXS> as the second argument.
+ * node and <code>null</code> as the second argument.
  * </p>
  * <p>
  * In the <tt>visit</tt> methods we can now savely assume that the node is of
