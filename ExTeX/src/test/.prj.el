afe8e77938fccde2d946d62a13874a131289b93b
Sebastian Waschik <sebastian.waschik@gmx.de>
2004-08-20 21:44:23 +0000

    - added JDEE-project files for test (does not run class ExTeX as default)

---
+++ ExTeX/src/test/.prj.el
@@ -0,0 +1,9 @@
+; author: Sebastian Waschik
+; created: 2004-07-26
+; RCS-ID: $Id$
+(jde-project-file-version "1.0")
+(jde-set-variables
+ '(jde-run-application-class "")
+ '(jde-run-working-directory "../..")
+ '(jde-run-read-app-args nil)
+)
