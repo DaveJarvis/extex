87ecb6181d5d87fe5611659a2d220327dbfbea1d
Gerd Neugebauer <gene@gerd-neugebauer.de>
2004-08-25 07:05:49 +0000

    TokenStream correctly fed with options

--- ExTeX/src/test/de/dante/extex/interpreter/Max1.java
+++ ExTeX/src/test/de/dante/extex/interpreter/Max1.java
@@ -283,7 +283,7 @@ public class Max1 extends TestCase {
         Interpreter interpreter = new InterpreterFactory(config
                 .getConfiguration("Interpreter")).newInstance();
         TokenStreamFactory factory = new TokenStreamFactory(config
-                .getConfiguration("Reader"), null);
+                .getConfiguration("Reader"), "base");
         interpreter.setTokenStreamFactory(factory);
 
         TestTypesetter typesetter = new TestTypesetter();
