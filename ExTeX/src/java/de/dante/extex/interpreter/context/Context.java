155cb660be08aafc570bbf7fbf757b64181291ba
Gerd Neugebauer <gene@gerd-neugebauer.de>
2004-12-03 16:44:22 +0000

    expand() got new parameter

--- ExTeX/src/java/de/dante/extex/interpreter/context/Context.java
+++ ExTeX/src/java/de/dante/extex/interpreter/context/Context.java
@@ -89,14 +89,14 @@ public interface Context extends Tokenizer, Serializable {
 
     /**
      * Expand some tokens.
-     *
      * @param tokens the tokens to expand
+     * @param typesetter TODO
      *
      * @return ...
      *
      * @throws GeneralException in case of an error
      */
-    Tokens expand(Tokens tokens) throws GeneralException;
+    Tokens expand(Tokens tokens, Typesetter typesetter) throws GeneralException;
 
     /**
      * Getter for the afterassignment token.
