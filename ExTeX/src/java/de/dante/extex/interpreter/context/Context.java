4b3c2dd819359ce643dc1ad44edfae5c83c16468
Gerd Neugebauer <gene@gerd-neugebauer.de>
2004-12-13 10:59:09 +0000

    methods esc() added

--- ExTeX/src/java/de/dante/extex/interpreter/context/Context.java
+++ ExTeX/src/java/de/dante/extex/interpreter/context/Context.java
@@ -88,6 +88,20 @@ public interface Context extends Tokenizer, Serializable {
             throws GeneralException;
 
     /**
+     * Attach the current escape character in front of a name and return the
+     * result.
+     * <p>
+     * This method is meant to produce a printable version of a control
+     * sequence for error messages.
+     * </p>
+     *
+     * @param name the name of the macro
+     *
+     * @return the control sequence including the escape character
+     */
+    String esc(String name);
+
+    /**
      * Expand some tokens.
      *
      * @param tokens the tokens to expand
