10dc592547c8d26402f74e8c81d78410e63e3fa9
Gerd Neugebauer <gene@gerd-neugebauer.de>
2005-01-04 09:55:14 +0000

    imports reorganized

--- ExTeX/src/test/de/dante/extex/interpreter/primitives/font/FontnameTest.java
+++ ExTeX/src/test/de/dante/extex/interpreter/primitives/font/FontnameTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004 The ExTeX Group and individual authors listed below
+ * Copyright (C) 2004-2005 The ExTeX Group and individual authors listed below
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
@@ -18,8 +18,6 @@
  */
 
 package de.dante.extex.interpreter.primitives.font;
-
-import java.util.Properties;
 
 import de.dante.test.ExTeXLauncher;
 
