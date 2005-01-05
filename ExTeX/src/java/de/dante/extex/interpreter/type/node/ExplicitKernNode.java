861c4953b148c19e9ab16690088c59b9fc3baa78
Gerd Neugebauer <gene@gerd-neugebauer.de>
2005-01-05 18:13:55 +0000

    documenation improved

--- ExTeX/src/java/de/dante/extex/interpreter/type/node/ExplicitKernNode.java
+++ ExTeX/src/java/de/dante/extex/interpreter/type/node/ExplicitKernNode.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2003-2004 The ExTeX Group and individual authors listed below
+ * Copyright (C) 2003-2005 The ExTeX Group and individual authors listed below
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
@@ -23,7 +23,7 @@ import de.dante.extex.interpreter.type.dimen.Dimen;
 import de.dante.extex.typesetter.Discartable;
 
 /**
- * TODO gene: missing JavaDoc.
+ * This class reresents an explicit kern node for the typesetter.
  *
  * From The TeXbook
  * <p>
