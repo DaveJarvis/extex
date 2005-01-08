17270a891de6a882a16eed81ce64b296f1eda8d2
Gerd Neugebauer <gene@gerd-neugebauer.de>
2005-01-08 16:31:56 +0000

    documentation improved

--- ExTeX/src/java/de/dante/extex/typesetter/paragraphBuilder/ParagraphBuilder.java
+++ ExTeX/src/java/de/dante/extex/typesetter/paragraphBuilder/ParagraphBuilder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004 The ExTeX Group and individual authors listed below
+ * Copyright (C) 2004-2005 The ExTeX Group and individual authors listed below
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
@@ -34,6 +34,7 @@ public interface ParagraphBuilder {
 
     /**
      * Break a horizontal list into lines.
+     * The horizontal list passed in might be modified under way.
      *
      * @param nodes the horizontal node list containing all nodes for the
      *   paragraph
