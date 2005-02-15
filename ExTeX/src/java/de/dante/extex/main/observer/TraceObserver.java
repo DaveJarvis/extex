5881cf1b38367814e1ecc4fd028e58783b59352a
Gerd Neugebauer <gene@gerd-neugebauer.de>
2005-02-15 09:49:16 +0000

    reformatted

--- ExTeX/src/java/de/dante/extex/main/observer/TraceObserver.java
+++ ExTeX/src/java/de/dante/extex/main/observer/TraceObserver.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004 The ExTeX Group and individual authors listed below
+ * Copyright (C) 2004-2005 The ExTeX Group and individual authors listed below
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -25,7 +25,8 @@ import de.dante.util.observer.Observable;
 import de.dante.util.observer.Observer;
 
 /**
- * TODO gene: missing JavaDoc.
+ * Observer for tracing macros. The macro is written to the log file preceeded
+ * by a <tt>&gt;</tt> mark and a space character.
  *
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
  * @version $Revision$
