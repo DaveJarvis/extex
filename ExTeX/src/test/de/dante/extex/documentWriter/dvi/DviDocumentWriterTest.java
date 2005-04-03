d275426837fd8b720956db085ee6deeb36ce70c7
Gerd Neugebauer <gene@gerd-neugebauer.de>
2005-04-03 16:29:41 +0000

    PanicException deleted since not used any more

--- ExTeX/src/test/de/dante/extex/documentWriter/dvi/DviDocumentWriterTest.java
+++ ExTeX/src/test/de/dante/extex/documentWriter/dvi/DviDocumentWriterTest.java
@@ -28,7 +28,6 @@ import de.dante.extex.documentWriter.DocumentWriter;
 import de.dante.extex.documentWriter.DocumentWriterOptions;
 import de.dante.extex.documentWriter.NoOutputStreamException;
 import de.dante.extex.documentWriter.SingleDocumentStream;
-import de.dante.extex.i18n.PanicException;
 import de.dante.extex.interpreter.context.Context;
 import de.dante.extex.interpreter.exception.InterpreterException;
 import de.dante.extex.interpreter.type.count.FixedCount;
