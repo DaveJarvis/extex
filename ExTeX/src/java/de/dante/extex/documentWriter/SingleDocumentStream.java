bad721eda6463f3bd62163d8c4ec89b86b33bb46
Gerd Neugebauer <gene@gerd-neugebauer.de>
2005-04-22 11:31:50 +0000

    documentation improved

--- ExTeX/src/java/de/dante/extex/documentWriter/SingleDocumentStream.java
+++ ExTeX/src/java/de/dante/extex/documentWriter/SingleDocumentStream.java
@@ -25,7 +25,7 @@ import java.io.OutputStream;
  * This interface describes the ability of a DocumentWriter to produce a single
  * output document.
  *
- * @see 
+ * @see de.dante.extex.documentWriter.MultipleDocumentStream
  *
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
  * @version $Revision$
