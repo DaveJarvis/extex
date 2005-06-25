6f5f0f90a2902774c91742a2d3c4c8a8c6525ac8
Gerd Neugebauer <gene@gerd-neugebauer.de>
2005-06-25 20:48:36 +0000

    MAX_VALUE moved in

--- ExTeX/src/java/de/dante/extex/interpreter/context/Color.java
+++ ExTeX/src/java/de/dante/extex/interpreter/context/Color.java
@@ -16,6 +16,7 @@
  * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  *
  */
+
 package de.dante.extex.interpreter.context;
 
 import java.io.Serializable;
@@ -28,6 +29,12 @@ import java.io.Serializable;
  * @version $Revision$
  */
 public interface Color extends Serializable {
+
+    /**
+     * The constant <tt>MAX_VALUE</tt> contains the maximal values for all
+     * channels.
+     */
+    int MAX_VALUE = 0xffff;
 
     /**
      * Getter for the alpha channel.
