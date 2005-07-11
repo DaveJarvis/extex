8f8fb709b21de996d4e8a4b3a5b37fb871f9cc19
Gerd Neugebauer <gene@gerd-neugebauer.de>
2005-07-11 05:38:06 +0000

    FixedHelpingException moved to package util

--- ExTeX/src/java/de/dante/extex/interpreter/primitives/info/Errmessage.java
+++ ExTeX/src/java/de/dante/extex/interpreter/primitives/info/Errmessage.java
@@ -23,6 +23,7 @@ import de.dante.extex.interpreter.Flags;
 import de.dante.extex.interpreter.TokenSource;
 import de.dante.extex.interpreter.context.Context;
 import de.dante.extex.interpreter.exception.InterpreterException;
+import de.dante.extex.interpreter.primitives.info.util.FixedHelpingException;
 import de.dante.extex.interpreter.type.AbstractCode;
 import de.dante.extex.typesetter.Typesetter;
 
