766ec4a18e1b6191035ddb97f8157cbc808fc969
Gerd Neugebauer <gene@gerd-neugebauer.de>
2005-08-16 08:49:38 +0000

    TODO removed

--- ExTeX/src/java/de/dante/extex/color/BasicColorConverter.java
+++ ExTeX/src/java/de/dante/extex/color/BasicColorConverter.java
@@ -32,7 +32,6 @@ import de.dante.util.exception.GeneralException;
  * This implementation of a color converter is based on the formulas in the
  * color space FAQ.
  *
- * TODO gene: use the new ColorVisitor
  *
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
  * @version $Revision$
