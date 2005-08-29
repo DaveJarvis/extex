f00f9cffb2652876fdedac92c588ac59184b36ae
Gerd Neugebauer <gene@gerd-neugebauer.de>
2005-08-29 07:11:09 +0000

    typo corrected

--- ExTeX/src/java/de/dante/util/framework/logger/LogEnabled.java
+++ ExTeX/src/java/de/dante/util/framework/logger/LogEnabled.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004 The ExTeX Group and individual authors listed below
+ * Copyright (C) 2004-2005 The ExTeX Group and individual authors listed below
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
@@ -23,7 +23,7 @@ import java.util.logging.Logger;
 
 /**
  * This interface describes the need of a component for a logger.
- * The logger is provided by the framework at startup. Alternatively some other
+ * The logger is provided by the framework at start-up. Alternatively some other
  * means might be present to provide the logger before the component comes to
  * action.
  *
