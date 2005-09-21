79873712177fe23df5c0bc33db26b7e91826b938
Gerd Neugebauer <gene@gerd-neugebauer.de>
2005-09-21 07:28:17 +0000

    reformatted

--- ExTeX/src/java/de/dante/extex/scanner/type/token/ActiveCharacterToken.java
+++ ExTeX/src/java/de/dante/extex/scanner/type/token/ActiveCharacterToken.java
@@ -146,7 +146,8 @@ public class ActiveCharacterToken extends AbstractToken implements CodeToken {
      */
     public String toString() {
 
-        return getLocalizer().format("ActiveCharacterToken.Text", getName());
+        return getLocalizer().format("ActiveCharacterToken.Text",
+                getChar().toString());
     }
 
     /**
@@ -158,7 +159,8 @@ public class ActiveCharacterToken extends AbstractToken implements CodeToken {
      */
     public void toString(final StringBuffer sb) {
 
-        sb.append(getLocalizer().format("ActiveCharacterToken.Text",
+        sb
+                .append(getLocalizer().format("ActiveCharacterToken.Text",
                         getName()));
     }
 
