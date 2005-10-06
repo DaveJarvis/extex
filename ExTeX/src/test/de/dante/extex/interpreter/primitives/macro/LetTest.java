afab1cea482a7cd2e7e88406153f6425194b1f05
Gerd Neugebauer <gene@gerd-neugebauer.de>
2005-10-06 10:54:16 +0000

    test result fixed

--- ExTeX/src/test/de/dante/extex/interpreter/primitives/macro/LetTest.java
+++ ExTeX/src/test/de/dante/extex/interpreter/primitives/macro/LetTest.java
@@ -123,7 +123,7 @@ public class LetTest extends NoFlagsButGlobalPrimitiveTester {
                 + "\\let\\a\\a"
                 + "\\a",
                 //--- err message ---
-                "Undefined control sequence");
+                "Undefined control sequence the control sequence \\a");
     }
 
     /**
