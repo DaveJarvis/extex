f38776a412227f8d8a0ebc17b2b9de27dd945b9d
Gerd Neugebauer <gene@gerd-neugebauer.de>
2005-11-06 17:13:54 +0000

    imports reorganized

--- ExTeX/src/java/de/dante/extex/interpreter/Interpreter.java
+++ ExTeX/src/java/de/dante/extex/interpreter/Interpreter.java
@@ -24,6 +24,7 @@ import java.io.InputStream;
 
 import de.dante.extex.font.FontFactory;
 import de.dante.extex.interpreter.context.Context;
+import de.dante.extex.interpreter.context.observer.count.CountObserver;
 import de.dante.extex.interpreter.exception.ErrorLimitException;
 import de.dante.extex.interpreter.exception.InterpreterException;
 import de.dante.extex.interpreter.interaction.Interaction;
