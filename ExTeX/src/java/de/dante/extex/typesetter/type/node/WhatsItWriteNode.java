5b80146abcfbf9c7e608155351331bddd0a96c27
Gerd Neugebauer <gene@gerd-neugebauer.de>
2005-11-20 17:55:00 +0000

    typo fixed

--- ExTeX/src/java/de/dante/extex/typesetter/type/node/WhatsItWriteNode.java
+++ ExTeX/src/java/de/dante/extex/typesetter/type/node/WhatsItWriteNode.java
@@ -80,7 +80,7 @@ public class WhatsItWriteNode extends WhatsItNode {
 
     /**
      * This method performs any actions which are required to be executed at the
-     * time of shipping the node to the DocumentWriter. In the case od this node
+     * time of shipping the node to the DocumentWriter. In the case of this node
      * this means that the tokens are expanded and written to an output writer.
      *
      * @param context the interpreter context
