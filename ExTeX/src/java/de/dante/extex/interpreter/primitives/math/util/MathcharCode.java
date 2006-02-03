83e962d2f1cd6a3970ab927ddef4a0adac52daec
Gerd Neugebauer <gene@gerd-neugebauer.de>
2006-02-03 15:06:49 +0000

    providing the TypesettingContext to the Noad

--- ExTeX/src/java/de/dante/extex/interpreter/primitives/math/util/MathcharCode.java
+++ ExTeX/src/java/de/dante/extex/interpreter/primitives/math/util/MathcharCode.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2003-2005 The ExTeX Group and individual authors listed below
+ * Copyright (C) 2003-2006 The ExTeX Group and individual authors listed below
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
@@ -16,6 +16,7 @@
  * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  *
  */
+
 package de.dante.extex.interpreter.primitives.math.util;
 
 import de.dante.extex.interpreter.Flags;
@@ -71,7 +72,7 @@ public class MathcharCode extends Mathchar implements CountConvertible {
             throws InterpreterException {
 
         NoadConsumer nc = getListMaker(context, typesetter);
-        insert(nc, mathchar.getValue());
+        insert(nc, mathchar.getValue(), context.getTypesettingContext());
     }
 
     /**
