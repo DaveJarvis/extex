8cf9c83cad4c2775e46888bcdf67f054484ff7dd
Gerd Neugebauer <gene@gerd-neugebauer.de>
2006-02-03 13:47:17 +0000

    scanTokens() can be controlled finer for error reporting

--- ExTeX/src/java/de/dante/extex/interpreter/primitives/dynamic/EnsureLoaded.java
+++ ExTeX/src/java/de/dante/extex/interpreter/primitives/dynamic/EnsureLoaded.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2005 The ExTeX Group and individual authors listed below
+ * Copyright (C) 2005-2006 The ExTeX Group and individual authors listed below
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
@@ -119,7 +119,7 @@ public class EnsureLoaded extends AbstractCode implements LogEnabled {
             final TokenSource source, final Typesetter typesetter)
             throws InterpreterException {
 
-        Tokens tokens = source.scanTokens(context, getName());
+        Tokens tokens = source.scanTokens(context, false, false, getName());
 
         if (tokens == null) {
             throw new EofException(getName());
