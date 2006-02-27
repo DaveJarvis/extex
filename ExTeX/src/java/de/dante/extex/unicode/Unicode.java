e28062d61e8abba50c3f045922bc22a15c8e10f1
Gerd Neugebauer <gene@gerd-neugebauer.de>
2006-02-27 10:48:00 +0000

    Unicode control characters added

--- ExTeX/src/java/de/dante/extex/unicode/Unicode.java
+++ ExTeX/src/java/de/dante/extex/unicode/Unicode.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2005 The ExTeX Group and individual authors listed below
+ * Copyright (C) 2005-2006 The ExTeX Group and individual authors listed below
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
@@ -16,8 +16,10 @@
  * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  *
  */
+
 package de.dante.extex.unicode;
 
+import de.dante.util.UnicodeChar;
 
 /**
  * Central utility class for Unicode compatibility in <logo>ExTeX</logo>.
@@ -28,12 +30,24 @@ package de.dante.extex.unicode;
 public final class Unicode {
 
     /**
-     * Creates a new object.
+     * The field <tt>BREAK_PERMITTED_HERE</tt> contains the ...
      */
-    private Unicode() {
+    public static final UnicodeChar BREAK_PERMITTED_HERE = new UnicodeChar(0x82);
 
-        super();
-    }
+    /**
+     * The field <tt>NEXT_LINE</tt> contains the next line control caracter.
+     */
+    public static final UnicodeChar NEXT_LINE = new UnicodeChar(0x85);
+
+    /**
+     * The field <tt>NO_BREAK_HERE</tt> contains the ...
+     */
+    public static final UnicodeChar NO_BREAK_HERE = new UnicodeChar(0x83);
+
+    /**
+     * The field <tt>NO_BREAK_SPACE</tt> contains the non-breakable space.
+     */
+    public static final UnicodeChar NO_BREAK_SPACE = new UnicodeChar(0xa0);
 
     /**
      * The constant <tt>OFFSET</tt> contains the offset for shifting code points
@@ -41,5 +55,23 @@ public final class Unicode {
      */
     public static final int OFFSET = 0xEE00;
     //public static final int OFFSET = 0;
+
+    /**
+     * The constant <tt>SHY</tt> contains the soft hyphenation character.
+     */
+    public static final UnicodeChar SHY = new UnicodeChar(0xad);
+
+    /**
+     * The field <tt>SPACE</tt> contains the space control character.
+     */
+    public static final UnicodeChar SPACE = new UnicodeChar(0x20);
+
+    /**
+     * Creates a new object.
+     */
+    private Unicode() {
+
+        super();
+    }
 
 }
