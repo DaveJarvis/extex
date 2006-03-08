bd4aa1605888348c5fd750739e95618adfe8399e
Gerd Neugebauer <gene@gerd-neugebauer.de>
2006-03-08 10:41:44 +0000

    documentation improved

--- ExTeX/src/java/de/dante/extex/interpreter/context/ModifiableTypesettingContext.java
+++ ExTeX/src/java/de/dante/extex/interpreter/context/ModifiableTypesettingContext.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2003-2005 The ExTeX Group and individual authors listed below
+ * Copyright (C) 2003-2006 The ExTeX Group and individual authors listed below
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
@@ -32,6 +32,8 @@ public interface ModifiableTypesettingContext extends TypesettingContext {
 
     /**
      * Setter for all components.
+     * The components color, direction, font, language, etc are copied from
+     * the instance given.
      *
      * @param context the context to clone
      */
