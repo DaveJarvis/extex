7ba486b2d76c88df366860fcd24193c64711f36f
Gerd Neugebauer <gene@gerd-neugebauer.de>
2006-04-09 14:38:10 +0000

    using variant of createToken() to save one method

--- ExTeX/src/test/de/dante/extex/interpreter/primitives/file/DumpTest.java
+++ ExTeX/src/test/de/dante/extex/interpreter/primitives/file/DumpTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2005 The ExTeX Group and individual authors listed below
+ * Copyright (C) 2005-2006 The ExTeX Group and individual authors listed below
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
@@ -92,7 +92,8 @@ public class DumpTest extends NoFlagsPrimitiveTester {
         assertEquals(1000, context.getMagnification());
         assertEquals(123, context.getCount("1").getValue());
         Code code = context.getCode((CodeToken) new TokenFactoryImpl()
-                .createToken(Catcode.ESCAPE, "x", Namespace.DEFAULT_NAMESPACE));
+                .createToken(Catcode.ESCAPE, null, "x",
+                        Namespace.DEFAULT_NAMESPACE));
         assertNotNull(code);
         fmt.delete();
     }
