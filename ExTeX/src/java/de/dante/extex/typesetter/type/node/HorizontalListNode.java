524d5e3aa0592d1a9b9e5fdcc95e3d4ba1c63f5e
Gerd Neugebauer <gene@gerd-neugebauer.de>
2006-04-09 14:41:19 +0000

    new constructor added

--- ExTeX/src/java/de/dante/extex/typesetter/type/node/HorizontalListNode.java
+++ ExTeX/src/java/de/dante/extex/typesetter/type/node/HorizontalListNode.java
@@ -23,7 +23,6 @@ import de.dante.extex.interpreter.context.Context;
 import de.dante.extex.interpreter.type.dimen.FixedDimen;
 import de.dante.extex.interpreter.type.glue.FixedGlue;
 import de.dante.extex.interpreter.type.glue.FixedGlueComponent;
-import de.dante.extex.interpreter.type.glue.GlueComponent;
 import de.dante.extex.interpreter.type.glue.WideGlue;
 import de.dante.extex.typesetter.Typesetter;
 import de.dante.extex.typesetter.type.Node;
@@ -55,6 +54,17 @@ public class HorizontalListNode extends AbstractNodeList {
     public HorizontalListNode() {
 
         super();
+    }
+
+    /**
+     * Creates a new object. The list is filled with the node given.
+     *
+     * @param width the width of the box
+     */
+    public HorizontalListNode(final FixedDimen width) {
+
+        super();
+        setWidth(width);
     }
 
     /**
