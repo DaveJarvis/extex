cf927214a1e52faae2d6a3a27e915ccbabad7e76
Gerd Neugebauer <gene@gerd-neugebauer.de>
2006-05-12 15:43:09 +0000

    getTokens() signals an eof with an exception instead of a null value

--- ExTeX/src/java/de/dante/extex/interpreter/primitives/string/Lowercase.java
+++ ExTeX/src/java/de/dante/extex/interpreter/primitives/string/Lowercase.java
@@ -24,6 +24,7 @@ import de.dante.extex.interpreter.TokenSource;
 import de.dante.extex.interpreter.context.Context;
 import de.dante.extex.interpreter.exception.InterpreterException;
 import de.dante.extex.interpreter.exception.helping.EofException;
+import de.dante.extex.interpreter.exception.helping.EofInToksException;
 import de.dante.extex.interpreter.type.AbstractCode;
 import de.dante.extex.interpreter.type.ExpandableCode;
 import de.dante.extex.interpreter.type.tokens.Tokens;
@@ -103,11 +104,15 @@ public class Lowercase extends AbstractCode implements ExpandableCode {
             final TokenSource source, final Typesetter typesetter)
             throws InterpreterException {
 
-        Tokens toks = source.getTokens(context, source, typesetter);
-
-        if (toks == null) {
+        Tokens toks;
+        try {
+            toks = source.getTokens(context, source, typesetter);
+        } catch (EofInToksException e) {
+            throw new EofInToksException(printableControlSequence(context));
+        } catch (EofException e) {
             throw new EofException(printableControlSequence(context));
         }
+
         String namespace = context.getNamespace();
         Token[] result = new Token[toks.length()];
         TokenFactory factory = context.getTokenFactory();
