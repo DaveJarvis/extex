72fb3998c783926a3517828adcc9e3ec7e01760f
Gerd Neugebauer <gene@gerd-neugebauer.de>
2006-06-18 22:43:21 +0000

    \textfont1 defined

--- ExTeX/src/test/de/dante/extex/interpreter/primitives/math/AbstractMathTester.java
+++ ExTeX/src/test/de/dante/extex/interpreter/primitives/math/AbstractMathTester.java
@@ -41,6 +41,7 @@ public class AbstractMathTester extends NoFlagsPrimitiveTester {
         + "\\scriptfont3=\\f"
         + "\\scriptscriptfont3=\\f"
         + "\\font\\f cmmi10 \\textfont1=\\f "
+        + "\\textfont0=\\f " // additionally for testing
         + "\\font\\f cmmi7 \\scriptfont1=\\f "
         + "\\font\\f cmmi5 \\scriptscriptfont1=\\f ";
 
