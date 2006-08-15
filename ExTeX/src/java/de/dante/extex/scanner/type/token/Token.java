6413bf77b0e4870ded5702567e49e0d1fc5fdf07
Gerd Neugebauer <gene@gerd-neugebauer.de>
2006-08-15 06:59:15 +0000

    typos fixed

--- ExTeX/src/java/de/dante/extex/scanner/type/token/Token.java
+++ ExTeX/src/java/de/dante/extex/scanner/type/token/Token.java
@@ -39,7 +39,7 @@ public interface Token {
 
     /**
      * Compare the current token with a pair of catcode and character value.
-     * This pair constitutes a vitual token. They are the same if the catcode
+     * This pair constitutes a virtual token. They are the same if the catcode
      * and the value are the same.
      *
      * @param cc the catcode
@@ -51,7 +51,7 @@ public interface Token {
 
     /**
      * Compare the current token with a pair of catcode and String value. This
-     * pair constitutes a vitual token. They are the same if the catcode and
+     * pair constitutes a virtual token. They are the same if the catcode and
      * the value are the same.
      *
      * @param cc the catcode
