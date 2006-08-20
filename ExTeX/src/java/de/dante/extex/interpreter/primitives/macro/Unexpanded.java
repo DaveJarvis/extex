ab5282dfae0b7d4fe5a37cfd34ef3a322eff732d
Gerd Neugebauer <gene@gerd-neugebauer.de>
2006-08-20 15:34:10 +0000

    imports simplified

--- ExTeX/src/java/de/dante/extex/interpreter/primitives/macro/Unexpanded.java
+++ ExTeX/src/java/de/dante/extex/interpreter/primitives/macro/Unexpanded.java
@@ -23,7 +23,6 @@ import de.dante.extex.interpreter.Flags;
 import de.dante.extex.interpreter.TokenSource;
 import de.dante.extex.interpreter.context.Context;
 import de.dante.extex.interpreter.exception.InterpreterException;
-import de.dante.extex.interpreter.exception.helping.HelpingException;
 import de.dante.extex.interpreter.type.AbstractCode;
 import de.dante.extex.interpreter.type.ExpandableCode;
 import de.dante.extex.typesetter.Typesetter;
