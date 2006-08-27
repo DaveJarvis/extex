4c5783f2b803fff42dc8fb3ea46f9aa0a5752031
Gerd Neugebauer <gene@gerd-neugebauer.de>
2006-08-27 21:17:19 +0000

    deprecated method removed

--- ExTeX/src/java/de/dante/extex/interpreter/context/impl/extension/ContextExtensionImpl.java
+++ ExTeX/src/java/de/dante/extex/interpreter/context/impl/extension/ContextExtensionImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004 The ExTeX Group and individual authors listed below
+ * Copyright (C) 2004-2006 The ExTeX Group and individual authors listed below
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
@@ -48,6 +48,11 @@ public class ContextExtensionImpl extends ContextImpl
             Serializable {
 
     /**
+     * The field <tt>serialVersionUID</tt> contains the ...
+     */
+    private static final long serialVersionUID = 2006L;
+
+    /**
      * Creates a new object.
      * @param   config  the configuration
      * @throws ConfigurationException ...
@@ -56,7 +61,8 @@ public class ContextExtensionImpl extends ContextImpl
     public ContextExtensionImpl(final Configuration config)
             throws ConfigurationException, GeneralException {
 
-        super(config);
+        super();
+        configure(config);
         if (!(getGroup() instanceof GroupExtension)) {
             throw new InterpreterExtensionException();
         }
