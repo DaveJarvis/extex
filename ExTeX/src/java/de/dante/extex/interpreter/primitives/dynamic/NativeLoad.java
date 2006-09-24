fb16daaa861c9cf0868c5394ee83da5180ac8e21
Gerd Neugebauer <gene@gerd-neugebauer.de>
2006-09-24 17:33:32 +0000

    logger made transient

--- ExTeX/src/java/de/dante/extex/interpreter/primitives/dynamic/NativeLoad.java
+++ ExTeX/src/java/de/dante/extex/interpreter/primitives/dynamic/NativeLoad.java
@@ -154,12 +154,12 @@ public class NativeLoad extends AbstractCode
     /**
      * The constant <tt>serialVersionUID</tt> contains the id for serialization.
      */
-    protected static final long serialVersionUID = 2005L;
+    protected static final long serialVersionUID = 2006L;
 
     /**
      * The field <tt>logger</tt> contains the logger to use.
      */
-    private Logger logger = null;
+    private transient Logger logger = null;
 
     /**
      * The field <tt>map</tt> contains the mapping from a symbolic name to a
