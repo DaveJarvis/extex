3e24e000e9a698ffa7f26baa462e310f04e1d2f7
Gerd Neugebauer <gene@gerd-neugebauer.de>
2006-10-24 09:26:02 +0000

    exceptions marked as processed

--- ExTeX/src/java/de/dante/extex/interpreter/max/Max.java
+++ ExTeX/src/java/de/dante/extex/interpreter/max/Max.java
@@ -595,6 +595,7 @@ public abstract class Max
 
                 handleException(token, context, (InterpreterException) x,
                         typesetter);
+                e.setProcessed(true);
 
             } catch (RuntimeException e) {
                 if (observersError != null) {
@@ -632,6 +633,7 @@ public abstract class Max
         } catch (InterpreterException e) {
 
             handleException(token, theContext, e, theTypesetter);
+            e.setProcessed(true);
 
         } catch (RuntimeException e) {
             if (observersError != null) {
@@ -710,16 +712,15 @@ public abstract class Max
         }
 
         try {
-            for (;;) {
-                Token t = stream.get(null, null);
-                if (t == null) {
-                    return result;
-                }
+            for (Token t = stream.get(null, null); t != null; t = stream.get(
+                    null, null)) {
+
                 t = (Token) t.visit(tv, stream);
                 if (t != null) {
                     result.add(t);
                 }
             }
+            return result;
         } catch (InterpreterException e) {
             throw e;
         } catch (Exception e) {
