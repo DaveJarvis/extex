43c913f74dcaf7e65f4145047efece3c94a246ee
Gerd Neugebauer <gene@gerd-neugebauer.de>
2006-10-24 10:15:42 +0000

    hsize fixed

--- ExTeX/src/test/de/dante/extex/interpreter/primitives/register/muskip/AbstractMuskipRegisterTester.java
+++ ExTeX/src/test/de/dante/extex/interpreter/primitives/register/muskip/AbstractMuskipRegisterTester.java
@@ -50,7 +50,7 @@ public abstract class AbstractMuskipRegisterTester extends ExTeXLauncher {
      * The field <tt>prepare</tt> contains the the preparation code inserted
      * before each test.
      */
-    private String prepare = "\\hsize=200pt";
+    private String prepare = "\\hsize=3000pt";
 
     /**
      * Creates a new object.
