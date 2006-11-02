25426d452e8770fbb5c7eabda998e16b4f55cbe6
Gerd Neugebauer <gene@gerd-neugebauer.de>
2006-11-02 20:06:37 +0000

    documentation improved

--- ExTeX/src/java/de/dante/extex/interpreter/primitives/conditional/Or.java
+++ ExTeX/src/java/de/dante/extex/interpreter/primitives/conditional/Or.java
@@ -39,6 +39,12 @@ import de.dante.extex.typesetter.Typesetter;
  *  error is raised.
  * </p>
  *
+ * <h4>Syntax</h4>
+ *  The formal description of this primitive is the following:
+ *  <pre class="syntax">
+ *    &lang;or&rang;
+ *      &rarr; <tt>\or</tt>  </pre>
+ *
  * <h4>Examples</h4>
  *  <pre class="TeXSample">
  *    \iffalse abc \or def \fi  </pre>
