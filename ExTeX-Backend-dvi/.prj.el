082d03f4ecdc19402bd81fbf8a36332309bc5cbb
Gerd Neugebauer <gene@gerd-neugebauer.de>
2006-12-21 10:37:20 +0000

    a try to provide a configuration for Emacs JDE

---
+++ ExTeX-Backend-dvi/.prj.el
@@ -0,0 +1,62 @@
+; author: Sebastian Waschik
+; created: 2004-07-26
+; RCS-ID: $Id: .prj.el 3722 2006-08-08 08:13:17Z plaicy $
+(jde-project-file-version "1.0")
+(jde-set-variables
+ '(jde-project-name "ExTeX-Backend-dvi")
+
+; allow paths to be relative to project file
+ '(jde-resolve-relative-paths-p t)
+ '(jde-sourcepath
+   (quote
+    ("./src/test"
+     "./src/java")))
+ '(jde-global-classpath
+   (quote
+    ("./target/classes"
+    "./lib"
+    "../ExTeX-base/target/classes"
+    "../ExTeX-base/lib"
+    "../ExTeX-core/target/classes"
+    "../ExTeX-core/lib"
+    "../ExTeX-Font/target/classes"
+    "../ExTeX-Font/lib")))
+
+ ;; ant
+ '(jde-ant-use-global-classpath t)
+ '(jde-ant-working-directory "./")
+
+ ;; checkstyle
+ '(jde-checkstyle-classpath
+   (quote
+    ("../develop/lib/checkstyle-all-3.4.jar"
+     "../develop/lib/checkstyle-optional-3.4.jar")))
+ '(jde-checkstyle-style "./.checkstyle.cfg")
+
+ ;; compile
+ '(jde-build-function (quote (jde-ant-build)))
+ '(jde-compile-option-deprecation t)
+ '(jde-compile-option-directory "./target/classes")
+ '(jde-compile-option-source (quote ("1.4")))
+ '(jde-compile-option-target (quote ("1.4")))
+ '(jde-built-class-path
+   (quote
+    ("./target/classes")))
+
+ ;; debug
+ '(jde-db-read-app-args t)
+
+ ;; run
+ '(jde-run-application-class "de.dante.extex.main.TeX")
+ '(jde-run-working-directory ".")
+ '(jde-run-read-app-args t)
+
+ ;; style
+ '(require-final-newline nil)
+ '(jde-import-auto-sort t)
+ '(jde-gen-k&r t)
+
+ ;; xrefdb
+ ;'(jde-xref-db-base-directory "./.jde")
+ '(jde-xref-store-prefixes (quote ("de.dante")))
+)
