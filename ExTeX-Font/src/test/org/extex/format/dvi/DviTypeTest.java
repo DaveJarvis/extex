11249e576b469a61e551286cf5ed44b7f88b47c0
Gerd Neugebauer <gene@gerd-neugebauer.de>
2007-01-08 10:45:51 +0000

    dvi moved to test

--- ExTeX-Font/src/test/org/extex/format/dvi/DviTypeTest.java
+++ ExTeX-Font/src/test/org/extex/format/dvi/DviTypeTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2005-2006 The ExTeX Group and individual authors listed below
+ * Copyright (C) 2005-2007 The ExTeX Group and individual authors listed below
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
@@ -22,6 +22,7 @@ package org.extex.format.dvi;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
@@ -36,7 +37,9 @@ import java.util.TreeMap;
 import junit.framework.TestCase;
 
 import org.extex.font.FontFactory;
+import org.extex.font.exception.FontException;
 import org.extex.format.dvi.DviType;
+import org.extex.format.dvi.exception.DviException;
 import org.extex.util.file.random.RandomAccessInputFile;
 import org.extex.util.framework.configuration.Configuration;
 import org.extex.util.framework.configuration.ConfigurationFactory;
@@ -54,7 +57,7 @@ import org.extex.util.resource.ResourceFinderFactory;
  * <p>
  * needs -Xms64m -Xmx127m
  * </p>
- *
+ * 
  * @author <a href="mailto:m.g.n@gmx.de">Michael Niedermair</a>
  * @version $Revision$
  */
@@ -62,73 +65,49 @@ import org.extex.util.resource.ResourceFinderFactory;
 public class DviTypeTest extends TestCase {
 
     /**
-     * path.
-     */
-    private static final String PATH = "../ExTeX-Font/src/dvi/";
-
-    /**
      * files.
      */
     private static final String[] FILES = {"lettrine", "listings", "andre"};
 
     /**
-     * @see junit.framework.TestCase#setUp()
+     * path.
      */
-    protected void setUp() throws Exception {
+    private static final String PATH = "../ExTeX-Font/src/test/dvi/";
 
-        for (int i = 0; i < FILES.length; i++) {
+    /**
+     * test DviXml.
+     * 
+     * @param args the commandline
+     */
+    public static void main(final String[] args) {
 
-            String file = PATH + FILES[i] + ".dvi";
-            RandomAccessInputFile rar = new RandomAccessInputFile(file);
-
-            PrintWriter writer = new PrintWriter(new BufferedOutputStream(
-                    new FileOutputStream(PATH + FILES[i] + ".tmp")));
-            DviType dvitype = new DviType(writer, makeFontFactory());
-
-            dvitype.interpret(rar);
-            rar.close();
-            writer.close();
-        }
+        junit.textui.TestRunner.run(DviTypeTest.class);
     }
 
     /**
-     * test the dviXml interpreter.
-     * @throws IOException if a IO-error occurs
+     * finder.
      */
-    public void testlettrine() throws IOException {
-
-        checkFile(FILES[0]);
-    }
+    private ResourceFinder finder;
 
     /**
-     * test the dviXml interpreter.
-     * @throws IOException if a IO-error occurs
+     * The field <tt>props</tt> contains the merged properties from the system
+     * properties and the properties loaded from <tt>.extex-test</tt>.
      */
-    public void testlistings() throws IOException {
-
-        checkFile(FILES[1]);
-    }
-
-    /**
-     * test the dviXml interpreter.
-     * @throws IOException if a IO-error occurs
-     */
-    public void testandre() throws IOException {
-
-        checkFile(FILES[2]);
-    }
+    private Properties props = null;
 
     /**
      * Check the file.
-     * @param file  the file to test
+     * 
+     * @param file the file to test
      * @throws IOException if an io-error occurs
      */
     private void checkFile(final String file) throws IOException {
 
-        LineNumberReader inorg = new LineNumberReader(new FileReader(PATH
-                + file + ".dvitype"));
+        LineNumberReader inorg = new LineNumberReader(new FileReader(
+                PATH + file + ".dvitype"));
         LineNumberReader innew = new LineNumberReader(new FileReader(PATH
-                + file + ".tmp"));
+                                                                     + file
+                                                                     + ".tmp"));
 
         Map maporg = new TreeMap();
         Map mapnew = new TreeMap();
@@ -151,7 +130,33 @@ public class DviTypeTest extends TestCase {
     }
 
     /**
+     * Getter for props.
+     * 
+     * @return the props
+     */
+    private Properties getProps() {
+
+        if (props == null) {
+            props = System.getProperties();
+
+            File file = new File(".extex-test");
+            if (file.canRead()) {
+                try {
+                    FileInputStream inputStream = new FileInputStream(file);
+                    props.load(inputStream);
+                    inputStream.close();
+                } catch (Exception e) {
+                    // ignored on purpose
+                    e.printStackTrace();
+                }
+            }
+        }
+        return (Properties) this.props.clone();
+    }
+
+    /**
      * remove the vv- or hh-value.
+     * 
      * @param s the String
      * @return Returns the String witout vv.. / hh..
      */
@@ -168,44 +173,10 @@ public class DviTypeTest extends TestCase {
         return s;
     }
 
-    // --------------------------------------
-    // --------------------------------------
-    // --------------------------------------
-
-    /**
-     * Read the file.
-     * @param in    the input
-     * @param map   the map
-     * @throws IOException if an io-error occurs
-     */
-    private void readFile(final LineNumberReader in, final Map map)
-            throws IOException {
-
-        // read all line with start with a number "111:..."
-        String line;
-        while ((line = in.readLine()) != null) {
-            if (line.matches("^[0-9]*:.*")) {
-                int pos = line.indexOf(":");
-                Integer key = new Integer(line.substring(0, pos));
-                map.put(key, line.trim());
-            }
-        }
-    }
-
-    /**
-     * The field <tt>props</tt> contains the merged properties from the
-     * system properties and the properties loaded from <tt>.extex-test</tt>.
-     */
-    private Properties props = null;
-
-    /**
-     * finder.
-     */
-    private ResourceFinder finder;
-
     /**
      * make a font factroy.
-     * @return  Return the fontfactory
+     * 
+     * @return Return the fontfactory
      * @throws Exception if an error occurs.
      */
 
@@ -225,15 +196,19 @@ public class DviTypeTest extends TestCase {
         return fontFactory;
     }
 
+    // --------------------------------------
+    // --------------------------------------
+    // --------------------------------------
+
     /**
      * Create a new font factory.
-     *
+     * 
      * @param config the configuration object for the font factory
-     *
+     * 
      * @return the new font factory
-     *
+     * 
      * @throws ConfigurationException in case that some kind of problems have
-     * been detected in the configuration
+     *             been detected in the configuration
      */
     protected FontFactory makeFontFactory(final Configuration config)
             throws ConfigurationException {
@@ -245,19 +220,19 @@ public class DviTypeTest extends TestCase {
             throw new ConfigurationMissingAttributeException("class", config);
         }
 
-        //        ResourceFinder fontFinder = (new ResourceFinderFactory())
-        //                .createResourceFinder(config.getConfiguration("Resource"),
-        //                        null, getProps());
-        //        if (Boolean.valueOf(getProps().getProperty("extex.trace.font.files"))
-        //                .booleanValue()) {
-        //            fontFinder.enableTracing(true);
-        //        }
+        // ResourceFinder fontFinder = (new ResourceFinderFactory())
+        // .createResourceFinder(config.getConfiguration("Resource"),
+        // null, getProps());
+        // if (Boolean.valueOf(getProps().getProperty("extex.trace.font.files"))
+        // .booleanValue()) {
+        // fontFinder.enableTracing(true);
+        // }
 
         try {
             fontFactory = (FontFactory) (Class.forName(fontClass)
                     .getConstructor(
-                            new Class[]{Configuration.class,
-                                    ResourceFinder.class})
+                                    new Class[]{Configuration.class,
+                                            ResourceFinder.class})
                     .newInstance(new Object[]{config, finder}));
         } catch (IllegalArgumentException e) {
             throw new ConfigurationInstantiationException(e);
@@ -283,36 +258,83 @@ public class DviTypeTest extends TestCase {
     }
 
     /**
-     * Getter for props.
-     *
-     * @return the props
+     * Read the file.
+     * 
+     * @param in the input
+     * @param map the map
+     * @throws IOException if an io-error occurs
      */
-    private Properties getProps() {
+    private void readFile(final LineNumberReader in, final Map map)
+            throws IOException {
 
-        if (props == null) {
-            props = System.getProperties();
-
-            File file = new File(".extex-test");
-            if (file.canRead()) {
-                try {
-                    FileInputStream inputStream = new FileInputStream(file);
-                    props.load(inputStream);
-                    inputStream.close();
-                } catch (Exception e) {
-                    // ignored on purpose
-                    e.printStackTrace();
-                }
+        // read all line with start with a number "111:..."
+        String line;
+        while ((line = in.readLine()) != null) {
+            if (line.matches("^[0-9]*:.*")) {
+                int pos = line.indexOf(":");
+                Integer key = new Integer(line.substring(0, pos));
+                map.put(key, line.trim());
             }
         }
-        return (Properties) this.props.clone();
     }
 
     /**
-     * test DviXml.
-     * @param args  the commandline
+     * ...
+     * 
+     * @param name
+     * @throws IOException
+     * @throws FileNotFoundException
+     * @throws Exception
+     * @throws DviException
+     * @throws FontException
+     * @throws ConfigurationException
      */
-    public static void main(final String[] args) {
+    private void run(final String name) throws IOException,
+            FileNotFoundException, Exception, DviException, FontException,
+            ConfigurationException {
+        String file = PATH + name + ".dvi";
+        RandomAccessInputFile rar = new RandomAccessInputFile(file);
 
-        junit.textui.TestRunner.run(DviTypeTest.class);
+        PrintWriter writer = new PrintWriter(new BufferedOutputStream(
+                new FileOutputStream(PATH + name + ".tmp")));
+        DviType dvitype = new DviType(writer, makeFontFactory());
+
+        dvitype.interpret(rar);
+        rar.close();
+        writer.close();
     }
+
+    /**
+     * test the dviXml interpreter.
+     * 
+     * @throws IOException if a IO-error occurs
+     */
+    public void testandre() throws Exception {
+
+        run(FILES[2]);
+        checkFile(FILES[2]);
+    }
+
+    /**
+     * test the dviXml interpreter.
+     * 
+     * @throws IOException if a IO-error occurs
+     */
+    public void testlettrine() throws Exception {
+
+        run(FILES[0]);
+        checkFile(FILES[0]);
+    }
+
+    /**
+     * test the dviXml interpreter.
+     * 
+     * @throws IOException if a IO-error occurs
+     */
+    public void testlistings() throws Exception {
+
+        run(FILES[1]);
+        checkFile(FILES[1]);
+    }
+
 }
