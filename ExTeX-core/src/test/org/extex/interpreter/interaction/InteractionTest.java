740a467d25a760fb7aa2563c5300c19ba6ed8578
Gerd Neugebauer <gene@gerd-neugebauer.de>
2007-01-22 09:07:08 +0000

    documentation improved

--- ExTeX-core/src/test/org/extex/interpreter/interaction/InteractionTest.java
+++ ExTeX-core/src/test/org/extex/interpreter/interaction/InteractionTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2006 The ExTeX Group and individual authors listed below
+ * Copyright (C) 2006-2007 The ExTeX Group and individual authors listed below
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
@@ -24,7 +24,7 @@ import org.extex.util.exception.GeneralException;
 import junit.framework.TestCase;
 
 /**
- * TODO gene: missing JavaDoc.
+ * Test suite for the interaction constants.
  *
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
  * @version $Revision$
