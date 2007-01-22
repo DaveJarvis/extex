c9dcbac45dca2102e012899f48a06b3ec96d8a50
Gerd Neugebauer <gene@gerd-neugebauer.de>
2007-01-22 09:02:43 +0000

    typos fixed

--- ExTeX-core/src/java/org/extex/interpreter/interaction/Interaction.java
+++ ExTeX-core/src/java/org/extex/interpreter/interaction/Interaction.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2003-2006 The ExTeX Group and individual authors listed below
+ * Copyright (C) 2003-2007 The ExTeX Group and individual authors listed below
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
@@ -349,7 +349,7 @@ public abstract class Interaction implements Serializable {
 
     /**
      * Get the numeric index of the interaction mode.
-     * According to the definition os <logo>TeX</logo> the following mapping
+     * According to the definition of <logo>TeX</logo> the following mapping
      * holds:
      * <table>
      *  <tr>
