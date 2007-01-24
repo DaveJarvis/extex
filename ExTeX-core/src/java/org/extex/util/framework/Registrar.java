dc89efa9b05c07d9bc788694a849e7572680e1dc
Gerd Neugebauer <gene@gerd-neugebauer.de>
2007-01-24 21:32:47 +0000

    toString() implemented for debugging

--- ExTeX-core/src/java/org/extex/util/framework/Registrar.java
+++ ExTeX-core/src/java/org/extex/util/framework/Registrar.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2005-2006 The ExTeX Group and individual authors listed below
+ * Copyright (C) 2005-2007 The ExTeX Group and individual authors listed below
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
@@ -119,6 +119,15 @@ public final class Registrar {
 
             return this.type;
         }
+
+        /**
+         * @see java.lang.Object#toString()
+         */
+        public String toString() {
+
+            return this.type.toString() + " " + this.observer.toString();
+        }
+
     }
 
     /**
