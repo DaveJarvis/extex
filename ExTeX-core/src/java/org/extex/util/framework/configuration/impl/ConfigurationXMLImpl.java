322f10b343752b7ede7d92498054ae3fa2f2f9b9
Gerd Neugebauer <gene@gerd-neugebauer.de>
2007-01-24 21:32:13 +0000

    unnecessary cast removed

--- ExTeX-core/src/java/org/extex/util/framework/configuration/impl/ConfigurationXMLImpl.java
+++ ExTeX-core/src/java/org/extex/util/framework/configuration/impl/ConfigurationXMLImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2003-2006 The ExTeX Group and individual authors listed below
+ * Copyright (C) 2003-2007 The ExTeX Group and individual authors listed below
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
@@ -806,7 +806,7 @@ public class ConfigurationXMLImpl implements Configuration, Serializable {
                 ConfigurationSyntaxException,
                 ConfigurationIOException {
 
-        String src = ((Element) root).getAttribute("src");
+        String src = root.getAttribute("src");
 
         if (src == null || src.equals("")) {
             return this;
