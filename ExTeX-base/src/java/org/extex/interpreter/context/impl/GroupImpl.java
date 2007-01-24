9376b0462f84cdd2a03fd4fe87f3532205aab40e
Gerd Neugebauer <gene@gerd-neugebauer.de>
2007-01-24 21:21:38 +0000

    unnecessary cast removed

--- ExTeX-base/src/java/org/extex/interpreter/context/impl/GroupImpl.java
+++ ExTeX-base/src/java/org/extex/interpreter/context/impl/GroupImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2003-2006 The ExTeX Group and individual authors listed below
+ * Copyright (C) 2003-2007< The ExTeX Group and individual authors listed below
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
@@ -389,7 +389,7 @@ public class GroupImpl implements Group {
 
         if (Namespace.SUPPORT_NAMESPACE_DEF && code == null
                 && token instanceof CodeToken) {
-            CodeToken t = (CodeToken) token.cloneInDefaultNamespace();
+            CodeToken t = token.cloneInDefaultNamespace();
             if (t != token) {
                 code = getCodeForToken(t);
             }
