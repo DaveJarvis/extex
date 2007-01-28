899df1b665a250f609238282b2016a3a6b23bc93
Gerd Neugebauer <gene@gerd-neugebauer.de>
2007-01-28 17:34:41 +0000

    cleanup and bug fixes

---
+++ ExTeX-Unit-extex/src/java/org/extex/unit/extex/prefix/Showprefix.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright (C) 2006-2007 The ExTeX Group and individual authors listed below
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package org.extex.unit.extex.prefix;
+
+import java.util.logging.Logger;
+
+import org.extex.interpreter.Flags;
+import org.extex.interpreter.TokenSource;
+import org.extex.interpreter.context.Context;
+import org.extex.interpreter.exception.InterpreterException;
+import org.extex.interpreter.type.AbstractCode;
+import org.extex.typesetter.Typesetter;
+import org.extex.util.framework.logger.LogEnabled;
+
+/**
+ * This class provides an implementation for the primitive
+ * <code>\showprefix</code>.
+ *
+ * <doc name="showprefix" extension="yes">
+ * <h3>The Primitive <tt>\showprefix</tt></h3>
+ * <p>
+ *  TODO missing documentation
+ * </p>
+ *
+ * <h4>Syntax</h4>
+ *  The formal description of this primitive is the following:
+ *  <pre class="syntax">
+ *    &lang;showprefix&rang;
+ *      &rarr; <tt>\showprefix</tt>  </pre>
+ *
+ * <h4>Examples</h4>
+ *  <pre class="TeXSample">
+ *    \showprefix  </pre>
+ * </p>
+ * </doc>
+ *
+ * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
+ * @version $Revision:4431 $
+ */
+public class Showprefix extends AbstractCode implements LogEnabled {
+
+    /**
+     * The constant <tt>serialVersionUID</tt> contains the id for serialization.
+     */
+    protected static final long serialVersionUID = 2006L;
+
+    /**
+     * The field <tt>logger</tt> contains the logger.
+     */
+    private Logger logger;
+
+    /**
+     * Creates a new object.
+     *
+     * @param name the name for debugging
+     */
+    public Showprefix(final String name) {
+
+        super(name);
+    }
+
+    /**
+     * @see org.extex.interpreter.type.Code#execute(
+     *      org.extex.interpreter.Flags,
+     *      org.extex.interpreter.context.Context,
+     *      org.extex.interpreter.TokenSource,
+     *      org.extex.typesetter.Typesetter)
+     */
+    public void execute(final Flags prefix, final Context context,
+            final TokenSource source, final Typesetter typesetter)
+            throws InterpreterException {
+
+        logger.info("\n" + prefix.toText() + "\n");
+        prefix.clear();
+    }
+
+    /**
+     * @see org.extex.util.framework.logger.LogEnabled#enableLogging(
+     *      java.util.logging.Logger)
+     */
+    public void enableLogging(final Logger logger) {
+
+        this.logger = logger;
+    }
+
+}
