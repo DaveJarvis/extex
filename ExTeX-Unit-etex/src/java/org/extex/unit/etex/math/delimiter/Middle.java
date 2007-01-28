01facbd75984ceb052fcab72a8f3bc6d14e9469e
Gerd Neugebauer <gene@gerd-neugebauer.de>
2007-01-28 17:37:22 +0000

    cleanup and bug fixes

---
+++ ExTeX-Unit-etex/src/java/org/extex/unit/etex/math/delimiter/Middle.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright (C) 2005-2007 The ExTeX Group and individual authors listed below
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package org.extex.unit.etex.math.delimiter;
+
+import org.extex.interpreter.Flags;
+import org.extex.interpreter.TokenSource;
+import org.extex.interpreter.context.Context;
+import org.extex.interpreter.exception.InterpreterException;
+import org.extex.interpreter.type.math.MathDelimiter;
+import org.extex.typesetter.Typesetter;
+import org.extex.typesetter.listMaker.math.NoadConsumer;
+import org.extex.unit.tex.math.delimiter.AbstractTeXDelimiter;
+
+/**
+ * This class provides an implementation for the primitive <code>\middle</code>.
+ *
+ * <doc name="middle">
+ * <h3>The Math Primitive <tt>\middle</tt></h3>
+ * <p>
+ *  TODO missing documentation
+ * </p>
+ *
+ * <h4>Syntax</h4>
+ *  The formal description of this primitive is the following:
+ *  <pre class="syntax">
+ *    &lang;span&rang;
+ *       &rarr; <tt>\middle</tt> ... </pre>
+ *
+ * <h4>Examples</h4>
+ *  <pre class="TeXSample">
+ *    \middle  </pre>
+ *
+ * </doc>
+ *
+ * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
+ * @version $Revision:4431 $
+ */
+public class Middle extends AbstractTeXDelimiter {
+
+    /**
+     * The constant <tt>serialVersionUID</tt> contains the id for serialization.
+     */
+    protected static final long serialVersionUID = 2005L;
+
+    /**
+     * Creates a new object.
+     *
+     * @param name the name for tracing and debugging
+     */
+    public Middle(final String name) {
+
+        super(name);
+    }
+
+    /**
+     * @see org.extex.interpreter.type.Code#execute(
+     *      org.extex.interpreter.Flags,
+     *      org.extex.interpreter.context.Context,
+     *      org.extex.interpreter.TokenSource,
+     *      org.extex.typesetter.Typesetter)
+     */
+    public void execute(final Flags prefix, final Context context,
+            final TokenSource source, final Typesetter typesetter)
+            throws InterpreterException {
+
+        NoadConsumer nc = getListMaker(context, typesetter);
+        MathDelimiter del = parseDelimiter(context, source, typesetter, getName());
+        nc.left(del);
+    }
+
+}
