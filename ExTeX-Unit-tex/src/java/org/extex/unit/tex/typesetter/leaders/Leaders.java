bdae95bf3d17549db5395e2f1c174dff7379c11e
Gerd Neugebauer <gene@gerd-neugebauer.de>
2007-02-16 08:52:14 +0000

    documentation improved

--- ExTeX-Unit-tex/src/java/org/extex/unit/tex/typesetter/leaders/Leaders.java
+++ ExTeX-Unit-tex/src/java/org/extex/unit/tex/typesetter/leaders/Leaders.java
@@ -86,6 +86,17 @@ public class Leaders extends AbstractCode {
     }
 
     /**
+     * This method takes the first token and executes it. The result is placed
+     * on the stack. This operation might have side effects. To execute a token
+     * it might be necessary to consume further tokens.
+     *
+     * @param prefix the prefix controlling the execution
+     * @param context the interpreter context
+     * @param source the token source
+     * @param typesetter the typesetter
+     *
+     * @throws InterpreterException in case of an error
+     *
      * @see org.extex.interpreter.type.Code#execute(
      *      org.extex.interpreter.Flags,
      *      org.extex.interpreter.context.Context,
@@ -107,7 +118,7 @@ public class Leaders extends AbstractCode {
 
         Node node = null;
         if (code instanceof Boxable) {
-            Box b = ((Boxable) code).getBox(context, source, typesetter);
+            Box b = ((Boxable) code).getBox(context, source, typesetter, null);
             node = b.getNodes();
             horizontal = b.isHbox();
         } else if (code instanceof RuleConvertible) {
