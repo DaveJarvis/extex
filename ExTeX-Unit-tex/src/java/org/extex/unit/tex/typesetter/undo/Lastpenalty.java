be9f308fe98aa605dfba8c10169678cfba0d16e0
Gerd Neugebauer <gene@gerd-neugebauer.de>
2007-02-18 18:26:38 +0000

    documentation improved

--- ExTeX-Unit-tex/src/java/org/extex/unit/tex/typesetter/undo/Lastpenalty.java
+++ ExTeX-Unit-tex/src/java/org/extex/unit/tex/typesetter/undo/Lastpenalty.java
@@ -73,6 +73,19 @@ public class Lastpenalty extends AbstractCode
     }
 
     /**
+     * This method converts a register into a count. It might be necessary to
+     * read further tokens to determine which value to use. For instance an
+     * additional register number might be required. In this case the additional
+     * arguments Context and TokenSource can be used.
+     *
+     * @param context the interpreter context
+     * @param source the source for new tokens
+     * @param typesetter the typesetter to use for conversion
+     *
+     * @return the converted value
+     *
+     * @throws InterpreterException in case of an error
+     *
      * @see org.extex.interpreter.type.count.CountConvertible#convertCount(
      *      org.extex.interpreter.context.Context,
      *      org.extex.interpreter.TokenSource,
@@ -88,6 +101,21 @@ public class Lastpenalty extends AbstractCode
     }
 
     /**
+     * This method converts a register into a dimen.
+     * It might be necessary to read further tokens to determine which value to
+     * use. For instance an additional register number might be required. In
+     * this case the additional arguments Context and TokenSource can be used.
+     *
+     * The return value is the length in scaled points.
+     *
+     * @param context the interpreter context
+     * @param source the source for new tokens
+     * @param typesetter the typesetter to use for conversion
+     *
+     * @return the converted value in sp
+     *
+     * @throws InterpreterException in case of an error
+     *
      * @see org.extex.interpreter.type.dimen.DimenConvertible#convertDimen(
      *      org.extex.interpreter.context.Context,
      *      org.extex.interpreter.TokenSource,
@@ -103,6 +131,15 @@ public class Lastpenalty extends AbstractCode
     }
 
     /**
+     * This method is the getter for the description of the primitive.
+     *
+     * @param context the interpreter context
+     * @param source the source for further tokens to qualify the request
+     * @param typesetter the typesetter to use
+     *
+     * @return the description of the primitive as list of Tokens
+     * @throws InterpreterException in case of an error
+     *
      * @see org.extex.interpreter.type.Theable#the(
      *      org.extex.interpreter.context.Context,
      *      org.extex.interpreter.TokenSource,
