9486e2fc2b5f8f242670cd80d636d0c78bbb761a
Gerd Neugebauer <gene@gerd-neugebauer.de>
2007-02-18 18:27:29 +0000

    documentation improved

--- ExTeX-Unit-tex/src/java/org/extex/unit/tex/typesetter/undo/Unpenalty.java
+++ ExTeX-Unit-tex/src/java/org/extex/unit/tex/typesetter/undo/Unpenalty.java
@@ -74,6 +74,17 @@ public class Unpenalty extends AbstractCode {
     }
 
     /**
+     * This method takes the first token and executes it. The result is placed
+     * on the stack. This operation might have side effects. To execute a token
+     * it might be necessary to consume further tokens.
+     *
+     * @param prefix the prefix controlling the execution
+     * @param context the interpreter context
+     * @param source the token source
+     * @param typesetter the typesetter
+     *
+     * @throws InterpreterException in case of an error
+     *
      * @see org.extex.interpreter.type.Code#execute(
      *      org.extex.interpreter.Flags,
      *      org.extex.interpreter.context.Context,
