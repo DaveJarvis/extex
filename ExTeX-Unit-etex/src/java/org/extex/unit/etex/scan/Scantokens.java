5fc9a6cf21ef553a868a38f01433c28291717eb6
Gerd Neugebauer <gene@gerd-neugebauer.de>
2007-02-18 17:48:03 +0000

    documenation improved

--- ExTeX-Unit-etex/src/java/org/extex/unit/etex/scan/Scantokens.java
+++ ExTeX-Unit-etex/src/java/org/extex/unit/etex/scan/Scantokens.java
@@ -196,6 +196,18 @@ public class Scantokens extends AbstractCode implements ExpandableCode {
     }
 
     /**
+     * This method takes the first token and executes it. The result is placed
+     * on the stack. This operation might have side effects. To execute a token
+     * it might be necessary to consume further tokens.
+     *
+     * @param prefix the prefix controlling the execution
+     * @param context the interpreter context
+     * @param source the token source
+     * @param typesetter the typesetter
+     *
+     * @throws InterpreterException in case of an error
+     * @throws ConfigurationException in case of an configuration error
+     *
      * @see org.extex.interpreter.type.AbstractCode#execute(
      *      org.extex.interpreter.Flags,
      *      org.extex.interpreter.context.Context,
@@ -204,7 +216,8 @@ public class Scantokens extends AbstractCode implements ExpandableCode {
      */
     public void execute(final Flags prefix, final Context context,
             final TokenSource source, final Typesetter typesetter)
-            throws InterpreterException {
+            throws InterpreterException,
+                ConfigurationException {
 
         Tokens tokens;
         try {
@@ -213,15 +226,25 @@ public class Scantokens extends AbstractCode implements ExpandableCode {
             throw new EofInToksException(printableControlSequence(context));
         }
         TokenStreamFactory factory = source.getTokenStreamFactory();
-        try {
-            String t = tokens.toText(context.escapechar());
-            source.addStream(new TokenStreamProxy(factory.newInstance(t)));
-        } catch (ConfigurationException e) {
-            throw new InterpreterException(e);
-        }
+        String t = tokens.toText(context.escapechar());
+        source.addStream(new TokenStreamProxy(factory.newInstance(t)));
     }
 
     /**
+     * This method takes the first token and expands it. The result is placed
+     * on the stack.
+     * This means that expandable code does one step of expansion and puts the
+     * result on the stack. To expand a token it might be necessary to consume
+     * further tokens.
+     *
+     * @param prefix the prefix flags controlling the expansion
+     * @param context the interpreter context
+     * @param source the token source
+     * @param typesetter the typesetter
+     *
+     * @throws InterpreterException in case of an error
+     * @throws ConfigurationException in case of an configuration error
+     *
      * @see org.extex.interpreter.type.ExpandableCode#expand(
      *      org.extex.interpreter.Flags,
      *      org.extex.interpreter.context.Context,
@@ -230,7 +253,8 @@ public class Scantokens extends AbstractCode implements ExpandableCode {
      */
     public void expand(final Flags prefix, final Context context,
             final TokenSource source, final Typesetter typesetter)
-            throws InterpreterException {
+            throws InterpreterException,
+                ConfigurationException {
 
         execute(prefix, context, source, typesetter);
     }
