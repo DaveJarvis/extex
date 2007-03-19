8e31cce070c30fe4d0e44fa77afb622d71554bfe
Gerd Neugebauer <gene@gerd-neugebauer.de>
2007-03-19 18:02:30 +0000

    another attempt of a general revision

---
+++ ExTeX-core/src/java/org/extex/resource/tool/Lsr.java
@@ -0,0 +1,248 @@
+/*
+ * Copyright (C) 2003-2007 The ExTeX Group and individual authors listed below
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package org.extex.resource.tool;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * This class creates the output of 'ls -R'.
+ *
+ * <p>
+ * With the method addexcludeDir() it is possible, to add directories, which
+ * are excluded from the ls-R list.
+ * </p>
+ * <p>
+ * With the method addexcludeRegExp() it is possible, to add a RegExp, which
+ * are excluded an entry in each directory.
+ * </p>
+ * <p>
+ * Files starting with a '.' are ignored!
+ * </p>
+ *
+ * @author <a href="mailto:m.g.n@gmx.de">Michael Niedermair</a>
+ * @author <a href="mailto:sebastian.waschik@gmx.de">Sebastian Waschik</a>
+ * @version $Revision$
+ */
+public class Lsr {
+
+    /**
+     * The name of the ls-R file.
+     */
+    private static final String LS_R = "ls-R";
+
+    /**
+     * The base directory for searching.
+     */
+    private File basedirectory;
+
+    /**
+     * A list of directories, which are excluded from the ls-R.
+     * <p>Only in the top directory!</p>
+     */
+    private List excludeDir;
+
+    /**
+     * A list of RegExp, which are excluded in each directory.
+     */
+    private List excludeRegExp;
+
+    /**
+     * The level for the directory depth.
+     */
+    private int level = 0;
+
+    /**
+     * Creates a new object.
+     * @param theBasedirectory  The base directory for searching
+     */
+    public Lsr(final File theBasedirectory) {
+
+        basedirectory = theBasedirectory;
+        excludeDir = new ArrayList();
+        excludeRegExp = new ArrayList();
+    }
+
+    /**
+     * Add a directory in the exclude list.
+     * <p>Only in the top directory!</p>
+     *
+     * @param name  The name of the directory.
+     */
+    public void addExcludeDir(final String name) {
+
+        excludeDir.add(name);
+    }
+
+    /**
+     * Add a RegExp in the exclude list.
+     *
+     * @param name  The name of the directory.
+     */
+    public void addExcludeRegExp(final String name) {
+
+        excludeRegExp.add(name);
+    }
+
+    /**
+     * Print the ls-R-information to the printStream.
+     *
+     * @param printStream   The result is written to
+     *                      the <code>PrintStream</code>.
+     * @param directory     The base directory for output.
+     * @throws IOException if an io-error occurs.
+     */
+    private void printLsr(final PrintStream printStream, final File directory)
+            throws IOException {
+
+        if (directory != null) {
+            level++;
+            File[] files = directory.listFiles(new FileFilter() {
+
+                /**
+                 * @see java.io.FileFilter#accept(java.io.File)
+                 */
+                public boolean accept(final File pathname) {
+
+                    // 'ls-R'
+                    if (pathname.getName().equals(LS_R)) {
+                        return false;
+                    }
+                    // '.'
+                    if (pathname.isFile() && pathname.getName().startsWith(".")) {
+                        return false;
+                    }
+                    // exclude dir
+                    if (level == 1
+                            && excludeDir.size() > 0
+                            && pathname.isDirectory()
+                            && Collections.binarySearch(excludeDir, pathname
+                                    .getName()) >= 0) {
+                        return false;
+                    }
+                    // exclude regexp
+                    for (int i = 0, n = excludeRegExp.size(); i < n; i++) {
+                        String regexp = (String) excludeRegExp.get(i);
+                        if (regexp != null
+                                && pathname.getName().matches(regexp)) {
+                            return false;
+                        }
+                    }
+                    return true;
+                }
+            });
+
+            if (files != null) {
+                Arrays.sort(files);
+                // replace base dir name with a '.'
+                printStream.println(directory.toString().replaceFirst(
+                        basedirectory.toString(), ".")
+                        + ":");
+                for (int i = 0; i < files.length; i++) {
+                    printStream.println(files[i].getName());
+                }
+                printStream.println();
+                for (int i = 0; i < files.length; i++) {
+                    if (files[i].isDirectory()) {
+                        printLsr(printStream, files[i]);
+                    }
+                }
+            }
+            level--;
+        }
+    }
+
+    /**
+     * Print the ls-R-information to the printStream.
+     *
+     * @param printStream   The result is written to
+     *                      the <code>PrintStream</code>.
+     * @throws IOException if an io-error occurs.
+     */
+    public void printLsr(final PrintStream printStream) throws IOException {
+
+        Collections.sort(excludeDir);
+        PrintStream out = printStream;
+        if (out == null) {
+            out = new PrintStream(new FileOutputStream(basedirectory
+                    .getAbsolutePath()
+                    + File.separator + LS_R));
+        }
+
+        printLsr(out, basedirectory);
+
+        out.close();
+    }
+
+    /**
+     * main.
+     *
+     * @param args      The command line.
+     *                  -excludeDir xxx
+     *                  -excludeRegExp xxx
+     * @throws IOException if an io-error occurs.
+     */
+    public static void main(final String[] args) throws IOException {
+
+        if (args.length >= 1) {
+            Lsr lsr = new Lsr(new File(args[0]));
+            int p = 1;
+            while (p < args.length) {
+                if (args[p].equalsIgnoreCase("-excludeDir")) {
+                    if (p + 1 < args.length) {
+                        lsr.addExcludeDir(args[++p]);
+                        p++;
+                    } else {
+                        printCommandLine();
+                    }
+                } else if (args[p].equalsIgnoreCase("-excludeRegExp")) {
+                    if (p + 1 < args.length) {
+                        lsr.addExcludeRegExp(args[++p]);
+                        p++;
+                    } else {
+                        printCommandLine();
+                    }
+                } else {
+                    printCommandLine();
+                }
+            }
+            lsr.printLsr(null);
+        } else {
+            printCommandLine();
+        }
+    }
+
+    /**
+     * print the command line
+     */
+    private static void printCommandLine() {
+
+        System.err.println("java de.dante.util.resource.Lsr <directory> "
+                + "[-excludeDir xxx] [-excludeRegExp xxx] [...]");
+        System.exit(1);
+    }
+}
---
+++ ExTeX-core/src/java/org/extex/resource/tool/TocIndex.java
@@ -0,0 +1,144 @@
+/*
+ * Copyright (C) 2007 The ExTeX Group and individual authors listed below
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package org.extex.resource.tool;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.net.MalformedURLException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Traverse a directory tree and collect the files in an index.
+ *
+ * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
+ * @version $Revision$
+ */
+public class TocIndex {
+
+    /**
+     * Command line interface.
+     *
+     * @param args the command line arguments
+     */
+    public static void main(final String[] args) {
+
+        PrintStream err = System.err;
+        PrintStream out = System.out;
+        TocIndex tocIndex = new TocIndex();
+        File base = null;
+
+        for (int i = 0; i < args.length; i++) {
+            String arg = args[i];
+            if (!arg.startsWith("-")) {
+                if (base != null) {
+                    err.println("Too many bases");
+                    System.exit(-1);
+                }
+                base = new File(arg);
+            } else if ("".equals(arg)) {
+                // silently ignored
+            } else if ("-".equals(arg)) {
+                // silently ignored
+            } else if (arg.startsWith("-omit=")) {
+                tocIndex.omit(arg.substring(6));
+            } else if ("-output".startsWith(arg)) {
+                if (i < args.length) {
+                    arg = args[++i];
+                    try {
+                        out = new PrintStream(new FileOutputStream(arg));
+                    } catch (FileNotFoundException e) {
+                        err.print(arg);
+                        err.print(" ");
+                        err.println(e.getLocalizedMessage());
+                    }
+                }
+            } else {
+                err.println("Unknown argument: " + arg);
+                System.exit(-1);
+            }
+        }
+
+        try {
+            out.println("#");
+            out.print("# Created ");
+            out.println(new Date().toString());
+            out.println("#");
+            tocIndex.collect(base != null ? base : new File("."), out);
+        } catch (MalformedURLException e) {
+            e.printStackTrace(err);
+            System.exit(-1);
+        }
+        System.exit(0);
+    }
+
+    /**
+     * The field <tt>omit</tt> contains the omit patterns.
+     */
+    private List omit = new ArrayList();
+
+    /**
+     * Recursive procedure to traverse a directory tree and collect the
+     * files for the index.
+     *
+     * @param file the current file to consider
+     * @param out the output stream
+     *
+     * @throws MalformedURLException just in case
+     */
+    private void collect(final File file, final PrintStream out)
+            throws MalformedURLException {
+
+        String f = file.toURL().getFile();
+        f = f.substring(f.indexOf("/./") + 3);
+
+        int n = omit.size();
+        for (int i = 0; i < n; i++) {
+            if (f.matches((String) omit.get(i))) {
+                return;
+            }
+        }
+
+        if (file.isDirectory()) {
+            File[] list = file.listFiles();
+            for (int i = 0; i < list.length; i++) {
+                collect(list[i], out);
+            }
+        } else if (file.isFile()) {
+            out.print(file.getName());
+            out.print("=");
+            out.println(f);
+        }
+    }
+
+    /**
+     * Add a pattern to the omit patterns.
+     *
+     * @param arg the pattern to omit
+     */
+    private void omit(final String arg) {
+
+        omit.add(arg);
+    }
+
+}
