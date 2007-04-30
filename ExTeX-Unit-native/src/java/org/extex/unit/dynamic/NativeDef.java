c8004907aa75e240250397b9916eb4c6053fc793
Gerd Neugebauer <gene@gerd-neugebauer.de>
2007-04-30 18:08:25 +0000

    using Java 5 for loop

--- ExTeX-Unit-native/src/java/org/extex/unit/dynamic/NativeDef.java
+++ ExTeX-Unit-native/src/java/org/extex/unit/dynamic/NativeDef.java
@@ -39,86 +39,90 @@ import org.extex.interpreter.type.AbstractAssignment;
 import org.extex.typesetter.Typesetter;
 
 /**
- * This primitive provides a binding of a macro or active character to
- * code in some programming language.
- * This code implements the primitive <tt>\nativedef</tt>.
- *
+ * This primitive provides a binding of a macro or active character to code in
+ * some programming language. This code implements the primitive
+ * <tt>\nativedef</tt>.
+ * 
  * <doc name="nativedef">
  * <h3>The Primitive <tt>\nativedef</tt></h3>
  * <p>
- *  The primitive <tt>\nativedef</tt> assigns a definition to a macro or
- *  active character. This is done in a similar way as <tt>\def</tt>
- *  works. The difference is that the definition has to be provided in
- *  form of a Java class which glues in native code.
+ * The primitive <tt>\nativedef</tt> assigns a definition to a macro or active
+ * character. This is done in a similar way as <tt>\def</tt> works. The
+ * difference is that the definition has to be provided in form of a Java class
+ * which glues in native code.
  * </p>
- *
+ * 
  * <h4>Syntax</h4>
  * The general form of this primitive is
+ * 
  * <pre class="syntax">
  *   &lang;nativedef&rang;
  *       &rarr; <tt>\nativedef</tt> &lang;type&rang; {@linkplain
  *       org.extex.interpreter.TokenSource#getControlSequence(Context, Typesetter)
  *       &lang;control sequence&rang;} &lang;name&rang;  </pre>
+ * 
  * <p>
- *  The <tt>&lang;type&rang;</tt> is any specification of a list of
- *  tokens like a constant list enclosed in braces or a token register.
- *  The value of these tokens are taken and resolved via the configuration.
- *  This appropriate class is loaded if needed and instantiated. The
- *  instance is bound as code to the <i>&lang;control sequence&rang;</i>.
+ * The <tt>&lang;type&rang;</tt> is any specification of a list of tokens like
+ * a constant list enclosed in braces or a token register. The value of these
+ * tokens are taken and resolved via the configuration. This appropriate class
+ * is loaded if needed and instantiated. The instance is bound as code to the
+ * <i>&lang;control sequence&rang;</i>.
  * </p>
  * <p>
- *  The <tt>&lang;control sequence&rang;</tt> is any macro or active
- *  character. If this token is missing or of the wrong type then an
- *  error is raised.
+ * The <tt>&lang;control sequence&rang;</tt> is any macro or active character.
+ * If this token is missing or of the wrong type then an error is raised.
  * </p>
  * <p>
- *  The <tt>&lang;name&rang;</tt> is any specification of a list of
- *  tokens like a constant list enclosed in braces or a token register.
- *  The value of these tokens are passed to the binding class to specify the
- *  target. For instance the Java binding requires this to be name of the
- *  Java class implementing the functionality.
+ * The <tt>&lang;name&rang;</tt> is any specification of a list of tokens like
+ * a constant list enclosed in braces or a token register. The value of these
+ * tokens are passed to the binding class to specify the target. For instance
+ * the Java binding requires this to be name of the Java class implementing the
+ * functionality.
  * </p>
  * <p>
- *  The primitive <tt>\nativedef</tt> is local to the enclosing group as
- *  is <tt>\def</tt>. And similar to <tt>\def</tt> the modifier
- *  <tt>\global</tt> can be used to make the definition in all groups
- *  instead of the current group only.
+ * The primitive <tt>\nativedef</tt> is local to the enclosing group as is
+ * <tt>\def</tt>. And similar to <tt>\def</tt> the modifier
+ * <tt>\global</tt> can be used to make the definition in all groups instead
+ * of the current group only.
  * </p>
  * <p>
- *  The primitive <tt>\nativedef</tt> also respects the count register
- *  <tt>\globaldefs</tt> to enable general global assignment.
+ * The primitive <tt>\nativedef</tt> also respects the count register
+ * <tt>\globaldefs</tt> to enable general global assignment.
  * </p>
  * <p>
- *  Since the primitive is classified as assignment the value of
- *  <tt>\afterassignment</tt> is applied.
+ * Since the primitive is classified as assignment the value of
+ * <tt>\afterassignment</tt> is applied.
  * </p>
- *
+ * 
  * <h4>Examples</h4>
- *  <pre class="TeXSample">
+ * 
+ * <pre class="TeXSample">
  *    \nativedef{java}\x{my.primitive.MyPrimitive}  </pre>
+ * 
  * <p>
- *  This example shows how the control sequence <tt>\x</tt> is bound to the
- *  Java class <tt>my.primitive.MyPrimitive</tt>. This definition is local to
- *  the current group.
+ * This example shows how the control sequence <tt>\x</tt> is bound to the
+ * Java class <tt>my.primitive.MyPrimitive</tt>. This definition is local to
+ * the current group.
  * </p>
- *  <pre class="TeXSample">
+ * 
+ * <pre class="TeXSample">
  *    \global\nativedef{java}\x{my.primitive.MyPrimitive}  </pre>
+ * 
  * <p>
- *  This example shows how the control sequence <tt>\x</tt> is bound to the
- *  Java class <tt>my.primitive.MyPrimitive</tt>. This definition is performed
- *  globally.
+ * This example shows how the control sequence <tt>\x</tt> is bound to the
+ * Java class <tt>my.primitive.MyPrimitive</tt>. This definition is performed
+ * globally.
  * </p>
- *
+ * 
  * <h4>Configuration</h4>
  * <p>
- *  The supported types are determined in the configuration of the unit which
- *  defines the primitive. Here a
- *  mapping is specified assigning a binding class for each supported type.
- *  Thus it is possible to configure in the support for several extension types.
- *  Currently a binding for Java is provided. In the future other languages can
- *  be added easily.
+ * The supported types are determined in the configuration of the unit which
+ * defines the primitive. Here a mapping is specified assigning a binding class
+ * for each supported type. Thus it is possible to configure in the support for
+ * several extension types. Currently a binding for Java is provided. In the
+ * future other languages can be added easily.
  * </p>
- *
+ * 
  * <pre class="Configuration">
  *  &lt;define name="nativedef"
  *          class="org.extex.interpreter.primitives.dynamic.NativeDef"&gt;
@@ -126,21 +130,21 @@ import org.extex.typesetter.Typesetter;
  *          class="org.extex.interpreter.primitives.dynamic.java.JavaDef"/&gt;
  *  &lt;/define&gt;
  * </pre>
- *
+ * 
  * <p>
- *  The body of the define tag for the primitive may contain an arbitrary number
- *  of load sections. Each load has the attribute name and class. The attribute
- *  name determines the type. This corresponds to the type given in the first
- *  argument of the primitive invocation.
+ * The body of the define tag for the primitive may contain an arbitrary number
+ * of load sections. Each load has the attribute name and class. The attribute
+ * name determines the type. This corresponds to the type given in the first
+ * argument of the primitive invocation.
  * </p>
  * <p>
- *  The class attribute names the class which provides the binding to the
- *  target programming language.
+ * The class attribute names the class which provides the binding to the target
+ * programming language.
  * </p>
- *
+ * 
  * </doc>
- *
- *
+ * 
+ * 
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
  * @version $Revision$
  */
@@ -153,7 +157,7 @@ public class NativeDef extends AbstractAssignment
      * This inner class provides access to the functionality of an abstract
      * factory. It is here to overcome the deficiency of a missing multiple
      * inheritance in Java.
-     *
+     * 
      * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
      * @version $Revision$
      */
@@ -162,7 +166,7 @@ public class NativeDef extends AbstractAssignment
         /**
          * Create a new instance of the class given by the attribute
          * <tt>class</tt> of the configuration.
-         *
+         * 
          * @return the Code loaded
          * @throws ConfigurationException in case of an error
          */
@@ -173,7 +177,8 @@ public class NativeDef extends AbstractAssignment
     }
 
     /**
-     * The constant <tt>serialVersionUID</tt> contains the id for serialization.
+     * The constant <tt>serialVersionUID</tt> contains the id for
+     * serialization.
      */
     protected static final long serialVersionUID = 2006L;
 
@@ -191,7 +196,7 @@ public class NativeDef extends AbstractAssignment
 
     /**
      * Creates a new object.
-     *
+     * 
      * @param codeName the name of the primitive
      */
     public NativeDef(String codeName) {
@@ -205,23 +210,21 @@ public class NativeDef extends AbstractAssignment
      * This method is preferable to <tt>execute()</tt> since the
      * <tt>execute()</tt> method provided in this class takes care of
      * <tt>\afterassignment</tt> and <tt>\globaldefs</tt> as well.
-     *
+     * 
      * @param prefix the prefix controlling the execution
      * @param context the interpreter context
      * @param source the token source
      * @param typesetter the typesetter
-     *
+     * 
      * @throws InterpreterException in case of an error
      * @throws ConfigurationException in case of an configuration error
-     *
+     * 
      * @see org.extex.interpreter.type.AbstractAssignment#assign(
-     *       org.extex.interpreter.Flags,
-     *       org.extex.interpreter.context.Context,
-     *       org.extex.interpreter.TokenSource,
-     *       org.extex.typesetter.Typesetter)
+     *      org.extex.interpreter.Flags, org.extex.interpreter.context.Context,
+     *      org.extex.interpreter.TokenSource, org.extex.typesetter.Typesetter)
      */
-    public void assign(Flags prefix, Context context,
-            TokenSource source, Typesetter typesetter)
+    public void assign(Flags prefix, Context context, TokenSource source,
+            Typesetter typesetter)
             throws InterpreterException,
                 ConfigurationException {
 
@@ -245,29 +248,26 @@ public class NativeDef extends AbstractAssignment
 
     /**
      * Configure an object according to a given Configuration.
-     *
+     * 
      * @param config the configuration object to consider
-     *
+     * 
      * @throws ConfigurationException in case that something went wrong
-     *
+     * 
      * @see org.extex.framework.configuration.Configurable#configure(
      *      org.extex.framework.configuration.Configuration)
      */
-    public void configure(Configuration config)
-            throws ConfigurationException {
+    public void configure(Configuration config) throws ConfigurationException {
 
-        Iterator<Configuration> iterator = config.iterator("load");
-        while (iterator.hasNext()) {
-            Configuration cfg = iterator.next();
+        for (Configuration cfg : config) {
             map.put(cfg.getAttribute("name"), cfg);
         }
     }
 
     /**
      * Setter for the logger.
-     *
+     * 
      * @param log the logger to use
-     *
+     * 
      * @see org.extex.framework.logger.LogEnabled#enableLogging(
      *      java.util.logging.Logger)
      */
@@ -278,7 +278,7 @@ public class NativeDef extends AbstractAssignment
 
     /**
      * Getter for logger.
-     *
+     * 
      * @return the logger
      */
     protected Logger getLogger() {
