d0483f577d4590e100c6925393e5c4864714b2f8
Gerd Neugebauer <gene@gerd-neugebauer.de>
2007-05-04 11:07:16 +0000

    major restructuring to separate components

---
+++ ExTeX-Typesetter-tex/src/main/java/org/extex/typesetter/listMaker/math/DisplaymathListMaker.java
@@ -0,0 +1,227 @@
+/*
+ * Copyright (C) 2003-2007 The ExTeX Group and individual authors listed below
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package org.extex.typesetter.listMaker.math;
+
+import org.extex.core.Locator;
+import org.extex.core.count.Count;
+import org.extex.interpreter.TokenSource;
+import org.extex.interpreter.context.Context;
+import org.extex.interpreter.exception.helping.CantUseInException;
+import org.extex.interpreter.exception.helping.EofException;
+import org.extex.interpreter.exception.helping.HelpingException;
+import org.extex.scanner.type.token.Token;
+import org.extex.scanner.type.tokens.Tokens;
+import org.extex.typesetter.ListManager;
+import org.extex.typesetter.Mode;
+import org.extex.typesetter.Typesetter;
+import org.extex.typesetter.TypesetterOptions;
+import org.extex.typesetter.exception.TypesetterException;
+import org.extex.typesetter.type.NodeList;
+import org.extex.typesetter.type.noad.MathList;
+import org.extex.typesetter.type.noad.StyleNoad;
+import org.extex.typesetter.type.noad.util.MathContext;
+import org.extex.typesetter.type.node.HorizontalListNode;
+
+/**
+ * This is the list maker for the display math formulae.
+ * 
+ * <doc name="everydisplayend" type="register">
+ * <h3>The Tokens Parameter <tt>\everydisplayend</tt></h3>
+ * <p>
+ * The tokens parameter <tt>\everydisplayend</tt> contains a list of tokens
+ * which is inserted at the end of display math. Those tokens take effect just
+ * before the math mode is ended but after any tokens given explicitly.
+ * </p>
+ * 
+ * <h4>Syntax</h4>
+ * The formal description of this primitive is the following:
+ * 
+ * <pre class="syntax">
+ *    &lang;everydisplayend&rang;
+ *      &rarr; <tt>\everydisplayend</tt> {@linkplain
+ *        org.extex.interpreter.TokenSource#getOptionalEquals(Context)
+ *        &lang;equals&rang;} {@linkplain
+ *        org.extex.interpreter.TokenSource#getTokens(Context,TokenSource,Typesetter)
+ *        &lang;tokens&rang;}  </pre>
+ * 
+ * <h4>Examples</h4>
+ * 
+ * <pre class="TeXSample">
+ *    \everydisplayend={\,}  </pre>
+ * 
+ * </doc>
+ * 
+ * <doc name="everydisplay" type="register">
+ * <h3>The Tokens Parameter <tt>\everydisplay</tt></h3>
+ * <p>
+ * The tokens parameter <tt>\everydisplay</tt> contains a list of tokens which
+ * is inserted at the beginning of display math. Those tokens take effect after
+ * the math mode has been entered but before any tokens given explicitly.
+ * </p>
+ * 
+ * <h4>Syntax</h4>
+ * The formal description of this primitive is the following:
+ * 
+ * <pre class="syntax">
+ *    &lang;everydisplay&rang;
+ *      &rarr; <tt>\everydisplay</tt> {@linkplain
+ *        org.extex.interpreter.TokenSource#getOptionalEquals(Context)
+ *        &lang;equals&rang;} {@linkplain
+ *        org.extex.interpreter.TokenSource#getTokens(Context,TokenSource,Typesetter)
+ *        &lang;tokens&rang;}  </pre>
+ * 
+ * <h4>Examples</h4>
+ * 
+ * <pre class="TeXSample">
+ *    \everydisplay={\,}  </pre>
+ * 
+ * </doc>
+ * 
+ * 
+ * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
+ * @version $Revision:4496 $
+ */
+public class DisplaymathListMaker extends MathListMaker implements EqConsumer {
+
+    /**
+     * The field <tt>eqno</tt> contains the math list for the equation number.
+     * It is <code>null</code> if no equation number is set.
+     */
+    private MathList eqno = null;
+
+    /**
+     * The field <tt>leq</tt> contains the indicator for the side of the
+     * equation number. A value of <code>true</code> indicates an equation
+     * number on the left side.
+     */
+    private boolean leq = false;
+
+    /**
+     * Creates a new object.
+     * 
+     * @param manager the manager to ask for global changes
+     * @param locator the locator
+     */
+    public DisplaymathListMaker(ListManager manager, Locator locator) {
+
+        super(manager, locator);
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see org.extex.typesetter.ListMaker#complete(TypesetterOptions)
+     */
+    public NodeList complete(TypesetterOptions context)
+            throws TypesetterException {
+
+        // see [TTP 1195]
+        if (insufficientSymbolFonts(context)) {
+            throw new TypesetterException(new HelpingException(getLocalizer(),
+                "TTP.InsufficientSymbolFonts"));
+        }
+        // see [TTP 1195]
+        if (insufficientExtensionFonts(context)) {
+            throw new TypesetterException(new HelpingException(getLocalizer(),
+                "TTP.InsufficientExtensionFonts"));
+        }
+
+        HorizontalListNode list = new HorizontalListNode();
+
+        if (eqno != null && leq) {
+            // TODO gene: leqno unimplemented
+            throw new RuntimeException("unimplemented");
+        }
+
+        getNoads().typeset(null, null, 0, list,
+            new MathContext(StyleNoad.DISPLAYSTYLE, context), getLogger());
+
+        if (eqno != null && !leq) {
+            // TODO gene: eqno unimplemented
+            throw new RuntimeException("unimplemented");
+        }
+
+        // see [TTP 1200]
+        getManager().setSpacefactor(Count.THOUSAND);
+        // TODO gene: set space factor 1000 etc
+
+        return list;
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see org.extex.typesetter.ListMaker#getMode()
+     */
+    public Mode getMode() {
+
+        return Mode.DISPLAYMATH;
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see org.extex.typesetter.ListMaker#mathShift(
+     *      org.extex.interpreter.context.Context,
+     *      org.extex.interpreter.TokenSource,
+     *      org.extex.scanner.type.token.Token)
+     */
+    public void mathShift(Context context, TokenSource source, Token t)
+            throws TypesetterException,
+                HelpingException {
+
+        if (!isClosing()) {
+            Tokens toks = context.getToks("everydisplayend");
+            if (toks != null && toks.length() != 0) {
+                source.push(t);
+                source.push(toks);
+                setClosing(true);
+                return;
+            }
+        }
+
+        Token token = source.getToken(context);
+        if (token == null) {
+            throw new EofException("$$");
+        } else if (!t.equals(token)) {
+            // see [TTP 1197]
+            throw new HelpingException(getLocalizer(), "TTP.DisplayMathEnd");
+        }
+
+        getManager().endParagraph();
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see org.extex.typesetter.listMaker.math.EqConsumer#switchToNumber(boolean)
+     */
+    public void switchToNumber(boolean left) throws CantUseInException {
+
+        if (eqno != null) {
+            throw new CantUseInException(null, null);
+        }
+
+        leq = left;
+        eqno = new MathList();
+        setInsertionPoint(eqno);
+    }
+
+}
---
+++ ExTeX-Typesetter-tex/src/main/java/org/extex/typesetter/listMaker/math/NoadConsumer.java
@@ -0,0 +1,170 @@
+/*
+ * Copyright (C) 2004-2007 The ExTeX Group and individual authors listed below
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package org.extex.typesetter.listMaker.math;
+
+import org.extex.core.dimen.FixedDimen;
+import org.extex.core.muskip.Mudimen;
+import org.extex.core.muskip.Muskip;
+import org.extex.interpreter.Flags;
+import org.extex.interpreter.TokenSource;
+import org.extex.interpreter.context.Context;
+import org.extex.interpreter.context.group.GroupType;
+import org.extex.interpreter.context.tc.TypesettingContext;
+import org.extex.interpreter.exception.helping.HelpingException;
+import org.extex.typesetter.ListMaker;
+import org.extex.typesetter.Typesetter;
+import org.extex.typesetter.exception.TypesetterException;
+import org.extex.typesetter.type.math.MathCode;
+import org.extex.typesetter.type.math.MathDelimiter;
+import org.extex.typesetter.type.noad.Noad;
+
+/**
+ * This interface describes list makers which are able to consume a Noad.
+ * This is usually the case for math list makers.
+ *
+ * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
+ * @version $Revision:4496 $
+ */
+public interface NoadConsumer extends ListMaker {
+
+    /**
+     * Add a mathematical glyph.
+     *
+     * @param mc the math code
+     * @param tc the typesetting context
+     *
+     * @throws TypesetterException in case of an error
+     */
+    void add(MathCode mc, TypesettingContext tc) throws TypesetterException;
+
+    /**
+     * Add a mathematical delimiter.
+     *
+     * @param del the delimiter
+     * @param tc the typesetting context
+     *
+     * @throws TypesetterException in case of an error
+     */
+    void add(MathDelimiter del, TypesettingContext tc)
+            throws TypesetterException;
+
+    /**
+     * Add some math glue Noad to the internal list.
+     *
+     * @param glue the glue to add
+     *
+     * @throws TypesetterException in case of an error
+     */
+    void add(Muskip glue) throws TypesetterException;
+
+    /**
+     * Add some math dimen Noad to the internal list.
+     *
+     * @param skip the length to add
+     *
+     * @throws TypesetterException in case of an error
+     */
+    void add(Mudimen skip) throws TypesetterException;
+
+    /**
+     * Add an arbitrary Noad to the internal list if it is prepared to hold one.
+     * This is usually the case in math modes.
+     *
+     * @param noad the noad to add
+     *
+     * @throws TypesetterException in case of an error
+     */
+    void add(Noad noad) throws TypesetterException;
+
+    /**
+     * Get access to the previous noad.
+     *
+     * @return the previous noad or <code>null</code> if there is none
+     *
+     * @throws TypesetterException in case of an error
+     */
+    Noad getLastNoad() throws TypesetterException;
+
+    /**
+     * Open the group for a \left-\right construction.
+     *
+     * @param delimiter the delimiter to typeset on theleft side
+     *
+     * @throws TypesetterException in case of an error
+     */
+    void left(MathDelimiter delimiter) throws TypesetterException;
+
+    /**
+     * Middle in the group for a \left-\right construction.
+     *
+     * @param delimiter the delimiter to typeset here
+     *
+     * @throws TypesetterException in case of an error
+     */
+    void middle(MathDelimiter delimiter) throws TypesetterException;
+
+    /**
+     * Close the group for a <tt>\left</tt>-<tt>\right</tt> construction.
+     *
+     * @param delimiter the delimiter to typeset on the right side
+     *
+     * @throws TypesetterException in case of an error
+     */
+    void right(MathDelimiter delimiter) throws TypesetterException;
+
+    /**
+     * Process the input until a Noad is completed. A Noad is either a single
+     * Noad or a list of Noads resulting from the processing of a block.
+     *
+     * @param flags the flags to restore after processing
+     * @param context the interpreter context
+     * @param source the source for new tokens
+     * @param typesetter the typesetter
+     * @param primitive the name of the primitive for error messages
+     * @param groupType the group type in case that a group needs to be opened
+     *
+     * @return the Noad read or <code>null</code> if none could be gathered
+     * @throws TypesetterException in case of an error
+     * @throws HelpingException TODO
+     */
+    Noad scanNoad(Flags flags, Context context, TokenSource source,
+            Typesetter typesetter, String primitive, GroupType groupType)
+            throws TypesetterException, HelpingException;
+
+    /**
+     * This method instructs the implementing class to use a fraction
+     * construction. The math list collected so far is integrated into the
+     * fraction noad.
+     *
+     * @param leftDelimiter the left delimiter or <code>null</code> if none
+     *  should be used.
+     * @param rightDelimiter the right delimiter or <code>null</code> if none
+     *  should be used.
+     * @param ruleWidth th width of the rule or <code>null</code> to indicate
+     *  that the default width should be used
+     * @param tc the typesetting context
+     *
+     * @throws TypesetterException in case of an error
+     */
+    void switchToFraction(MathDelimiter leftDelimiter,
+            MathDelimiter rightDelimiter, FixedDimen ruleWidth,
+            TypesettingContext tc) throws TypesetterException;
+
+}
