80b5b9c340457d5c4b436020147e080bb5663f73
Gerd Neugebauer <gene@gerd-neugebauer.de>
2007-05-04 11:03:32 +0000

    major restructuring to separate components

---
+++ ExTeX-Typesetter-API/src/main/java/org/extex/interpreter/context/tc/TypesettingContext.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (C) 2003-2007 The ExTeX Group and individual authors listed below
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package org.extex.interpreter.context.tc;
+
+import java.io.Serializable;
+
+import org.extex.interpreter.context.Color;
+import org.extex.interpreter.type.font.Font;
+import org.extex.language.Language;
+
+/**
+ * The typesetting context is a container for attributes describing the
+ * appearance of glyphs or other nodes. This interface provides reading
+ * methods only. Thus the client can rely on an immutable object only.
+ *
+ *
+ * @see org.extex.interpreter.context.tc.ModifiableTypesettingContext
+ *
+ * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
+ * @version $Revision:4399 $
+ */
+public interface TypesettingContext extends Serializable {
+
+    /**
+     * Getter for the color.
+     *
+     * @return the current color
+     */
+    Color getColor();
+
+    /**
+     * Getter for the writing direction.
+     *
+     * @return the current direction
+     */
+    Direction getDirection();
+
+    /**
+     * Getter for the font component.
+     *
+     * @return the font
+     */
+    Font getFont();
+
+    /**
+     * Getter for the hyphenation table.
+     *
+     * @return the hyphenation table
+     */
+    Language getLanguage();
+
+}
---
+++ ExTeX-Typesetter-API/src/main/java/org/extex/interpreter/context/tc/TypesettingContextFactory.java
@@ -0,0 +1,286 @@
+/*
+ * Copyright (C) 2004-2007 The ExTeX Group and individual authors listed below
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package org.extex.interpreter.context.tc;
+
+import org.extex.font.type.other.NullFont;
+import org.extex.framework.AbstractFactory;
+import org.extex.framework.configuration.Configuration;
+import org.extex.framework.configuration.exception.ConfigurationClassNotFoundException;
+import org.extex.framework.configuration.exception.ConfigurationException;
+import org.extex.framework.configuration.exception.ConfigurationInstantiationException;
+import org.extex.framework.configuration.exception.ConfigurationMissingAttributeException;
+import org.extex.interpreter.context.Color;
+import org.extex.interpreter.type.font.Font;
+import org.extex.language.Language;
+import org.extex.language.LanguageManager;
+
+/**
+ * This class provides a factory for a
+ * {@link org.extex.interpreter.context.tc.TypesettingContext
+ *  TypesettingContext}.
+ *
+ * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
+ * @version $Revision:4399 $
+ */
+public class TypesettingContextFactory extends AbstractFactory {
+
+    /**
+     * The constant <tt>CLASS_ATTRIBUTE</tt> contains the name of the
+     * attribute used to extract the class name from the given configuration.
+     */
+    private static final String CLASS_ATTRIBUTE = "class";
+
+    /**
+     * The field <tt>languageManager</tt> contains the language manager.
+     */
+    private LanguageManager languageManager = null;
+
+    /**
+     * The field <tt>theClass</tt> contains the class to instantiate. It is
+     * kept here to speed up the method
+     * <tt>{@link #newInstance() newInstance()}</tt>.
+     */
+    private Class<?> theClass;
+
+    /**
+     * Creates a new object.
+     */
+    public TypesettingContextFactory() {
+
+        super();
+    }
+
+    /**
+     * Configure the factory according to a given Configuration.
+     *
+     * @param configuration the configuration for this factory
+     *
+     * @throws ConfigurationException in case of an error
+     * <ul>
+     * <li>ConfigurationMissingAttributeException in case that the
+     *      attribute <tt>CLASS_ATTRIBUTE</tt> is not set for the given
+     *      configuration.</li>
+     * <li>ConfigurationInstantiationException in case that the
+     *      instantiation of the given class causes a SecurityException.</li>
+     * <li>ConfigurationClassNotFoundException in case that the named class
+     *      could not be loaded.</li>
+     * </ul>
+     *
+     * @see org.extex.framework.configuration.Configurable#configure(
+     *      org.extex.framework.configuration.Configuration)
+     */
+    public void configure(Configuration configuration)
+            throws ConfigurationException {
+
+        super.configure(configuration);
+
+        String classname = configuration.getAttribute(CLASS_ATTRIBUTE);
+        if (classname == null) {
+            throw new ConfigurationMissingAttributeException(CLASS_ATTRIBUTE,
+                configuration);
+        }
+
+        try {
+            theClass = Class.forName(classname);
+        } catch (SecurityException e) {
+            throw new ConfigurationInstantiationException(e);
+        } catch (ClassNotFoundException e) {
+            throw new ConfigurationClassNotFoundException(classname,
+                configuration);
+        }
+    }
+
+    /**
+     * Factory method to acquire an instance of the TypesettingContext.
+     *
+     * @return an appropriate instance of the TypesettingContext.
+     *
+     * @throws ConfigurationInstantiationException in case that the
+     *             instantiation of the class failed.
+     */
+    protected ModifiableTypesettingContext newInstance()
+            throws ConfigurationInstantiationException {
+
+        ModifiableTypesettingContext context;
+
+        try {
+            context = (ModifiableTypesettingContext) (theClass.newInstance());
+            context.setFont(new NullFont());
+        } catch (InstantiationException e) {
+            throw new ConfigurationInstantiationException(e);
+        } catch (IllegalAccessException e) {
+            throw new ConfigurationInstantiationException(e);
+        }
+
+        return context;
+    }
+
+    /**
+     * Factory method to acquire an instance of the TypesettingContext.
+     *
+     * @param context the typesetting context to clone
+     * @param color the new value for the color
+     *
+     * @return an appropriate instance of the TypesettingContext.
+     *
+     * @throws ConfigurationInstantiationException in case that the
+     *             instantiation of the class failed.
+     */
+    public TypesettingContext newInstance(TypesettingContext context,
+            Color color) throws ConfigurationInstantiationException {
+
+        ModifiableTypesettingContext c = newInstance();
+        c.set(context);
+        c.setColor(color);
+
+        return c;
+    }
+
+    /**
+     * Factory method to acquire an instance of the TypesettingContext.
+     *
+     * @param context the typesetting context to clone
+     * @param direction the new value for the direction
+     *
+     * @return an appropriate instance of the TypesettingContext.
+     *
+     * @throws ConfigurationInstantiationException in case that the
+     *             instantiation of the class failed.
+     */
+    public TypesettingContext newInstance(TypesettingContext context,
+            Direction direction)
+            throws ConfigurationInstantiationException {
+
+        ModifiableTypesettingContext c = newInstance();
+        c.set(context);
+        c.setDirection(direction);
+
+        return c;
+    }
+
+    /**
+     * Factory method to acquire an instance of the TypesettingContext.
+     *
+     * @param context the typesetting context to clone
+     * @param font the new value for the font
+     *
+     * @return an appropriate instance of the TypesettingContext.
+     *
+     * @throws ConfigurationInstantiationException in case that the
+     *             instantiation of the class failed.
+     */
+    public TypesettingContext newInstance(TypesettingContext context,
+            Font font) throws ConfigurationInstantiationException {
+
+        ModifiableTypesettingContext c = newInstance();
+        c.set(context);
+        c.setFont(font);
+
+        return c;
+    }
+
+    /**
+     * Factory method to acquire an instance of the TypesettingContext with a
+     * new value for the language.
+     *
+     * @param context the typesetting context to clone
+     * @param language the new value for the hyphenation table
+     *
+     * @return an appropriate instance of the TypesettingContext.
+     *
+     * @throws ConfigurationException in case of a configuration problem
+     */
+    public TypesettingContext newInstance(TypesettingContext context,
+            Language language) {
+
+        ModifiableTypesettingContext c = newInstance();
+        c.set(context);
+        c.setLanguage(language);
+
+        return c;
+    }
+
+    /**
+     * Factory method to acquire an instance of the TypesettingContext with a
+     * new value for the language. The language might be loaded if necessary.
+     *
+     * @param tc the typesetting context to clone
+     * @param language the new value for the hyphenation table
+     *
+     * @return an appropriate instance of the TypesettingContext.
+     *
+     * @throws ConfigurationException in case of a configuration problem
+     */
+    public TypesettingContext newInstance(TypesettingContext tc,
+            String language) {
+
+        ModifiableTypesettingContext c = newInstance();
+        c.set(tc);
+        c.setLanguage(languageManager.getLanguage(language));
+
+        return c;
+    }
+
+    /**
+     * Setter for the language manager.
+     *
+     * @param languageManager the new language manager
+     */
+    public void setLanguageManager(LanguageManager languageManager) {
+
+        this.languageManager = languageManager;
+    }
+
+    /**
+     * Getter for the initial instance.
+     *
+     * @return the initial instance
+     *
+     * @throws ConfigurationException in case of an error
+     */
+    public TypesettingContext initial() {
+
+        ModifiableTypesettingContext tc = newInstance();
+        if (languageManager != null) {
+            tc.setLanguage(languageManager.getLanguage("0"));
+        }
+        return tc;
+    }
+
+    /**
+     * Create a new instance of a typesetting context. The typesetting context
+     * passed in as argument may not be under the control of this factory.
+     *
+     * @param tc the typesetting context to take over
+     *
+     * @return a typesetting context with the same attributes as the argument
+     *
+     * @throws ConfigurationException in case of an error
+     */
+    public TypesettingContext newInstance(TypesettingContext tc) {
+
+        if (languageManager != null) {
+            return newInstance(tc, languageManager.getLanguage(tc.getLanguage()
+                .getName()));
+        }
+        return tc;
+    }
+
+}
