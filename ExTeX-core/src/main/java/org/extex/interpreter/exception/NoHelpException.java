00a1a7a44adbf3e75b219bbc3f488ef05151a5b2
Gerd Neugebauer <gene@gerd-neugebauer.de>
2007-05-04 11:08:24 +0000

    major restructuring to separate components

---
+++ ExTeX-core/src/main/java/org/extex/interpreter/exception/NoHelpException.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2007 The ExTeX Group and individual authors listed below
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package org.extex.interpreter.exception;
+
+import org.extex.framework.i18n.LocalizerFactory;
+import org.extex.interpreter.exception.helping.HelpingException;
+
+/**
+ * TODO gene: missing JavaDoc.
+ * 
+ * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
+ * @version $Revision$
+ */
+public class NoHelpException extends HelpingException {
+
+    /**
+     * The field <tt>serialVersionUID</tt> contains the version number for
+     * serialization.
+     */
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * The field <tt>cause</tt> contains the cause.
+     */
+    private Throwable cause = null;
+
+    /**
+     * Creates a new object.
+     * 
+     * @param cause the cause
+     */
+    public NoHelpException(Throwable cause) {
+
+        super();
+        this.cause = cause;
+    }
+
+    /**
+     * Creates a new object.
+     * 
+     */
+    public NoHelpException(String message) {
+
+        super(LocalizerFactory.getLocalizer(NoHelpException.class), "Text",
+            message);
+    }
+
+}
