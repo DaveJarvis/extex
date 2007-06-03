e8ac9d21da1176c9bddb915abe1bba1d522e16b8
Gerd Neugebauer <gene@gerd-neugebauer.de>
2007-06-03 17:54:01 +0000

    reference fixed

--- ExTeX-Unit-tex/src/main/java/org/extex/unit/tex/file/nodes/WhatsItOpenNode.java
+++ ExTeX-Unit-tex/src/main/java/org/extex/unit/tex/file/nodes/WhatsItOpenNode.java
@@ -69,7 +69,8 @@ public class WhatsItOpenNode extends WhatsItNode {
      * 
      * @see org.extex.typesetter.type.Node#atShipping(
      *      org.extex.interpreter.context.Context,
-     *      org.extex.typesetter.Typesetter, FixedDimen, FixedDimen)
+     *      org.extex.typesetter.Typesetter,
+     *      org.extex.core.dimen.FixedDimen, org.extex.core.dimen.FixedDimen)
      */
     @Override
     public Node atShipping(PageContext context, Typesetter typesetter,
