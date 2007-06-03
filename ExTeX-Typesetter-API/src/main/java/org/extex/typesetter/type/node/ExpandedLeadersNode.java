0220c3e0dfc84a8676b0603f7bf03387d377f463
Gerd Neugebauer <gene@gerd-neugebauer.de>
2007-06-03 17:51:30 +0000

    reference fixed

--- ExTeX-Typesetter-API/src/main/java/org/extex/typesetter/type/node/ExpandedLeadersNode.java
+++ ExTeX-Typesetter-API/src/main/java/org/extex/typesetter/type/node/ExpandedLeadersNode.java
@@ -73,7 +73,8 @@ public class ExpandedLeadersNode extends AbstractLeadersNode {
      * {@inheritDoc}
      * 
      * @see org.extex.typesetter.type.node.AbstractLeadersNode#fillHorizontally(
-     *      long, org.extex.typesetter.type.Node, FixedDimen, FixedDimen)
+     *      long, org.extex.typesetter.type.Node,
+     *      org.extex.core.dimen.FixedDimen, org.extex.core.dimen.FixedDimen)
      */
     protected Node fillHorizontally(long total, Node node, FixedDimen posX,
             FixedDimen posY) {
@@ -117,14 +118,15 @@ public class ExpandedLeadersNode extends AbstractLeadersNode {
      * </p>
      * <p>
      * Otherwise a vlist is created and filled with appropriately many
-     * references to the repeat material. Optionally each elemnt is preceded by
+     * references to the repeat material. Optionally each element is preceded by
      * a kern node to achieve the proper distribution.
      * </p>
      * 
      * {@inheritDoc}
      * 
      * @see org.extex.typesetter.type.node.AbstractLeadersNode#fillVertically(
-     *      long, org.extex.typesetter.type.Node, FixedDimen, FixedDimen)
+     *      long, org.extex.typesetter.type.Node,
+     *      org.extex.core.dimen.FixedDimen, org.extex.core.dimen.FixedDimen)
      */
     protected Node fillVertically(long total, Node node, FixedDimen posX,
             FixedDimen posY) {
