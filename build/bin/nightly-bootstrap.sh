06853dc8d6ba72ee05396da038ac2f6ffc6b8d84
Gerd Neugebauer <gene@gerd-neugebauer.de>
2007-06-03 06:59:19 +0000

    renaming log to logs

--- build/bin/nightly-bootstrap.sh
+++ build/bin/nightly-bootstrap.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 #--------------------------------------------------------------------
-# This is the cron script used to start the nightly build.
-#
 # (c) 2003-2007 Gerd Neugebauer (gene@gerd-neugebauer.de)
-#--------------------------------------------------------------------
-#
-# CONFIGURATION SECTION
 #
 #--------------------------------------------------------------------
 # LOCALDIR contains the location where the build resides.
@@ -16,24 +11,23 @@ LOCALDIR="/serv/extex-project/build"
 INSTALLDIR=/serv/www/extex
 #--------------------------------------------------------------------
 # Point to the Java SDK
-export JAVA_HOME=/serv/extex-project/lib/jdk/
+export JAVA_HOME=/serv/extex-project/lib/jdk
 #--------------------------------------------------------------------
 # Point to Ant
 export ANT_HOME=/serv/extex-project/lib/ant/
 #--------------------------------------------------------------------
 #
-# END OF CONFIGURATION SECTION
+LOG=$LOCALDIR/logs/build.log
+#--------------------------------------------------------------------
 #
-LOG=$LOCALDIR/log
-TRUNK=$LOCALDIR/trunk
-export PATH=${JAVA_HOME}/bin:${ANT_HOME}/bin:$PATH
+export PATH=$JAVA_HOME/bin:$ANT_HOME/bin:$HOME/bin:$PATH
 
-cd $TRUNK
-date >$LOG/export.log >>$LOG/export.log 2>&1
+cd $LOCALDIR/trunk
+date >$LOG/export.log >>$LOG 2>&1
 
 svn update >>$LOG/export.log 2>&1
 
 cd build
-source bin/nightly-build.sh
+source bin/nightly-build.sh >>$LOG 2>&1
 
 #
