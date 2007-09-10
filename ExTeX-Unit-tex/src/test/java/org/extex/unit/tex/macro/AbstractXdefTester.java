c01ba212581ddf55b536c8b43f03e0282baa5078
Gerd Neugebauer <gene@gerd-neugebauer.de>
2007-09-10 21:22:15 +0000

    test cases added

--- ExTeX-Unit-tex/src/test/java/org/extex/unit/tex/macro/AbstractXdefTester.java
+++ ExTeX-Unit-tex/src/test/java/org/extex/unit/tex/macro/AbstractXdefTester.java
@@ -21,7 +21,7 @@ package org.extex.unit.tex.macro;
 
 /**
  * This is a test suite for xdef primitives.
- *
+ * 
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
  * @version $Revision: 2974 $
  */
@@ -29,7 +29,7 @@ public abstract class AbstractXdefTester extends AbstractDefTester {
 
     /**
      * Creates a new object.
-     *
+     * 
      * @param name the name
      * @param def the name of the primitive
      */
@@ -39,22 +39,62 @@ public abstract class AbstractXdefTester extends AbstractDefTester {
     }
 
     /**
-     * <testcase>
-     *  Test case checking that ...
-     * </testcase>
-     *
+     * <testcase> Test case checking that ... </testcase>
+     * 
      * @throws Exception in case of an error
      */
     public void testExpand1() throws Exception {
 
-        assertSuccess(//--- input code ---
-                DEFINE_BRACES
-                + "\\def\\a{A}"
-                + "\\" + getDef() + "\\aaa{X\\a X}"
-                + "\\def\\a{B}"
-                + "--\\aaa--\\end",
-                //--- output message ---
-                "--XAX--" + TERM);
+        assertSuccess(// --- input code ---
+            DEFINE_BRACES + "\\def\\a{A}" + "\\" + getDef() + "\\aaa{X\\a X}"
+                    + "\\def\\a{B}" + "--\\aaa--\\end",
+            // --- output message ---
+            "--XAX--" + TERM);
+    }
+
+    /**
+     * <testcase> Test case checking that ... </testcase>
+     * 
+     * @throws Exception in case of an error
+     */
+    public void testExpand2() throws Exception {
+
+        assertSuccess(// --- input code ---
+            DEFINE_CATCODES + "\\def\\a#1{-#1-}" + "\\" + getDef()
+                    + "\\aaa{X\\a9X}" + "\\def\\a{B}" + "--\\aaa--\\end",
+            // --- output message ---
+            "--X-9-X--" + TERM);
+    }
+
+    /**
+     * <testcase> Test case checking that ... </testcase>
+     * 
+     * @throws Exception in case of an error
+     */
+    public void testExpand3() throws Exception {
+
+        assertSuccess(// --- input code ---
+            DEFINE_CATCODES + "\\def\\a#1{-#1+\\b{#1}-}" + "\\def\\b#1{>#1<}"
+                    + "\\" + getDef() + "\\aaa{X\\a9X}" + "\\def\\a{B}"
+                    + "--\\aaa--\\end",
+            // --- output message ---
+            "--X-9+>9<-X--" + TERM);
+    }
+
+    /**
+     * <testcase> Test case checking that ... </testcase>
+     * 
+     * @throws Exception in case of an error
+     */
+    public void testNoexpand1() throws Exception {
+
+        assertOutput(// --- input code ---
+            DEFINE_BRACES + "\\def\\a{A}" + "\\" + getDef()
+                    + "\\aaa{X\\noexpand\\a X}" + "\\show\\aaa\\end",
+            // --- error message ---
+            "> \\aaa=macro:\n" + "->X\\aX.\n",
+            // --- output message ---
+            "");
     }
 
 }
