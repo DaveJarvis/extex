851a9c15d135c0eb5030485dc15df4fb3fb5942d
Gerd Neugebauer <gene@gerd-neugebauer.de>
2007-09-23 16:16:18 +0000

    documentation improved

--- ExTeX-Unit-tex/src/test/java/org/extex/unit/tex/file/ReadTest.java
+++ ExTeX-Unit-tex/src/test/java/org/extex/unit/tex/file/ReadTest.java
@@ -23,22 +23,22 @@ import org.extex.test.NoFlagsButGlobalPrimitiveTester;
 
 /**
  * This is a test suite for the primitive <tt>\read</tt>.
- *
+ * 
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
  * @version $Revision: 4808 $
  */
 public class ReadTest extends NoFlagsButGlobalPrimitiveTester {
 
     /**
-     * The constant <tt>DATA_FILE</tt> contains the name of the file to use for
-     * testing.
+     * The constant <tt>DATA_FILE</tt> contains the name of the file to use
+     * for testing.
      */
     private static final String DATA_FILE =
             "../ExTeX-Unit-tex/src/test/data/read_data.tex";
 
     /**
      * Method for running the tests standalone.
-     *
+     * 
      * @param args command line parameter
      */
     public static void main(String[] args) {
@@ -48,7 +48,7 @@ public class ReadTest extends NoFlagsButGlobalPrimitiveTester {
 
     /**
      * Creates a new object.
-     *
+     * 
      * @param arg the name
      */
     public ReadTest(String arg) {
@@ -57,134 +57,122 @@ public class ReadTest extends NoFlagsButGlobalPrimitiveTester {
     }
 
     /**
-     * <testcase primitive="\read">
-     *  Test case checking that a <tt>\read</tt> from the terminal in batch mode
-     *  leads to an error.
-     * </testcase>
-     *
+     * <testcase primitive="\read"> Test case checking that a <tt>\read</tt>
+     * from the terminal in batch mode leads to an error. </testcase>
+     * 
      * @throws Exception in case of an error
      */
     public void testTerm0() throws Exception {
 
-        assertFailure(//--- input code ---
+        assertFailure(// --- input code ---
             "\\batchmode\\read 1 to \\x ",
-            //--- error channel ---
+            // --- error channel ---
             "*** (cannot \\read from terminal in nonstop modes)");
     }
 
     /**
-     * <testcase primitive="\read">
-     *  Test case checking that a <tt>\read</tt> from the terminal in batch mode
-     *  leads to an error.
-     * </testcase>
-     *
+     * <testcase primitive="\read"> Test case checking that a <tt>\read</tt>
+     * from the terminal in batch mode leads to an error. </testcase>
+     * 
      * @throws Exception in case of an error
      */
     public void testTerm1() throws Exception {
 
-        assertFailure(//--- input code ---
+        assertFailure(// --- input code ---
             "\\scrollmode\\read 1 to \\x ",
-            //--- error channel ---
+            // --- error channel ---
             "*** (cannot \\read from terminal in nonstop modes)");
     }
 
     /**
-     * <testcase primitive="\read">
-     *  Test case checking that a <tt>\read</tt> from the terminal in batch mode
-     *  leads to an error.
-     * </testcase>
-     *
+     * <testcase primitive="\read"> Test case checking that a <tt>\read</tt>
+     * from the terminal in batch mode leads to an error. </testcase>
+     * 
      * @throws Exception in case of an error
      */
     public void testTerm2() throws Exception {
 
-        assertFailure(//--- input code ---
+        assertFailure(// --- input code ---
             "\\nonstopmode\\read 1 to \\x ",
-            //--- error channel ---
+            // --- error channel ---
             "*** (cannot \\read from terminal in nonstop modes)");
     }
 
     /**
-     * <testcase primitive="\read">
-     *  Test case checking that a <tt>\read</tt> ...
-     * </testcase>
-     *
+     * <testcase primitive="\read"> Test case checking that a <tt>\read</tt>
+     * needs the <tt>to</tt> after the stream name. </testcase>
+     * 
      * @throws Exception in case of an error
      */
     public void testErr0() throws Exception {
 
-        assertFailure(//--- input code ---
+        assertFailure(// --- input code ---
             "\\read 1",
-            //--- error channel ---
+            // --- error channel ---
             "Missing `to' inserted");
     }
 
     /**
-     * <testcase primitive="\read">
-     *  Test case checking that a <tt>\read</tt> ...
-     * </testcase>
-     *
+     * <testcase primitive="\read"> Test case checking that a <tt>\read</tt>
+     * needs a control sequence to store the result in. </testcase>
+     * 
      * @throws Exception in case of an error
      */
     public void testErr1() throws Exception {
 
-        assertFailure(//--- input code ---
+        assertFailure(// --- input code ---
             "\\read 1 to ",
-            //--- error channel ---
+            // --- error channel ---
             "Missing control sequence inserted");
     }
 
     /**
-     * <testcase primitive="\read">
-     *  Test case checking that a <tt>\read</tt> can read the first line of the
-     *  test file.
-     * </testcase>
-     *
+     * <testcase primitive="\read"> Test case checking that a <tt>\read</tt>
+     * can read the first line of the test file. </testcase>
+     * 
      * @throws Exception in case of an error
      */
     public void test1() throws Exception {
 
-        assertSuccess(//--- input code ---
+        assertSuccess(// --- input code ---
             DEFINE_CATCODES + "\\openin1 " + DATA_FILE + " "
                     + "\\read 1 to \\x " + "\\x" + "\\end",
-            //--- output channel ---
+            // --- output channel ---
             "123xyz" + TERM);
     }
 
     /**
-     * <testcase primitive="\read">
-     *  Test case checking that a <tt>\read</tt> honors the <tt>\global</tt>
-     *  prefix b defining the macro globally.
+     * <testcase primitive="\read"> Test case checking that a <tt>\read</tt>
+     * honors the <tt>\global</tt> prefix by defining the macro globally.
      * </testcase>
-     *
+     * 
      * @throws Exception in case of an error
      */
     public void test2() throws Exception {
 
-        assertSuccess(//--- input code ---
+        assertSuccess(// --- input code ---
             DEFINE_CATCODES + "\\openin1 " + DATA_FILE + " "
                     + "{\\global\\read 1 to \\x}" + "\\x" + "\\end",
-            //--- output channel ---
+            // --- output channel ---
             "123xyz" + TERM);
     }
 
     /**
-     * <testcase primitive="\read">
-     *  Test case checking that a <tt>\read</tt> makes its definition locally
-     *  to the current group per default.
+     * <testcase primitive="\read"> Test case checking that a <tt>\read</tt>
+     * makes its definition locally to the current group per default.
      * </testcase>
-     *
+     * 
      * @throws Exception in case of an error
      */
     public void test3() throws Exception {
 
-        assertFailure(//--- input code ---
+        assertFailure(// --- input code ---
             DEFINE_CATCODES + "\\openin1 " + DATA_FILE + " "
                     + "{\\read 1 to \\x}" + "\\x" + "\\end",
-            //--- output channel ---
+            // --- output channel ---
             "Undefined control sequence \\x");
     }
 
-    //TODO implement the primitive specific test cases
+    // TODO implement more primitive specific test cases
 
 }
