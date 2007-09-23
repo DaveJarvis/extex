677706932da93c926cf1a624ece0dfdb71298ff7
Gerd Neugebauer <gene@gerd-neugebauer.de>
2007-09-23 14:19:54 +0000

    test case added

--- ExTeX-Unit-tex/src/test/java/org/extex/unit/tex/typesetter/undo/LastkernTest.java
+++ ExTeX-Unit-tex/src/test/java/org/extex/unit/tex/typesetter/undo/LastkernTest.java
@@ -23,7 +23,7 @@ import org.extex.unit.tex.register.dimen.AbstractReadonlyDimenRegisterTester;
 
 /**
  * This is a test suite for the primitive <tt>\lastkern</tt>.
- *
+ * 
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
  * @version $Revision$
  */
@@ -31,6 +31,7 @@ public class LastkernTest extends AbstractReadonlyDimenRegisterTester {
 
     /**
      * Command line interface.
+     * 
      * @param args the arguments
      */
     public static void main(String[] args) {
@@ -40,7 +41,7 @@ public class LastkernTest extends AbstractReadonlyDimenRegisterTester {
 
     /**
      * Constructor for LastboxTest.
-     *
+     * 
      * @param arg the name
      */
     public LastkernTest(String arg) {
@@ -49,93 +50,105 @@ public class LastkernTest extends AbstractReadonlyDimenRegisterTester {
     }
 
     /**
-     * <testcase>
-     *  Test case checking that ...
+     * <testcase> Test case checking that <tt>\lastkern</tt> on an empty list
+     * returns 0pt. </testcase>
+     * 
+     * @throws Exception in case of an error
+     */
+    public void test0() throws Exception {
+
+        assertOutput(
+        // --- input code ---
+            "\\showthe\\lastkern\\end",
+            // --- output channel ---
+            "> 0.0pt.\n",
+            //
+            null);
+    }
+
+    /**
+     * <testcase> Test case checking that <tt>\lastkern</tt> is showable.
      * </testcase>
-     *
+     * 
      * @throws Exception in case of an error
      */
     public void test1() throws Exception {
 
         assertOutput(
-        //--- input code ---
-                "a\\kern123pt\\showthe\\lastkern\\end",
-                //--- output channel ---
-                "> 123.0pt.\n",
-                //
-                null);
+        // --- input code ---
+            "a\\kern123pt\\showthe\\lastkern\\end",
+            // --- output channel ---
+            "> 123.0pt.\n",
+            //
+            null);
     }
 
     /**
-     * <testcase>
-     *  Test case checking that ...
+     * <testcase> Test case checking that \relax does not produce a node.
      * </testcase>
-     *
+     * 
      * @throws Exception in case of an error
      */
     public void test2() throws Exception {
 
         assertOutput(
-        //--- input code ---
-                "a\\kern123pt\\relax\\showthe\\lastkern\\end",
-                //--- output channel ---
-                "> 123.0pt.\n",
-                //
-                null);
+        // --- input code ---
+            "a\\kern123pt\\relax\\showthe\\lastkern\\end",
+            // --- output channel ---
+            "> 123.0pt.\n",
+            //
+            null);
     }
 
     /**
-     * <testcase>
-     *  Test case checking that ...
-     * </testcase>
-     *
+     * <testcase> Test case checking that <tt>\lastkern</tt> returns 0pt if
+     * the last node is not a kern node. </testcase>
+     * 
      * @throws Exception in case of an error
      */
     public void test3() throws Exception {
 
         assertOutput(
-        //--- input code ---
-                "a\\showthe\\lastkern\\end",
-                //--- output channel ---
-                "> 0.0pt.\n",
-                //
-                null);
+        // --- input code ---
+            "a\\showthe\\lastkern\\end",
+            // --- output channel ---
+            "> 0.0pt.\n",
+            //
+            null);
     }
 
     /**
-     * <testcase>
-     *  Test case checking that ...
+     * <testcase> Test case checking that <tt>\lastkern</tt> is a dimen value.
      * </testcase>
-     *
+     * 
      * @throws Exception in case of an error
      */
     public void test10() throws Exception {
 
         assertOutput(
-        //--- input code ---
-                "a\\kern123pt\\dimen0=\\lastkern x\\showthe\\dimen0\\end",
-                //--- output channel ---
-                "> 123.0pt.\n",
-                //
-                null);
+        // --- input code ---
+            "a\\kern123pt\\dimen0=\\lastkern x\\showthe\\dimen0\\end",
+            // --- output channel ---
+            "> 123.0pt.\n",
+            //
+            null);
     }
 
     /**
-     * <testcase>
-     *  Test case checking that ...
-     * </testcase>
-     *
+     * <testcase> Test case checking that <tt>\lastkern</tt> is count
+     * convertible. </testcase>
+     * 
      * @throws Exception in case of an error
      */
     public void test11() throws Exception {
 
         assertOutput(
-        //--- input code ---
-                "a\\kern123pt\\count0=\\lastkern x\\showthe\\count0\\end",
-                //--- output channel ---
-                "> 8060928.\n",
-                //
-                null);
+        // --- input code ---
+            "a\\kern123pt\\count0=\\lastkern x\\showthe\\count0\\end",
+            // --- output channel ---
+            "> 8060928.\n",
+            //
+            null);
     }
 
 }
