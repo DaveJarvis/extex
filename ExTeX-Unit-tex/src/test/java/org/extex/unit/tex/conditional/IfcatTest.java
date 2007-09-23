7a60401e1d37301a7a2307efc9c3b51b34ccbeac
Gerd Neugebauer <gene@gerd-neugebauer.de>
2007-09-23 19:35:21 +0000

    test cases added

--- ExTeX-Unit-tex/src/test/java/org/extex/unit/tex/conditional/IfcatTest.java
+++ ExTeX-Unit-tex/src/test/java/org/extex/unit/tex/conditional/IfcatTest.java
@@ -48,11 +48,12 @@ public class IfcatTest extends ConditionalTester {
     }
 
     /**
-     * <testcase primitive="\ifcat"> Test case checking that ... </testcase>
+     * <testcase primitive="\ifcat"> Test case checking that <tt>\ifcat</tt>
+     * needs an argument </testcase>
      * 
      * @throws Exception in case of an error
      */
-    public void testErr1() throws Exception {
+    public void testError1() throws Exception {
 
         assertFailure(// --- input code ---
             "\\ifcat",
@@ -62,7 +63,21 @@ public class IfcatTest extends ConditionalTester {
 
     /**
      * <testcase primitive="\ifcat"> Test case checking that <tt>\ifcat</tt>
-     * ... </testcase>
+     * needs two arguments </testcase>
+     * 
+     * @throws Exception in case of an error
+     */
+    public void testError2() throws Exception {
+
+        assertFailure(// --- input code ---
+            "\\ifcat \\relax",
+            // --- output channel ---
+            "Unexpected end of file while processing \\ifcat");
+    }
+
+    /**
+     * <testcase primitive="\ifcat"> Test case checking that <tt>\ifcat</tt>
+     * detects two identical letters. </testcase>
      * 
      * @throws Exception in case of an error
      */
@@ -76,7 +91,7 @@ public class IfcatTest extends ConditionalTester {
 
     /**
      * <testcase primitive="\ifcat"> Test case checking that <tt>\ifcat</tt>
-     * ... </testcase>
+     * detects two different letters. </testcase>
      * 
      * @throws Exception in case of an error
      */
@@ -90,7 +105,7 @@ public class IfcatTest extends ConditionalTester {
 
     /**
      * <testcase primitive="\ifcat"> Test case checking that <tt>\ifcat</tt>
-     * ... </testcase>
+     * classifies a letter and an other character as different. </testcase>
      * 
      * @throws Exception in case of an error
      */
@@ -104,7 +119,7 @@ public class IfcatTest extends ConditionalTester {
 
     /**
      * <testcase primitive="\ifcat"> Test case checking that <tt>\ifcat</tt>
-     * ... </testcase>
+     * classifies a letter and a control sequence as different. </testcase>
      * 
      * @throws Exception in case of an error
      */
@@ -118,7 +133,7 @@ public class IfcatTest extends ConditionalTester {
 
     /**
      * <testcase primitive="\ifcat"> Test case checking that <tt>\ifcat</tt>
-     * ... </testcase>
+     * classifies two identical control sequences as equal. </testcase>
      * 
      * @throws Exception in case of an error
      */
@@ -132,7 +147,7 @@ public class IfcatTest extends ConditionalTester {
 
     /**
      * <testcase primitive="\ifcat"> Test case checking that <tt>\ifcat</tt>
-     * ... </testcase>
+     * classifies two different control sequences as equal. </testcase>
      * 
      * @throws Exception in case of an error
      */
@@ -144,6 +159,117 @@ public class IfcatTest extends ConditionalTester {
             "a" + TERM);
     }
 
-    // TODO implement the primitive specific test cases
+    /**
+     * <testcase primitive="\ifcat"> Test case checking that <tt>\ifcat</tt>
+     * classifies two identical other characters as equal. </testcase>
+     * 
+     * @throws Exception in case of an error
+     */
+    public void testOther1() throws Exception {
 
+        assertSuccess(// --- input code ---
+            DEFINE_CATCODES + "\\ifcat 11 a\\else b\\fi \\end",
+            // --- output channel ---
+            "a" + TERM);
+    }
+
+    /**
+     * <testcase primitive="\ifcat"> Test case checking that <tt>\ifcat</tt>
+     * classifies two different other characters as equal. </testcase>
+     * 
+     * @throws Exception in case of an error
+     */
+    public void testOther2() throws Exception {
+
+        assertSuccess(// --- input code ---
+            DEFINE_CATCODES + "\\ifcat 12 a\\else b\\fi \\end",
+            // --- output channel ---
+            "a" + TERM);
+    }
+
+    /**
+     * <testcase primitive="\ifcat"> Test case checking that <tt>\ifcat</tt>
+     * classifies two math shift characters as equal. </testcase>
+     * 
+     * @throws Exception in case of an error
+     */
+    public void testMath1() throws Exception {
+
+        assertSuccess(// --- input code ---
+            DEFINE_CATCODES + "\\ifcat $$ a\\else b\\fi \\end",
+            // --- output channel ---
+            "a" + TERM);
+    }
+
+    /**
+     * <testcase primitive="\ifcat"> Test case checking that <tt>\ifcat</tt>
+     * classifies two subscript characters as equal. </testcase>
+     * 
+     * @throws Exception in case of an error
+     */
+    public void testSub1() throws Exception {
+
+        assertSuccess(// --- input code ---
+            DEFINE_CATCODES + "\\ifcat __ a\\else b\\fi \\end",
+            // --- output channel ---
+            "a" + TERM);
+    }
+
+    /**
+     * <testcase primitive="\ifcat"> Test case checking that <tt>\ifcat</tt>
+     * classifies two tab mark characters as equal. </testcase>
+     * 
+     * @throws Exception in case of an error
+     */
+    public void testSuper1() throws Exception {
+
+        assertSuccess(// --- input code ---
+            DEFINE_CATCODES + "\\ifcat && a\\else b\\fi \\end",
+            // --- output channel ---
+            "a" + TERM);
+    }
+
+    /**
+     * <testcase primitive="\ifcat"> Test case checking that <tt>\ifcat</tt>
+     * classifies two open group characters as equal. </testcase>
+     * 
+     * @throws Exception in case of an error
+     */
+    public void testOpen1() throws Exception {
+
+        assertSuccess(// --- input code ---
+            DEFINE_CATCODES + "\\ifcat {{ a\\else b\\fi \\end",
+            // --- output channel ---
+            "a" + TERM);
+    }
+
+    /**
+     * <testcase primitive="\ifcat"> Test case checking that <tt>\ifcat</tt>
+     * classifies two close group characters as equal. </testcase>
+     * 
+     * @throws Exception in case of an error
+     */
+    public void testClose1() throws Exception {
+
+        assertSuccess(// --- input code ---
+            DEFINE_CATCODES + "\\ifcat }} a\\else b\\fi \\end",
+            // --- output channel ---
+            "a" + TERM);
+    }
+
+    /**
+     * <testcase primitive="\ifcat"> Test case checking that <tt>\ifcat</tt>
+     * classifies two macro parameter characters as equal. </testcase>
+     * 
+     * @throws Exception in case of an error
+     */
+    public void testMacro1() throws Exception {
+
+        assertSuccess(// --- input code ---
+            DEFINE_CATCODES + "\\ifcat ## a\\else b\\fi \\end",
+            // --- output channel ---
+            "a" + TERM);
+    }
+
+    // TODO implement more primitive specific test cases
 }
