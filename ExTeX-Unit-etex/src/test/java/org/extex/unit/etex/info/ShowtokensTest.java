1509c9ab43ed8a52d0830b48c43b1096b9fcb7a3
Gerd Neugebauer <gene@gerd-neugebauer.de>
2007-09-24 21:31:38 +0000

    documentation improved

--- ExTeX-Unit-etex/src/test/java/org/extex/unit/etex/info/ShowtokensTest.java
+++ ExTeX-Unit-etex/src/test/java/org/extex/unit/etex/info/ShowtokensTest.java
@@ -23,7 +23,7 @@ import org.extex.test.NoFlagsPrimitiveTester;
 
 /**
  * This is a test suite for the primitive <tt>\showtokens</tt>.
- *
+ * 
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
  * @version $Revision: 4808 $
  */
@@ -31,7 +31,7 @@ public class ShowtokensTest extends NoFlagsPrimitiveTester {
 
     /**
      * Constructor for JobnameTest.
-     *
+     * 
      * @param arg the name
      */
     public ShowtokensTest(String arg) {
@@ -41,254 +41,206 @@ public class ShowtokensTest extends NoFlagsPrimitiveTester {
     }
 
     /**
-     * <testcase primitive="\showtokens">
-     *  Test case checking that <tt>\showtokens</tt> ...
-     * </testcase>
-     *
+     * <testcase primitive="\showtokens"> Test case checking that
+     * <tt>\showtokens</tt> needs an argument. </testcase>
+     * 
+     * @throws Exception in case of an error
+     */
+    public void testError0() throws Exception {
+
+        assertFailure(// --- input code ---
+            "\\showtokens",
+            // --- output channel ---
+            "File ended while scanning text of \\showtokens");
+    }
+
+    /**
+     * <testcase primitive="\showtokens"> Test case checking that
+     * <tt>\showtokens</tt> needs a complete argument group. </testcase>
+     * 
      * @throws Exception in case of an error
      */
     public void testError1() throws Exception {
 
-        assertFailure(//--- input code ---
-                DEFINE_BRACES
-                + "\\showtokens{xxx",
-                //--- output channel ---
-                "File ended while scanning text of \\showtokens");
+        assertFailure(// --- input code ---
+            DEFINE_BRACES + "\\showtokens{xxx",
+            // --- output channel ---
+            "File ended while scanning text of \\showtokens");
     }
 
     /**
-     * <testcase primitive="\showtokens">
-     *  Test case checking that <tt>\showtokens</tt> ...
-     * </testcase>
-     *
-     * @throws Exception in case of an error
-     */
-    public void testError2() throws Exception {
-
-        assertFailure(//--- input code ---
-                DEFINE_BRACES
-                + "\\showtokens ",
-                //--- output channel ---
-                "Unexpected end of file while processing tokens");
-    }
-
-    /**
-     * <testcase primitive="\showtokens">
-     *  Test case checking that <tt>\showtokens</tt> ...
-     * </testcase>
-     *
+     * <testcase primitive="\showtokens"> Test case checking that
+     * <tt>\showtokens</tt> does not expand \relax. </testcase>
+     * 
      * @throws Exception in case of an error
      */
     public void test1() throws Exception {
 
-        assertOutput(//--- input code ---
-                DEFINE_BRACES
-                + "\\showtokens{\\relax}"
-                + "\\end",
-                //--- output channel ---
-                "> \\relax.\n",
-                "");
+        assertOutput(// --- input code ---
+            DEFINE_BRACES + "\\showtokens{\\relax}" + "\\end",
+            // --- output channel ---
+            "> \\relax.\n", "");
     }
 
     /**
-     * <testcase primitive="\showtokens">
-     *  Test case checking that <tt>\showtokens</tt> ...
-     * </testcase>
-     *
+     * <testcase primitive="\showtokens"> Test case checking that
+     * <tt>\showtokens</tt> does not expand \jobname. </testcase>
+     * 
      * @throws Exception in case of an error
      */
     public void test2() throws Exception {
 
-        assertOutput(//--- input code ---
-                DEFINE_BRACES
-                + "\\showtokens{\\jobname}"
-                + "\\end",
-                //--- output channel ---
-                "> \\jobname.\n",
-                "");
+        assertOutput(// --- input code ---
+            DEFINE_BRACES + "\\showtokens{\\jobname}" + "\\end",
+            // --- output channel ---
+            "> \\jobname.\n", "");
     }
 
     /**
-     * <testcase primitive="\showtokens">
-     *  Test case checking that <tt>\showtokens</tt> ...
-     * </testcase>
-     *
+     * <testcase primitive="\showtokens"> Test case checking that
+     * <tt>\showtokens</tt> shows an undefined active character. </testcase>
+     * 
      * @throws Exception in case of an error
      */
     public void test3() throws Exception {
 
-        assertOutput(//--- input code ---
-                DEFINE_BRACES + "\\catcode`\\~=13 "
-                + "\\showtokens{x~x}"
-                + "\\end",
-                //--- output channel ---
-                "> x~x.\n",
-                "");
+        assertOutput(// --- input code ---
+            DEFINE_BRACES + "\\catcode`\\~=13 " + "\\showtokens{x~x}" + "\\end",
+            // --- output channel ---
+            "> x~x.\n", "");
     }
 
     /**
-     * <testcase primitive="\showtokens">
-     *  Test case checking that <tt>\showtokens</tt> ...
-     * </testcase>
-     *
+     * <testcase primitive="\showtokens"> Test case checking that
+     * <tt>\showtokens</tt> doubles the macro param character. </testcase>
+     * 
      * @throws Exception in case of an error
      */
     public void test4() throws Exception {
 
-        assertOutput(//--- input code ---
-                DEFINE_CATCODES
-                + "\\showtokens{x#x}"
-                + "\\end",
-                //--- output channel ---
-                "> x##x.\n",
-                "");
+        assertOutput(// --- input code ---
+            DEFINE_CATCODES + "\\showtokens{x#x}" + "\\end",
+            // --- output channel ---
+            "> x##x.\n", "");
     }
 
     /**
-     * <testcase primitive="\showtokens">
-     *  Test case checking that <tt>\showtokens</tt> ...
-     * </testcase>
-     *
+     * <testcase primitive="\showtokens"> Test case checking that
+     * <tt>\showtokens</tt> shows an subscript character. </testcase>
+     * 
      * @throws Exception in case of an error
      */
     public void test5() throws Exception {
 
-        assertOutput(//--- input code ---
-                DEFINE_CATCODES
-                + "\\showtokens{x_x}"
-                + "\\end",
-                //--- output channel ---
-                "> x_x.\n",
-                "");
+        assertOutput(// --- input code ---
+            DEFINE_CATCODES + "\\showtokens{x_x}" + "\\end",
+            // --- output channel ---
+            "> x_x.\n", "");
     }
 
     /**
-     * <testcase primitive="\showtokens">
-     *  Test case checking that <tt>\showtokens</tt> ...
-     * </testcase>
-     *
+     * <testcase primitive="\showtokens"> Test case checking that
+     * <tt>\showtokens</tt> shows an superscript character. </testcase>
+     * 
      * @throws Exception in case of an error
      */
     public void test6() throws Exception {
 
-        assertOutput(//--- input code ---
-                DEFINE_CATCODES
-                + "\\showtokens{x^x}"
-                + "\\end",
-                //--- output channel ---
-                "> x^x.\n",
-                "");
+        assertOutput(// --- input code ---
+            DEFINE_CATCODES + "\\showtokens{x^x}" + "\\end",
+            // --- output channel ---
+            "> x^x.\n", "");
     }
 
     /**
-     * <testcase primitive="\showtokens">
-     *  Test case checking that <tt>\showtokens</tt> ...
-     * </testcase>
-     *
+     * <testcase primitive="\showtokens"> Test case checking that
+     * <tt>\showtokens</tt> lets \expandafter do it's work before reading the
+     * token list. </testcase>
+     * 
      * @throws Exception in case of an error
      */
     public void test12() throws Exception {
 
-        assertOutput(//--- input code ---
-                DEFINE_BRACES
-                + "\\showtokens\\expandafter{\\jobname}"
-                + "\\end",
-                //--- output channel ---
-                "> texput.\n",
-                "");
+        assertOutput(// --- input code ---
+            DEFINE_BRACES + "\\showtokens\\expandafter{\\jobname}" + "\\end",
+            // --- output channel ---
+            "> texput.\n", "");
     }
 
     /**
-     * <testcase primitive="\showtokens">
-     *  Test case checking that <tt>\showtokens</tt> ...
+     * <testcase primitive="\showtokens"> Test case checking that
+     * <tt>\showtokens</tt> complains if aargument group is missing.
      * </testcase>
-     *
+     * 
      * @throws Exception in case of an error
      */
     public void test21() throws Exception {
 
-        assertFailure(//--- input code ---
-                DEFINE_BRACES
-                + "\\showtokens\\relax",
-                //--- output channel ---
-                "Missing `{' inserted");
+        assertFailure(// --- input code ---
+            DEFINE_BRACES + "\\showtokens\\relax",
+            // --- output channel ---
+            "Missing `{' inserted");
     }
 
     /**
-     * <testcase primitive="\showtokens">
-     *  Test case checking that <tt>\showtokens</tt> ...
+     * <testcase primitive="\showtokens"> Test case checking that
+     * <tt>\showtokens</tt> can take the tokens from a tokens register.
      * </testcase>
-     *
+     * 
      * @throws Exception in case of an error
      */
     public void test22() throws Exception {
 
-        assertOutput(//--- input code ---
-                DEFINE_BRACES
-                + "\\toks0={abc}"
-                + "\\showtokens\\toks0"
-                + "\\end",
-                //--- output channel ---
-                "> abc.\n",
-                "");
+        assertOutput(// --- input code ---
+            DEFINE_BRACES + "\\toks0={abc}" + "\\showtokens\\toks0" + "\\end",
+            // --- output channel ---
+            "> abc.\n", "");
     }
 
     /**
-     * <testcase primitive="\showtokens">
-     *  Test case checking that <tt>\showtokens</tt> ...
-     * </testcase>
-     *
+     * <testcase primitive="\showtokens"> Test case checking that
+     * <tt>\showtokens</tt> shows embedded groups with braces. </testcase>
+     * 
      * @throws Exception in case of an error
      */
     public void test23() throws Exception {
 
-        assertOutput(//--- input code ---
-                DEFINE_BRACES
-                + "\\showtokens{x{y}z}"
-                + "\\end",
-                //--- output channel ---
-                "> x{y}z.\n",
-                "");
+        assertOutput(// --- input code ---
+            DEFINE_BRACES + "\\showtokens{x{y}z}" + "\\end",
+            // --- output channel ---
+            "> x{y}z.\n", "");
     }
 
     /**
-     * <testcase primitive="\showtokens">
-     *  Test case checking that <tt>\showtokens</tt> ...
-     * </testcase>
-     *
+     * <testcase primitive="\showtokens"> Test case checking that
+     * <tt>\showtokens</tt> can be used in a macro. </testcase>
+     * 
      * @throws Exception in case of an error
      */
     public void test31() throws Exception {
 
-        assertOutput(//--- input code ---
-                DEFINE_CATCODES
-                + "\\def\\a#1{\\showtokens{#1}}"
-                + "\\a{0}"
-                + "\\end",
-                //--- output channel ---
-                "> 0.\n",
-                "");
+        assertOutput(// --- input code ---
+            DEFINE_CATCODES + "\\def\\a#1{\\showtokens{#1}}" + "\\a{0}"
+                    + "\\end",
+            // --- output channel ---
+            "> 0.\n", "");
     }
 
     /**
-     * <testcase primitive="\showtokens">
-     *  Test case checking that <tt>\showtokens</tt> ...
-     * </testcase>
-     *
+     * <testcase primitive="\showtokens"> Test case checking that
+     * <tt>\showtokens</tt> preserves braces in an argument of the macr
+     * invocation. </testcase>
+     * 
      * @throws Exception in case of an error
      */
     public void test32() throws Exception {
 
-        assertOutput(//--- input code ---
-                DEFINE_CATCODES
-                + "\\def\\a#1{\\showtokens{#1}}"
-                + "\\a{{0}}"
-                + "\\end",
-                //--- output channel ---
-                "> {0}.\n",
-                "");
+        assertOutput(// --- input code ---
+            DEFINE_CATCODES + "\\def\\a#1{\\showtokens{#1}}" + "\\a{{0}}"
+                    + "\\end",
+            // --- output channel ---
+            "> {0}.\n", "");
     }
 
-    //TODO implement the primitive specific test cases
-
+    // TODO implement more primitive specific test cases
 }
