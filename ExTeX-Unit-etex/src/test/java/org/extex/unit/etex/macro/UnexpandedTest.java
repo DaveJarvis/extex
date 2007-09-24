18a8a35b82fa395f32de168677dc36e91cfefcfe
Gerd Neugebauer <gene@gerd-neugebauer.de>
2007-09-24 22:32:09 +0000

    new

---
+++ ExTeX-Unit-etex/src/test/java/org/extex/unit/etex/macro/UnexpandedTest.java
@@ -0,0 +1,99 @@
+/*
+ * Copyright (C) 2005-2007 The ExTeX Group and individual authors listed below
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package org.extex.unit.etex.macro;
+
+import org.extex.test.ExTeXLauncher;
+
+/**
+ * This is a test suite for the primitive <tt>&#005c;unexpanded</tt>.
+ * 
+ * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
+ * @version $Revision$
+ */
+public class UnexpandedTest extends ExTeXLauncher {
+
+    /**
+     * Method for running the tests standalone.
+     * 
+     * @param args command line parameter
+     */
+    public static void main(String[] args) {
+
+        junit.textui.TestRunner.run(UnexpandedTest.class);
+    }
+
+    /**
+     * Constructor for RelaxTest.
+     * 
+     * @param arg the name
+     */
+    public UnexpandedTest(String arg) {
+
+        super(arg);
+        setConfig("etex-test");
+    }
+
+    /**
+     * <testcase primitive="&#005c;unexpanded"> Test case checking that
+     * <tt>&#005c;unexpanded</tt> needs an argument. </testcase>
+     * 
+     * @throws Exception in case of an error
+     */
+    public void testError1() throws Exception {
+
+        assertFailure(
+        // --- input code ---
+            "\\unexpanded",
+            // --- error channel ---
+            "Unexpected end of file while processing \\unexpanded");
+    }
+
+    /**
+     * <testcase primitive="&#005c;unexpanded"> Test case checking that
+     * <tt>&#005c;unexpanded</tt> needs a group argument. </testcase>
+     * 
+     * @throws Exception in case of an error
+     */
+    public void testError2() throws Exception {
+
+        assertFailure(
+        // --- input code ---
+            "\\unexpanded a",
+            // --- error channel ---
+            "Missing `{' inserted");
+    }
+
+    /**
+     * <testcase primitive="&#005c;unexpanded"> Test case checking that
+     * <tt>&#005c;unexpanded</tt> sets the flag. </testcase>
+     * 
+     * @throws Exception in case of an error
+     */
+    public void test1() throws Exception {
+
+        assertSuccess(
+        // --- input code ---
+            DEFINE_BRACES + "\\unexpanded{abc}\\end",
+            // --- output channel ---
+            "abc" + TERM);
+    }
+
+    // TODO implement more primitive specific test cases
+}
