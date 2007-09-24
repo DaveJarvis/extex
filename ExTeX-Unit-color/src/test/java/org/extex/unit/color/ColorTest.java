ca57888da7a7c647be8a19daf6fcb16c4a771d51
Gerd Neugebauer <gene@gerd-neugebauer.de>
2007-09-24 21:05:05 +0000

    test case added

--- ExTeX-Unit-color/src/test/java/org/extex/unit/color/ColorTest.java
+++ ExTeX-Unit-color/src/test/java/org/extex/unit/color/ColorTest.java
@@ -27,15 +27,35 @@ import org.extex.test.NoFlagsButGlobalPrimitiveTester;
 
 /**
  * This is a test suite for the primitive <tt>\color</tt>.
- *
+ * 
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
  * @version $Revision: 4732 $
  */
 public class ColorTest extends NoFlagsButGlobalPrimitiveTester {
 
     /**
+     * The field <tt>FIFTY_PERCENT</tt> contains the representation for .5.
+     */
+    private static final int FIFTY_PERCENT = 0x7fff;
+
+    /**
+     * The field <tt>THIRTY_PERCENT</tt> contains the representation for .3.
+     */
+    private static final int THIRTY_PERCENT = 19660;
+
+    /**
+     * The field <tt>TWENTY_PERCENT</tt> contains the representation for .2.
+     */
+    private static final int TWENTY_PERCENT = 13106;
+
+    /**
+     * The field <tt>TEN_PERCENT</tt> contains the representation for .1.
+     */
+    private static final int TEN_PERCENT = 6553;
+
+    /**
      * Method for running the tests standalone.
-     *
+     * 
      * @param args command line parameter
      */
     public static void main(String[] args) {
@@ -45,7 +65,7 @@ public class ColorTest extends NoFlagsButGlobalPrimitiveTester {
 
     /**
      * Creates a new object.
-     *
+     * 
      * @param arg the name
      */
     public ColorTest(String arg) {
@@ -55,220 +75,225 @@ public class ColorTest extends NoFlagsButGlobalPrimitiveTester {
     }
 
     /**
-     * <testcase primitive="\color">
-     *  Test case checking that <tt>\color</tt> ...
-     * </testcase>
-     *
+     * <testcase primitive="\color"> Test case checking that <tt>\color</tt>
+     * needs an argument. </testcase>
+     * 
+     * @throws Exception in case of an error
+     */
+    public void testError1() throws Exception {
+
+        assertFailure(// --- input code ---
+            "\\color ",
+            // --- log message ---
+            "Unexpected end of file while processing \\color");
+    }
+
+    /**
+     * <testcase primitive="\color"> Test case checking that <tt>\color</tt>
+     * complains about a missing brace. </testcase>
+     * 
      * @throws Exception in case of an error
      */
     public void testMissing1() throws Exception {
 
-        assertFailure(//--- input code ---
-                DEFINE_BRACES + "\\color .1 .2 .3",
-                //--- log message ---
-                "Missing left brace for color value");
+        assertFailure(// --- input code ---
+            DEFINE_BRACES + "\\color .1 .2 .3",
+            // --- log message ---
+            "Missing left brace for color value");
     }
 
     /**
-     * <testcase primitive="\color">
-     *  Test case checking that <tt>\color</tt> needs a left brace to start a
-     *  RGB color triple.
-     * </testcase>
-     *
+     * <testcase primitive="\color"> Test case checking that <tt>\color</tt>
+     * needs a left brace to start a RGB color triple. </testcase>
+     * 
      * @throws Exception in case of an error
      */
     public void testMissing2() throws Exception {
 
-        assertFailure(//--- input code ---
-                DEFINE_BRACES + "\\color rgb .1 .2 .3",
-                //--- log message ---
-                "Missing left brace for color value");
+        assertFailure(// --- input code ---
+            DEFINE_BRACES + "\\color rgb .1 .2 .3",
+            // --- log message ---
+            "Missing left brace for color value");
     }
 
     /**
-     * <testcase primitive="\color">
-     *  Test case checking that <tt>\color</tt> ...
-     * </testcase>
-     *
+     * <testcase primitive="\color"> Test case checking that <tt>\color</tt>
+     * in default mode returns a RGB color and the components make it to the
+     * output. </testcase>
+     * 
      * @throws Exception in case of an error
      */
     public void testDefault1() throws Exception {
 
-        Interpreter interpreter = assertSuccess(//--- input code ---
-                DEFINE_BRACES + "\\color {.1 .2 .3}",
-                //--- log message ---
-                "");
+        Interpreter interpreter = assertSuccess(// --- input code ---
+            DEFINE_BRACES + "\\color {.1 .2 .3}",
+            // --- log message ---
+            "");
         assertNotNull(interpreter);
-        Color color = interpreter.getContext().getTypesettingContext()
-                .getColor();
+        Color color =
+                interpreter.getContext().getTypesettingContext().getColor();
         assertTrue(color instanceof RgbColor);
         RgbColor c = (RgbColor) color;
-        assertEquals(6553, c.getRed());
-        assertEquals(13106, c.getGreen());
-        assertEquals(19660, c.getBlue());
+        assertEquals(TEN_PERCENT, c.getRed());
+        assertEquals(TWENTY_PERCENT, c.getGreen());
+        assertEquals(THIRTY_PERCENT, c.getBlue());
         assertEquals(0, c.getAlpha());
     }
 
     /**
-     * <testcase primitive="\color">
-     *  Test case checking that <tt>\color</tt> ...
-     * </testcase>
-     *
+     * <testcase primitive="\color"> Test case checking that <tt>\color</tt>
+     * can digest an alpha channel value in default mode. </testcase>
+     * 
      * @throws Exception in case of an error
      */
     public void testDefault2() throws Exception {
 
-        Interpreter interpreter = assertSuccess(//--- input code ---
-                DEFINE_BRACES + "\\color alpha .5 {.1 .2 .3}",
-                //--- log message ---
-                "");
+        Interpreter interpreter = assertSuccess(// --- input code ---
+            DEFINE_BRACES + "\\color alpha .5 {.1 .2 .3}",
+            // --- log message ---
+            "");
         assertNotNull(interpreter);
-        Color color = interpreter.getContext().getTypesettingContext()
-                .getColor();
+        Color color =
+                interpreter.getContext().getTypesettingContext().getColor();
         assertTrue(color instanceof RgbColor);
         RgbColor c = (RgbColor) color;
-        assertEquals(6553, c.getRed());
-        assertEquals(13106, c.getGreen());
-        assertEquals(19660, c.getBlue());
-        assertEquals(0xffff / 2, c.getAlpha());
+        assertEquals(TEN_PERCENT, c.getRed());
+        assertEquals(TWENTY_PERCENT, c.getGreen());
+        assertEquals(THIRTY_PERCENT, c.getBlue());
+        assertEquals(FIFTY_PERCENT, c.getAlpha());
     }
 
     /**
-     * <testcase primitive="\color">
-     *  Test case checking that <tt>\color</tt> ...
-     * </testcase>
-     *
+     * <testcase primitive="\color"> Test case checking that <tt>\color</tt>
+     * can consume an explicit RGB color. </testcase>
+     * 
      * @throws Exception in case of an error
      */
     public void testRgb1() throws Exception {
 
-        Interpreter interpreter = assertSuccess(//--- input code ---
-                DEFINE_BRACES + "\\color rgb {.1 .2 .3}",
-                //--- log message ---
-                "");
+        Interpreter interpreter = assertSuccess(// --- input code ---
+            DEFINE_BRACES + "\\color rgb {.1 .2 .3}",
+            // --- log message ---
+            "");
         assertNotNull(interpreter);
-        Color color = interpreter.getContext().getTypesettingContext()
-                .getColor();
+        Color color =
+                interpreter.getContext().getTypesettingContext().getColor();
         assertTrue(color instanceof RgbColor);
         RgbColor c = (RgbColor) color;
-        assertEquals(6553, c.getRed());
-        assertEquals(13106, c.getGreen());
-        assertEquals(19660, c.getBlue());
+        assertEquals(TEN_PERCENT, c.getRed());
+        assertEquals(TWENTY_PERCENT, c.getGreen());
+        assertEquals(THIRTY_PERCENT, c.getBlue());
         assertEquals(0, c.getAlpha());
     }
 
     /**
-     * <testcase primitive="\color">
-     *  Test case checking that <tt>\color</tt> ...
-     * </testcase>
-     *
+     * <testcase primitive="\color"> Test case checking that <tt>\color</tt>
+     * can consume an explicit RGB color with alpha channel. </testcase>
+     * 
      * @throws Exception in case of an error
      */
     public void testRgb2() throws Exception {
 
-        Interpreter interpreter = assertSuccess(//--- input code ---
-                DEFINE_BRACES + "\\color rgb alpha .5 {.1 .2 .3}",
-                //--- log message ---
-                "");
+        Interpreter interpreter = assertSuccess(// --- input code ---
+            DEFINE_BRACES + "\\color rgb alpha .5 {.1 .2 .3}",
+            // --- log message ---
+            "");
         assertNotNull(interpreter);
-        Color color = interpreter.getContext().getTypesettingContext()
-                .getColor();
+        Color color =
+                interpreter.getContext().getTypesettingContext().getColor();
         assertTrue(color instanceof RgbColor);
         RgbColor c = (RgbColor) color;
-        assertEquals(6553, c.getRed());
-        assertEquals(13106, c.getGreen());
-        assertEquals(19660, c.getBlue());
-        assertEquals(0xffff / 2, c.getAlpha());
+        assertEquals(TEN_PERCENT, c.getRed());
+        assertEquals(TWENTY_PERCENT, c.getGreen());
+        assertEquals(THIRTY_PERCENT, c.getBlue());
+        assertEquals(FIFTY_PERCENT, c.getAlpha());
     }
 
     /**
-     * <testcase primitive="\color">
-     *  Test case checking that <tt>\color</tt> ...
-     * </testcase>
-     *
+     * <testcase primitive="\color"> Test case checking that <tt>\color</tt>
+     * can consume an alpha channel with an explicit RGB color. </testcase>
+     * 
      * @throws Exception in case of an error
      */
     public void testRgb3() throws Exception {
 
-        Interpreter interpreter = assertSuccess(//--- input code ---
-                DEFINE_BRACES + "\\color alpha .5 rgb {.1 .2 .3}",
-                //--- log message ---
-                "");
+        Interpreter interpreter = assertSuccess(// --- input code ---
+            DEFINE_BRACES + "\\color alpha .5 rgb {.1 .2 .3}",
+            // --- log message ---
+            "");
         assertNotNull(interpreter);
-        Color color = interpreter.getContext().getTypesettingContext()
-                .getColor();
+        Color color =
+                interpreter.getContext().getTypesettingContext().getColor();
         assertTrue(color instanceof RgbColor);
         RgbColor c = (RgbColor) color;
-        assertEquals(6553, c.getRed());
-        assertEquals(13106, c.getGreen());
-        assertEquals(19660, c.getBlue());
-        assertEquals(0xffff / 2, c.getAlpha());
+        assertEquals(TEN_PERCENT, c.getRed());
+        assertEquals(TWENTY_PERCENT, c.getGreen());
+        assertEquals(THIRTY_PERCENT, c.getBlue());
+        assertEquals(FIFTY_PERCENT, c.getAlpha());
     }
 
     /**
-     * <testcase primitive="\color">
-     *  Test case checking that <tt>\color</tt> ...
-     * </testcase>
-     *
+     * <testcase primitive="\color"> Test case checking that <tt>\color</tt>
+     * can consume an explicit gray scale color. </testcase>
+     * 
      * @throws Exception in case of an error
      */
     public void testGray1() throws Exception {
 
-        Interpreter interpreter = assertSuccess(//--- input code ---
-                DEFINE_BRACES + "\\color gray {.1}",
-                //--- log message ---
-                "");
+        Interpreter interpreter = assertSuccess(// --- input code ---
+            DEFINE_BRACES + "\\color gray {.1}",
+            // --- log message ---
+            "");
         assertNotNull(interpreter);
-        Color color = interpreter.getContext().getTypesettingContext()
-                .getColor();
+        Color color =
+                interpreter.getContext().getTypesettingContext().getColor();
         assertTrue(color instanceof GrayscaleColor);
         GrayscaleColor c = (GrayscaleColor) color;
-        assertEquals(6553, c.getGray());
+        assertEquals(TEN_PERCENT, c.getGray());
         assertEquals(0, c.getAlpha());
     }
 
     /**
-     * <testcase primitive="\color">
-     *  Test case checking that <tt>\color</tt> ...
+     * <testcase primitive="\color"> Test case checking that <tt>\color</tt>
+     * can consume an explicit gray scale color with an alpha channel.
      * </testcase>
-     *
+     * 
      * @throws Exception in case of an error
      */
     public void testGray2() throws Exception {
 
-        Interpreter interpreter = assertSuccess(//--- input code ---
-                DEFINE_BRACES + "\\color gray alpha .5 {.1}",
-                //--- log message ---
-                "");
+        Interpreter interpreter = assertSuccess(// --- input code ---
+            DEFINE_BRACES + "\\color gray alpha .5 {.1}",
+            // --- log message ---
+            "");
         assertNotNull(interpreter);
-        Color color = interpreter.getContext().getTypesettingContext()
-                .getColor();
+        Color color =
+                interpreter.getContext().getTypesettingContext().getColor();
         assertTrue(color instanceof GrayscaleColor);
         GrayscaleColor c = (GrayscaleColor) color;
-        assertEquals(6553, c.getGray());
-        assertEquals(0xffff / 2, c.getAlpha());
+        assertEquals(TEN_PERCENT, c.getGray());
+        assertEquals(FIFTY_PERCENT, c.getAlpha());
     }
 
     /**
-     * <testcase primitive="\color">
-     *  Test case checking that <tt>\color</tt> ...
-     * </testcase>
-     *
+     * <testcase primitive="\color"> Test case checking that <tt>\color</tt>
+     * can consume an alpha value with an explicit gray scale color. </testcase>
+     * 
      * @throws Exception in case of an error
      */
     public void testGray3() throws Exception {
 
-        Interpreter interpreter = assertSuccess(//--- input code ---
-                DEFINE_BRACES + "\\color alpha .5 gray {.1}",
-                //--- log message ---
-                "");
+        Interpreter interpreter = assertSuccess(// --- input code ---
+            DEFINE_BRACES + "\\color alpha .5 gray {.1}",
+            // --- log message ---
+            "");
         assertNotNull(interpreter);
-        Color color = interpreter.getContext().getTypesettingContext()
-                .getColor();
+        Color color =
+                interpreter.getContext().getTypesettingContext().getColor();
         assertTrue(color instanceof GrayscaleColor);
         GrayscaleColor c = (GrayscaleColor) color;
-        assertEquals(6553, c.getGray());
-        assertEquals(0xffff / 2, c.getAlpha());
+        assertEquals(TEN_PERCENT, c.getGray());
+        assertEquals(FIFTY_PERCENT, c.getAlpha());
     }
 
 }
--- ExTeX-Unit-color/src/test/java/org/extex/unit/color/ColordefTest.java
+++ ExTeX-Unit-color/src/test/java/org/extex/unit/color/ColordefTest.java
@@ -23,7 +23,7 @@ import org.extex.test.NoFlagsButGlobalPrimitiveTester;
 
 /**
  * This is a test suite for the primitive <tt>\colordef</tt>.
- *
+ * 
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
  * @version $Revision$
  */
@@ -31,7 +31,7 @@ public class ColordefTest extends NoFlagsButGlobalPrimitiveTester {
 
     /**
      * Method for running the tests standalone.
-     *
+     * 
      * @param args command line parameter
      */
     public static void main(String[] args) {
@@ -41,7 +41,7 @@ public class ColordefTest extends NoFlagsButGlobalPrimitiveTester {
 
     /**
      * Creates a new object.
-     *
+     * 
      * @param arg the name
      */
     public ColordefTest(String arg) {
@@ -51,169 +51,181 @@ public class ColordefTest extends NoFlagsButGlobalPrimitiveTester {
     }
 
     /**
-     * <testcase primitive="colordef">
-     *  Test case checking that <tt>\colordef</tt> ...
-     * </testcase>
-     *
+     * <testcase primitive="colordef"> Test case checking that
+     * <tt>\colordef</tt> neds an argument. </testcase>
+     * 
      * @throws Exception in case of an error
      */
-    public void testError1() throws Exception {
+    public void testError0() throws Exception {
 
-        assertFailure(//--- input code ---
-            DEFINE_BRACES + "\\colordef rgb",
-            //--- log message ---
+        assertFailure(// --- input code ---
+            "\\colordef",
+            // --- log message ---
             "Missing control sequence inserted");
     }
 
     /**
-     * <testcase primitive="colordef">
-     *  Test case checking that <tt>\colordef</tt> ...
+     * <testcase primitive="colordef"> Test case checking that
+     * <tt>\colordef</tt> needs a control sequence as first argument.
      * </testcase>
-     *
+     * 
+     * @throws Exception in case of an error
+     */
+    public void testError1() throws Exception {
+
+        assertFailure(// --- input code ---
+            "\\colordef rgb",
+            // --- log message ---
+            "Missing control sequence inserted");
+    }
+
+    /**
+     * <testcase primitive="colordef"> Test case checking that
+     * <tt>\colordef</tt> needs a left brace after the control seqeunce.
+     * </testcase>
+     * 
      * @throws Exception in case of an error
      */
     public void testError2() throws Exception {
 
-        assertFailure(//--- input code ---
-            DEFINE_BRACES + "\\colordef\\x undef ",
-            //--- log message ---
+        assertFailure(// --- input code ---
+            "\\colordef\\x undef ",
+            // --- log message ---
             "Missing left brace for color value");
     }
 
     /**
-     * <testcase primitive="colordef">
-     *  Test case checking that <tt>\colordef</tt> ...
-     * </testcase>
-     *
+     * <testcase primitive="colordef"> Test case checking that
+     * <tt>\colordef</tt> needs enough numbers in the braces. </testcase>
+     * 
      * @throws Exception in case of an error
      */
     public void testError3() throws Exception {
 
-        assertFailure(//--- input code ---
+        assertFailure(// --- input code ---
             DEFINE_BRACES + "\\colordef\\x {1} ",
-            //--- log message ---
+            // --- log message ---
             "Missing number, treated as zero");
     }
 
     /**
-     * <testcase primitive="colordef">
-     *  Test case checking that <tt>\colordef</tt> ...
-     * </testcase>
-     *
+     * <testcase primitive="colordef"> Test case checking that
+     * <tt>\colordef</tt> complains about too large values for the color
+     * components. </testcase>
+     * 
      * @throws Exception in case of an error
      */
     public void testError4() throws Exception {
 
-        assertFailure(//--- input code ---
+        assertFailure(// --- input code ---
             DEFINE_BRACES + "\\colordef\\x {1 2 3} ",
-            //--- log message ---
+            // --- log message ---
             "Illegal color value");
     }
 
     /**
-     * <testcase primitive="colordef">
-     *  Test case checking that <tt>\colordef</tt> ...
+     * <testcase primitive="colordef"> Test case checking that
+     * <tt>\colordef</tt> produces a control sequence which is showable.
      * </testcase>
-     *
+     * 
      * @throws Exception in case of an error
      */
     public void test1() throws Exception {
 
-        assertFailure(//--- input code ---
+        assertFailure(// --- input code ---
             DEFINE_BRACES + "\\colordef\\x {.1 .2 .3} " + "\\showthe\\x \\end",
-            //--- log message ---
+            // --- log message ---
             "> rgb {0.09999237 0.19998474 0.29999238}.\n");
     }
 
     /**
-     * <testcase primitive="colordef">
-     *  Test case checking that <tt>\colordef</tt> ...
-     * </testcase>
-     *
+     * <testcase primitive="colordef"> Test case checking that
+     * <tt>\colordef</tt> can take as value a color variable. </testcase>
+     * 
      * @throws Exception in case of an error
      */
     public void test20() throws Exception {
 
-        assertFailure(//--- input code ---
+        assertFailure(// --- input code ---
             DEFINE_BRACES + "\\colordef\\x {.1 .2 .3} " + "\\colordef\\y\\x"
                     + "\\showthe\\y \\end",
-            //--- log message ---
+            // --- log message ---
             "> rgb {0.09999237 0.19998474 0.29999238}.\n");
     }
 
     /**
-     * <testcase primitive="colordef">
-     *  Test case checking that <tt>\colordef</tt> ...
-     * </testcase>
-     *
+     * <testcase primitive="colordef"> Test case checking that
+     * <tt>\colordef</tt> can take a value as color constant with an implicit
+     * RGB color model. </testcase>
+     * 
      * @throws Exception in case of an error
      */
     public void test30() throws Exception {
 
-        assertFailure(//--- input code ---
+        assertFailure(// --- input code ---
             DEFINE_BRACES + "\\colordef\\x {1 1 1} " + "\\showthe\\x \\end",
-            //--- log message ---
+            // --- log message ---
             "> rgb {1.0 1.0 1.0}.\n");
     }
 
     /**
-     * <testcase primitive="colordef">
-     *  Test case checking that <tt>\colordef</tt> ...
-     * </testcase>
-     *
+     * <testcase primitive="colordef"> Test case checking that
+     * <tt>\colordef</tt> can take a value as color constant with an explicit
+     * RGB color model. </testcase>
+     * 
      * @throws Exception in case of an error
      */
     public void test31() throws Exception {
 
-        assertFailure(//--- input code ---
+        assertFailure(// --- input code ---
             DEFINE_BRACES + "\\colordef\\x rgb {1 1 1} " + "\\showthe\\x \\end",
-            //--- log message ---
+            // --- log message ---
             "> rgb {1.0 1.0 1.0}.\n");
     }
 
     /**
-     * <testcase primitive="colordef">
-     *  Test case checking that <tt>\colordef</tt> ...
-     * </testcase>
-     *
+     * <testcase primitive="colordef"> Test case checking that
+     * <tt>\colordef</tt> can take a value as color constant with an explicit
+     * gray scale color model. </testcase>
+     * 
      * @throws Exception in case of an error
      */
     public void test32() throws Exception {
 
-        assertFailure(//--- input code ---
+        assertFailure(// --- input code ---
             DEFINE_BRACES + "\\colordef\\x gray {1} " + "\\showthe\\x \\end",
-            //--- log message ---
+            // --- log message ---
             "> gray {1.0}.\n");
     }
 
     /**
-     * <testcase primitive="colordef">
-     *  Test case checking that <tt>\colordef</tt> ...
-     * </testcase>
-     *
+     * <testcase primitive="colordef"> Test case checking that
+     * <tt>\colordef</tt> can take a value as color constant with an explicit
+     * HSV color model. </testcase>
+     * 
      * @throws Exception in case of an error
      */
     public void test33() throws Exception {
 
-        assertFailure(//--- input code ---
+        assertFailure(// --- input code ---
             DEFINE_BRACES + "\\colordef\\x hsv {1 1 1} " + "\\showthe\\x \\end",
-            //--- log message ---
+            // --- log message ---
             "> hsv {1.0 1.0 1.0}.\n");
     }
 
     /**
-     * <testcase primitive="colordef">
-     *  Test case checking that <tt>\colordef</tt> ...
-     * </testcase>
-     *
+     * <testcase primitive="colordef"> Test case checking that
+     * <tt>\colordef</tt> can take a value as color constant with an explicit
+     * CMYK color model. </testcase>
+     * 
      * @throws Exception in case of an error
      */
     public void test34() throws Exception {
 
-        assertFailure(//--- input code ---
+        assertFailure(// --- input code ---
             DEFINE_BRACES + "\\colordef\\x cmyk {1 1 1 1} "
                     + "\\showthe\\x \\end",
-            //--- log message ---
+            // --- log message ---
             "> cmyk {1.0 1.0 1.0 1.0}.\n");
     }
 
