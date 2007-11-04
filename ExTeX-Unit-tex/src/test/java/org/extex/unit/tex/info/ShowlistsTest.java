99783ff24685d156ca93967a6ce855788c439951
Gerd Neugebauer <gene@gerd-neugebauer.de>
2007-11-04 18:44:26 +0000

    test case added

--- ExTeX-Unit-tex/src/test/java/org/extex/unit/tex/info/ShowlistsTest.java
+++ ExTeX-Unit-tex/src/test/java/org/extex/unit/tex/info/ShowlistsTest.java
@@ -26,7 +26,7 @@ import org.junit.Test;
 
 /**
  * This is a test suite for the primitive <tt>\showlists</tt>.
- *
+ * 
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
  * @version $Revision$
  */
@@ -41,53 +41,8 @@ public class ShowlistsTest extends NoFlagsPrimitiveTester {
     }
 
     /**
-     * <testcase primitive="\showlists">
-     *  Test case checking that the <tt>\showlists</tt> ...
-     * </testcase>
-     *
-     * @throws Exception in case of an error
-     */
-    @Test
-    public void test1() throws Exception {
-
-        Properties p = prepare();
-
-        assertFailure(p, //--- input code ---
-                "\\showlists\\end ",
-                //--- error channel ---
-                "### vertical mode entered at line 0\n" + "prevdepth ignored\n"
-                + "Transcript written on ."
-                + System.getProperty("file.separator") + "texput.log.\n");
-    }
-
-    /**
-     * <testcase primitive="\showlists">
-     *  Test case checking that the <tt>\showlists</tt> ...
-     * </testcase>
-     *
-     * @throws Exception in case of an error
-     */
-    @Test
-    public void test2() throws Exception {
-
-        Properties p = prepare();
-
-        assertOutput(p, //--- input code ---
-                DEFINE_BRACES + "\\hbox{\\showlists}\\end ",
-                //--- error channel ---
-                "### restricted horizontal mode entered at line 1\n"
-                + "spacefactor 1000\n"
-                + "### vertical mode entered at line 0\n"
-                + "prevdepth ignored\n"
-                + "Transcript written on ."
-                + System.getProperty("file.separator") + "texput.log.\n",
-                //
-                "" + TERM);
-    }
-
-    /**
      * Prepare the properties to use a fine log level.
-     *
+     * 
      * @return the properties to use
      */
     private Properties prepare() {
@@ -95,6 +50,72 @@ public class ShowlistsTest extends NoFlagsPrimitiveTester {
         Properties p = getProps();
         p.setProperty("extex.launcher.loglevel", "fine");
         return p;
+    }
+
+    /**
+     * <testcase primitive="\showlists"> Test case checking that
+     * <tt>\showlists</tt> on the top level brings the expected results.
+     * </testcase>
+     * 
+     * @throws Exception in case of an error
+     */
+    @Test
+    public void test1() throws Exception {
+
+        Properties p = prepare();
+
+        assertOutput(
+            p, // --- input code ---
+            "\\showlists\\end ",
+            // --- error channel ---
+            "### vertical mode entered at line 0\n" + "prevdepth ignored\n"
+                    + "Transcript written on ."
+                    + System.getProperty("file.separator") + "texput.log.\n",
+            "");
+    }
+
+    /**
+     * <testcase primitive="\showlists"> Test case checking that
+     * <tt>\showlists</tt> in a groupworks. </testcase>
+     * 
+     * @throws Exception in case of an error
+     */
+    @Test
+    public void test2() throws Exception {
+
+        Properties p = prepare();
+
+        assertOutput(p, // --- input code ---
+            DEFINE_BRACES + "{\\showlists}\\end ",
+            // --- error channel ---
+            "### vertical mode entered at line 0\n" + "prevdepth ignored\n"
+                    + "Transcript written on ."
+                    + System.getProperty("file.separator") + "texput.log.\n",
+            //
+            "");
+    }
+
+    /**
+     * <testcase primitive="\showlists"> Test case checking that
+     * <tt>\showlists</tt> in an hbox works. </testcase>
+     * 
+     * @throws Exception in case of an error
+     */
+    @Test
+    public void test3() throws Exception {
+
+        Properties p = prepare();
+
+        assertOutput(p, // --- input code ---
+            DEFINE_BRACES + "\\hbox{\\showlists}\\end ",
+            // --- error channel ---
+            "### restricted horizontal mode entered at line 1\n"
+                    + "spacefactor 1000\n"
+                    + "### vertical mode entered at line 0\n"
+                    + "prevdepth ignored\n" + "Transcript written on ."
+                    + System.getProperty("file.separator") + "texput.log.\n",
+            //
+            "" + TERM);
     }
 
 }
