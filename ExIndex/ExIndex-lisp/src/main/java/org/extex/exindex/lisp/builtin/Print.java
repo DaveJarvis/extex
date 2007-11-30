effce70f3912849739b4b66e737963a654fdf9ba
Gerd Neugebauer <gene@gerd-neugebauer.de>
2007-11-30 17:40:32 +0000

    new

---
+++ ExIndex/ExIndex-lisp/src/main/java/org/extex/exindex/lisp/builtin/Print.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (C) 2007 The ExTeX Group and individual authors listed below
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package org.extex.exindex.lisp.builtin;
+
+import org.extex.exindex.lisp.LInterpreter;
+import org.extex.exindex.lisp.exception.LException;
+import org.extex.exindex.lisp.type.function.Arg;
+import org.extex.exindex.lisp.type.function.LFunction;
+import org.extex.exindex.lisp.type.value.LValue;
+
+/**
+ * Print the argument.
+ * 
+ * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
+ * @version $Revision$
+ */
+public class Print extends LFunction {
+
+    /**
+     * Creates a new object.
+     * 
+     * @param name the name of the function
+     * 
+     * @throws NoSuchMethodException in case that no method corresponding to the
+     *         argument specification could be found
+     * @throws SecurityException in case a security problem occurred
+     */
+    public Print(String name) throws SecurityException, NoSuchMethodException {
+
+        super(name, new Arg[]{Arg.VALUE});
+    }
+
+    /**
+     * Perform the function.
+     * 
+     * @param interpreter the interpreter
+     * @param arg the argument to print
+     * 
+     * @return the last item read or <code>nil</code>
+     * 
+     * @throws LException in case of an error
+     */
+    public LValue evaluate(LInterpreter interpreter, LValue arg)
+            throws LException {
+
+        arg.print(System.out);
+        return null;
+    }
+}
---
+++ ExIndex/ExIndex-lisp/src/main/java/org/extex/exindex/lisp/exception/LSettingConstantException.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2007 The ExTeX Group and individual authors listed below
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package org.extex.exindex.lisp.exception;
+
+import org.extex.exindex.lisp.type.value.LSymbol;
+
+/**
+ * This exception signals that an attempt has been made to set the value of a
+ * constant.
+ * 
+ * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
+ * @version $Revision$
+ */
+public class LSettingConstantException extends LException {
+
+    /**
+     * The field <tt>serialVersionUID</tt> contains the version number for
+     * serialization.
+     */
+    private static final long serialVersionUID = 2007L;
+
+    /**
+     * Creates a new object.
+     * 
+     * @param resource the name of the constant
+     */
+    public LSettingConstantException(LSymbol resource) {
+
+        super(resource.toString());
+    }
+
+}
