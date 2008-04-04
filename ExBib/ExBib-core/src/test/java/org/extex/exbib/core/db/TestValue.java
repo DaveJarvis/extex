1436ce867bf2c7a45abd26659103f3d1703964fb
Gerd Neugebauer <gene@gerd-neugebauer.de>
2008-04-04 18:19:27 +0000

    converted to JUnit 4

--- ExBib/ExBib-core/src/test/java/org/extex/exbib/core/db/TestValue.java
+++ ExBib/ExBib-core/src/test/java/org/extex/exbib/core/db/TestValue.java
@@ -1,57 +1,38 @@
 /*
- * Copyright (C) 2003-2008 Gerd Neugebauer
+ * Copyright (C) 2003-2008 The ExTeX Group and individual authors listed below
  * This file is part of ExBib a BibTeX compatible database.
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
  *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  *
  */
 
 package org.extex.exbib.core.db;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.assertEquals;
 
 import org.extex.exbib.core.db.impl.DBImpl;
+import org.junit.Before;
+import org.junit.Test;
 
 /**
+ * Test suite for {@link Value}.
  * 
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
  * @version $Revision: 1.1 $
  */
-public class TestValue extends TestCase {
-
-    /**
-     * The main program just uses the text interface of JUnit.
-     * 
-     * @param args command line parameters are ignored
-     */
-    public static void main(String[] args) {
-
-        junit.textui.TestRunner.run(suite());
-    }
-
-    /**
-     * Generate a new test suite
-     * 
-     * @return the new test suite
-     */
-    public static Test suite() {
-
-        return new TestSuite(TestValue.class);
-    }
+public class TestValue {
 
     /**
      * The field <tt>db</tt> contains the database.
@@ -60,20 +41,18 @@ public class TestValue extends TestCase {
 
     /**
      * Create a new object.
-     * 
-     * @param name the name
      */
-    public TestValue(String name) {
+    public TestValue() {
 
-        super(name);
+        super();
     }
 
     /**
-     * {@inheritDoc}
+     * Set-up.
      * 
-     * @see junit.framework.TestCase#setUp()
+     * @throws Exception in case of an error
      */
-    @Override
+    @Before
     public void setUp() throws Exception {
 
         db = new DBImpl();
@@ -87,6 +66,7 @@ public class TestValue extends TestCase {
      * 
      * @throws Exception in case of an error
      */
+    @Test
     public void testExpand0() throws Exception {
 
         assertEquals("", new Value().expand(db));
@@ -97,6 +77,7 @@ public class TestValue extends TestCase {
      * 
      * @throws Exception in case of an error
      */
+    @Test
     public void testExpand1() throws Exception {
 
         assertEquals("January", new Value(new VMacro("jan")).expand(db));
@@ -107,10 +88,12 @@ public class TestValue extends TestCase {
      * 
      * @throws Exception in case of an error
      */
+    @Test
     public void testExpand2() throws Exception {
 
         Value v = new Value(new VMacro("jan"));
         v.add(new VString(" 21st"));
         assertEquals("January 21st", v.expand(db));
     }
+
 }
