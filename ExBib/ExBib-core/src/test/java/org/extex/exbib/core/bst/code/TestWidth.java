54c087e1c1caa22b87d24eba78823922a59b1402
Gerd Neugebauer <gene@gerd-neugebauer.de>
2008-04-06 21:29:54 +0000

    test cases added

--- ExBib/ExBib-core/src/test/java/org/extex/exbib/core/bst/code/TestWidth.java
+++ ExBib/ExBib-core/src/test/java/org/extex/exbib/core/bst/code/TestWidth.java
@@ -1,20 +1,19 @@
 /*
- * Copyright (C) 2003-2008 Gerd Neugebauer
- * This file is part of ExBib a BibTeX compatible database.
+ * Copyright (C) 2003-2008 The ExTeX Group and individual authors listed below
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
  *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  *
  */
 
@@ -195,13 +194,33 @@ public class TestWidth extends TestCase {
     }
 
     /**
-     * <testcase> Test "OXstoc83b". </testcase>
+     * <testcase> Test "OX{\\singleletter{stoc}}83b". </testcase>
      * 
      * @throws Exception in case of an error
      */
     public void testWidth0117() throws Exception {
 
+        testWidth("OX{\\singleletter  {stoc}}83b", 4811);
+    }
+
+    /**
+     * <testcase> Test "OXstoc83b". </testcase>
+     * 
+     * @throws Exception in case of an error
+     */
+    public void testWidth0118() throws Exception {
+
         testWidth("OXstoc83b", 4811);
+    }
+
+    /**
+     * <testcase> Test "\AE". </testcase>
+     * 
+     * @throws Exception in case of an error
+     */
+    public void testWidth0119() throws Exception {
+
+        testWidth("{\\AE}", 903);
     }
 
     /**
