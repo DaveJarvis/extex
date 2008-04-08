b3224f115ec9a1169a7fd3adced2e9ad7fbe1450
Gerd Neugebauer <gene@gerd-neugebauer.de>
2008-04-08 20:50:22 +0000

    test cases added

--- ExBib/ExBib-core/src/test/java/org/extex/exbib/core/bst/code/TestIf.java
+++ ExBib/ExBib-core/src/test/java/org/extex/exbib/core/bst/code/TestIf.java
@@ -1,20 +1,19 @@
 /*
- * This file is part of ExBib a BibTeX compatible database.
- * Copyright (C) 2003-2008 Gerd Neugebauer
+ * Copyright (C) 2003-2008 The ExTeX Group and individual authors listed below
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
  *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  *
  */
 
@@ -28,12 +27,15 @@ import org.extex.exbib.core.bst.Processor;
 import org.extex.exbib.core.bst.Processor099Impl;
 import org.extex.exbib.core.bst.code.impl.If;
 import org.extex.exbib.core.bst.exception.ExBibStackEmptyException;
+import org.extex.exbib.core.bst.node.impl.TChar;
 import org.extex.exbib.core.bst.node.impl.TInteger;
 import org.extex.exbib.core.bst.node.impl.TLiteral;
+import org.extex.exbib.core.bst.node.impl.TString;
 import org.extex.exbib.core.db.DB;
 import org.extex.exbib.core.db.Entry;
 import org.extex.exbib.core.db.impl.DBImpl;
 import org.extex.exbib.core.exceptions.ExBibException;
+import org.extex.exbib.core.exceptions.ExBibMissingNumberException;
 import org.extex.exbib.core.io.Locator;
 import org.extex.exbib.core.io.NullWriter;
 
@@ -46,14 +48,15 @@ import org.extex.exbib.core.io.NullWriter;
 public class TestIf extends TestCase {
 
     /**
-     * TODO gene: missing JavaDoc.
+     * Test case emulating the else branch.
      */
     private class ElseCode extends AbstractCode {
 
         /**
          * {@inheritDoc}
          * 
-         * @see org.extex.exbib.core.bst.code.AbstractCode#execute(org.extex.exbib.core.bst.Processor,
+         * @see org.extex.exbib.core.bst.code.AbstractCode#execute(
+         *      org.extex.exbib.core.bst.Processor,
          *      org.extex.exbib.core.db.Entry, org.extex.exbib.core.io.Locator)
          */
         @Override
@@ -65,14 +68,15 @@ public class TestIf extends TestCase {
     }
 
     /**
-     * TODO gene: missing JavaDoc.
+     * Test case emulating the then branch.
      */
     private class ThenCode extends AbstractCode {
 
         /**
          * {@inheritDoc}
          * 
-         * @see org.extex.exbib.core.bst.code.AbstractCode#execute(org.extex.exbib.core.bst.Processor,
+         * @see org.extex.exbib.core.bst.code.AbstractCode#execute(
+         *      org.extex.exbib.core.bst.Processor,
          *      org.extex.exbib.core.db.Entry, org.extex.exbib.core.io.Locator)
          */
         @Override
@@ -104,14 +108,14 @@ public class TestIf extends TestCase {
     }
 
     /**
-     * The field <tt>thenCount</tt> contains the ...
+     * The field <tt>thenCount</tt> contains the counter for the then branch.
      */
-    protected int thenCount = 0;
+    private int thenCount = 0;
 
     /**
-     * The field <tt>elseCount</tt> contains the ...
+     * The field <tt>elseCount</tt> contains the counter for the else branch.
      */
-    protected int elseCount = 0;
+    private int elseCount = 0;
 
     /**
      * The field <tt>db</tt> contains the database.
@@ -160,13 +164,15 @@ public class TestIf extends TestCase {
     }
 
     /**
-     * TODO gene: missing JavaDoc
+     * <testcase> If the condition evaluates to 0 then only the else branch is
+     * executed. </testcase>
      * 
      * @throws Exception in case of an error
      */
     public void test0() throws Exception {
 
-        thenCount = elseCount = 0;
+        thenCount = 0;
+        elseCount = 0;
         p.push(new TInteger(0));
         p.push(new TLiteral("t"));
         p.push(new TLiteral("e"));
@@ -177,13 +183,15 @@ public class TestIf extends TestCase {
     }
 
     /**
-     * TODO gene: missing JavaDoc
+     * <testcase> If the condition evaluates to 1 then only the then branch is
+     * executed. </testcase>
      * 
      * @throws Exception in case of an error
      */
     public void test1() throws Exception {
 
-        thenCount = elseCount = 0;
+        thenCount = 0;
+        elseCount = 0;
         p.push(new TInteger(1));
         p.push(new TLiteral("t"));
         p.push(new TLiteral("e"));
@@ -194,7 +202,7 @@ public class TestIf extends TestCase {
     }
 
     /**
-     * TODO gene: missing JavaDoc
+     * <testcase> An empty stack leads to an error. </testcase>
      * 
      * @throws Exception in case of an error
      */
@@ -209,7 +217,7 @@ public class TestIf extends TestCase {
     }
 
     /**
-     * TODO gene: missing JavaDoc
+     * <testcase> A short stack leads to an error. </testcase>
      * 
      * @throws Exception in case of an error
      */
@@ -225,7 +233,7 @@ public class TestIf extends TestCase {
     }
 
     /**
-     * TODO gene: missing JavaDoc
+     * <testcase> A short stack leads to an error. </testcase>
      * 
      * @throws Exception in case of an error
      */
@@ -240,4 +248,41 @@ public class TestIf extends TestCase {
             assertTrue(true);
         }
     }
+
+    /**
+     * <testcase> The first argument needs to be a number. </testcase>
+     * 
+     * @throws Exception in case of an error
+     */
+    public void testTypeError1() throws Exception {
+
+        try {
+            p.push(new TString("xxx"));
+            p.push(new TLiteral("t"));
+            p.push(new TLiteral("e"));
+            new If("if$").execute(p, null, null);
+            assertTrue(false);
+        } catch (ExBibMissingNumberException e) {
+            assertTrue(true);
+        }
+    }
+
+    /**
+     * <testcase> The first argument needs to be a number. </testcase>
+     * 
+     * @throws Exception in case of an error
+     */
+    public void testTypeError2() throws Exception {
+
+        try {
+            p.push(new TChar("x", null));
+            p.push(new TLiteral("t"));
+            p.push(new TLiteral("e"));
+            new If("if$").execute(p, null, null);
+            assertTrue(false);
+        } catch (ExBibMissingNumberException e) {
+            assertTrue(true);
+        }
+    }
+
 }
