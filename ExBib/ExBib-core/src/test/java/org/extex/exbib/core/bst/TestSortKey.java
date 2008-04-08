960b7fc81576652258068fc9d6444b5eb517b15c
Gerd Neugebauer <gene@gerd-neugebauer.de>
2008-04-08 21:04:40 +0000

    documentation improved

--- ExBib/ExBib-core/src/test/java/org/extex/exbib/core/bst/TestSortKey.java
+++ ExBib/ExBib-core/src/test/java/org/extex/exbib/core/bst/TestSortKey.java
@@ -1,20 +1,19 @@
 /*
- * This file is part of ExBib a BibTeX compatible database.
- * Copyright (C) 2003-2008 Gerd Neugebauer
+ * Copyright (C) 2003-2008 The ExTeX Group and individual authors listed below
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
  *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  *
  */
 
@@ -31,6 +30,7 @@ import org.extex.exbib.core.db.impl.DBImpl;
 import org.extex.exbib.core.io.NullWriter;
 
 /**
+ * Test suite for sort.key$.
  * 
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
  * @version $Revision: 1.3 $
@@ -58,7 +58,7 @@ public class TestSortKey extends TestCase {
     }
 
     /**
-     * The field <tt>e</tt> contains the ...
+     * The field <tt>e</tt> contains the entry.
      */
     private Entry e;
 
@@ -103,9 +103,10 @@ public class TestSortKey extends TestCase {
     }
 
     /**
-     * TODO gene: missing JavaDoc
+     * <testcase> sort.key$ is empty if the sort key from the current entry is
+     * not set. </testcase>
      * 
-     * @throws Exception
+     * @throws Exception in case of an error
      */
     public void testNoSortKey() throws Exception {
 
@@ -115,9 +116,9 @@ public class TestSortKey extends TestCase {
     }
 
     /**
-     * TODO gene: missing JavaDoc
+     * <testcase> getter and setter coincide. </testcase>
      * 
-     * @throws Exception
+     * @throws Exception in case of an error
      */
     public void testSortKey1() throws Exception {
 
@@ -126,9 +127,10 @@ public class TestSortKey extends TestCase {
     }
 
     /**
-     * TODO gene: missing JavaDoc
+     * <testcase> sort.key$ extracts the sort key from the current entry.
+     * </testcase>
      * 
-     * @throws Exception
+     * @throws Exception in case of an error
      */
     public void testSortKey11() throws Exception {
 
