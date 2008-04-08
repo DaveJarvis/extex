49de4b5a83e30fa02d4f297d7f4e9b9a60a200ee
Gerd Neugebauer <gene@gerd-neugebauer.de>
2008-04-08 22:07:21 +0000

    documentation improved

--- ExBib/ExBib-core/src/test/java/org/extex/exbib/core/bst/code/TestEq.java
+++ ExBib/ExBib-core/src/test/java/org/extex/exbib/core/bst/code/TestEq.java
@@ -1,20 +1,19 @@
 /*
- * This file is part of ExBib a BibTeX compatible database.
- * Copyright (C) 2003-2008 Gerd Neugebauer
+ * Copyright (C) 2003-2008 The ExTeX Group and individual authors listed below
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
  *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  *
  */
 
@@ -41,6 +40,21 @@ import org.extex.exbib.core.io.NullWriter;
  * @version $Revision: 1.3 $
  */
 public class TestEq extends TestCase {
+
+    /**
+     * The field <tt>INT_MINUS_123</tt> contains -123.
+     */
+    private static final TInteger INT_MINUS_123 = new TInteger(-123);
+
+    /**
+     * The field <tt>INT_12</tt> contains 12.
+     */
+    private static final TInteger INT_12 = new TInteger(12);
+
+    /**
+     * The field <tt>INT_123</tt> contains 123.
+     */
+    private static final TInteger INT_123 = new TInteger(123);
 
     /**
      * The main program just uses the text interface of JUnit.
@@ -70,11 +84,29 @@ public class TestEq extends TestCase {
     /**
      * Create a new object.
      * 
-     * @param name
+     * @param name the name
      */
     public TestEq(String name) {
 
         super(name);
+    }
+
+    /**
+     * Run a test case.
+     * 
+     * @param t1 the first token
+     * @param t2 the second token
+     * @param result the expected result
+     * 
+     * @throws Exception in case of an error
+     */
+    private void runTest(Token t1, Token t2, String result) throws Exception {
+
+        p.push(t1);
+        p.push(t2);
+        new Eq("=").execute(p, null, null);
+        assertEquals(result, p.popInteger(null).getValue());
+        assertNull(p.popUnchecked());
     }
 
     /**
@@ -100,25 +132,9 @@ public class TestEq extends TestCase {
     }
 
     /**
-     * TODO gene: missing JavaDoc
+     * <testcase> An empty stack leads to an error. </testcase>
      * 
-     * @throws Exception
-     */
-    public void test1Stack() throws Exception {
-
-        try {
-            p.push(new TInteger(2));
-            new Eq("=").execute(p, null, null);
-            assertTrue(false);
-        } catch (ExBibStackEmptyException e) {
-            assertTrue(true);
-        }
-    }
-
-    /**
-     * TODO gene: missing JavaDoc
-     * 
-     * @throws Exception
+     * @throws Exception in case of an error
      */
     public void testEmptyStack() throws Exception {
 
@@ -131,170 +147,169 @@ public class TestEq extends TestCase {
     }
 
     /**
-     * TODO gene: missing JavaDoc
+     * <testcase> Identical numbers compare as equal. </testcase>
      * 
-     * @param t1
-     * @param t2
-     * @param result
-     * @throws Exception
+     * @throws Exception in case of an error
      */
-    private void testEq(Token t1, Token t2, String result) throws Exception {
+    public void testEqI1() throws Exception {
 
-        p.push(t1);
-        p.push(t2);
-        new Eq("=").execute(p, null, null);
-        assertEquals(result, p.popInteger(null).getValue());
-        assertNull(p.popUnchecked());
+        runTest(INT_123, INT_123, "1");
     }
 
     /**
-     * TODO gene: missing JavaDoc
+     * <testcase> Identical negative numbers compare as equal. </testcase>
      * 
-     * @throws Exception
+     * @throws Exception in case of an error
      */
-    public void testEQI1() throws Exception {
+    public void testEqI1n() throws Exception {
 
-        testEq(new TInteger(123), new TInteger(123), "1");
+        runTest(INT_MINUS_123, INT_MINUS_123, "1");
     }
 
     /**
-     * TODO gene: missing JavaDoc
+     * <testcase> Identical numbers (0) compare as equal. </testcase>
      * 
-     * @throws Exception
+     * @throws Exception in case of an error
      */
-    public void testEQI1n() throws Exception {
+    public void testEqI2() throws Exception {
 
-        testEq(new TInteger(-123), new TInteger(-123), "1");
+        runTest(new TInteger(0), new TInteger(0), "1");
     }
 
     /**
-     * TODO gene: missing JavaDoc
+     * <testcase> Different numbers compare as not equal. </testcase>
      * 
-     * @throws Exception
+     * @throws Exception in case of an error
      */
-    public void testEQI2() throws Exception {
+    public void testEqI3() throws Exception {
 
-        testEq(new TInteger(0), new TInteger(0), "1");
+        runTest(INT_12, INT_123, "0");
     }
 
     /**
-     * TODO gene: missing JavaDoc
+     * <testcase> Different numbers compare as not equal. </testcase>
      * 
-     * @throws Exception
+     * @throws Exception in case of an error
      */
-    public void testEQI3() throws Exception {
+    public void testEqI4() throws Exception {
 
-        testEq(new TInteger(12), new TInteger(123), "0");
+        runTest(INT_123, INT_12, "0");
     }
 
     /**
-     * TODO gene: missing JavaDoc
+     * <testcase> Equal numbers (-1) compare as equal. </testcase>
      * 
-     * @throws Exception
+     * @throws Exception in case of an error
      */
-    public void testEQI4() throws Exception {
+    public void testEqI5() throws Exception {
 
-        testEq(new TInteger(123), new TInteger(12), "0");
+        runTest(new TInteger(-1), new TInteger(-1), "1");
     }
 
     /**
-     * TODO gene: missing JavaDoc
+     * <testcase> Different numbers compare as not equal. </testcase>
      * 
-     * @throws Exception
+     * @throws Exception in case of an error
      */
-    public void testEQI5() throws Exception {
+    public void testEqI6() throws Exception {
 
-        testEq(new TInteger(-1), new TInteger(-1), "1");
+        runTest(INT_123, new TInteger(0), "0");
     }
 
     /**
-     * TODO gene: missing JavaDoc
+     * <testcase> Different numbers compare as not equal. </testcase>
      * 
-     * @throws Exception
+     * @throws Exception in case of an error
      */
-    public void testEQI6() throws Exception {
+    public void testEqI7() throws Exception {
 
-        testEq(new TInteger(123), new TInteger(0), "0");
+        runTest(new TInteger(1), new TInteger(0), "0");
     }
 
     /**
-     * TODO gene: missing JavaDoc
+     * <testcase> Identical strings compare as equal. </testcase>
      * 
-     * @throws Exception
-     */
-    public void testEQI7() throws Exception {
-
-        testEq(new TInteger(1), new TInteger(0), "0");
-    }
-
-    /**
-     * TODO gene: missing JavaDoc
-     * 
-     * @throws Exception
+     * @throws Exception in case of an error
      */
     public void testEqS1() throws Exception {
 
-        testEq(new TString("abc"), new TString("abc"), "1");
+        runTest(new TString("abc"), new TString("abc"), "1");
     }
 
     /**
-     * TODO gene: missing JavaDoc
+     * <testcase> Identical empty strings compare as equal. </testcase>
      * 
-     * @throws Exception
+     * @throws Exception in case of an error
      */
     public void testEqS2() throws Exception {
 
-        testEq(new TString(""), new TString(""), "1");
+        runTest(new TString(""), new TString(""), "1");
     }
 
     /**
-     * TODO gene: missing JavaDoc
+     * <testcase> Different strings compare as not equal. </testcase>
      * 
-     * @throws Exception
+     * @throws Exception in case of an error
      */
     public void testEqS3() throws Exception {
 
-        testEq(new TString("ab"), new TString("abc"), "0");
+        runTest(new TString("ab"), new TString("abc"), "0");
     }
 
     /**
-     * TODO gene: missing JavaDoc
+     * <testcase> Different strings compare as not equal. </testcase>
      * 
-     * @throws Exception
+     * @throws Exception in case of an error
      */
     public void testEqS4() throws Exception {
 
-        testEq(new TString("abc"), new TString("ab"), "0");
+        runTest(new TString("abc"), new TString("ab"), "0");
     }
 
     /**
-     * TODO gene: missing JavaDoc
+     * <testcase> Different strings compare as not equal. </testcase>
      * 
-     * @throws Exception
+     * @throws Exception in case of an error
      */
     public void testEqS5() throws Exception {
 
-        testEq(new TString("{}"), new TString(""), "0");
+        runTest(new TString("{}"), new TString(""), "0");
     }
 
     /**
-     * TODO gene: missing JavaDoc
+     * <testcase> Different strings compare as not equal. </testcase>
      * 
-     * @throws Exception
+     * @throws Exception in case of an error
      */
     public void testEqS6() throws Exception {
 
-        testEq(new TString("abc"), new TString(""), "0");
+        runTest(new TString("abc"), new TString(""), "0");
     }
 
     /**
-     * TODO gene: missing JavaDoc
+     * <testcase> Different strings compare as not equal. </testcase>
      * 
-     * @throws Exception
+     * @throws Exception in case of an error
      */
     public void testEqS7() throws Exception {
 
-        testEq(new TString("a"), new TString(""), "0");
+        runTest(new TString("a"), new TString(""), "0");
+    }
+
+    /**
+     * <testcase> A short stack leads to an error. </testcase>
+     * 
+     * @throws Exception in case of an error
+     */
+    public void testShortStack() throws Exception {
+
+        try {
+            p.push(new TInteger(2));
+            new Eq("=").execute(p, null, null);
+            assertTrue(false);
+        } catch (ExBibStackEmptyException e) {
+            assertTrue(true);
+        }
     }
 
 }
