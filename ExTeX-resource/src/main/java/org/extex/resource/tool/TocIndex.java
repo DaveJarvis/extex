a7f40d3c7761524ad88003983e72d5ec4b191329
Gerd Neugebauer <gene@gerd-neugebauer.de>
2008-06-08 19:35:22 +0000

    TocIndex improved and test cases added

--- ExTeX-resource/src/main/java/org/extex/resource/tool/TocIndex.java
+++ ExTeX-resource/src/main/java/org/extex/resource/tool/TocIndex.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2007 The ExTeX Group and individual authors listed below
+ * Copyright (C) 2007-2008 The ExTeX Group and individual authors listed below
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
@@ -23,10 +23,13 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.PrintStream;
-import java.net.MalformedURLException;
+import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.ResourceBundle;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
 
 /**
  * Traverse a directory tree and collect the files in an index.
@@ -37,65 +40,57 @@ import java.util.List;
 public class TocIndex {
 
     /**
+     * The main program. The command line parameters are evaluated and the
+     * appropriate actions are performed. Exceptions are caught and reported to
+     * stderr.
+     * 
+     * @param argv list of command line parameters
+     * 
+     * @return the exit code
+     */
+    public static int commandLine(String[] argv) {
+
+        try {
+            new TocIndex().processCommandLine(argv);
+        } catch (Exception e) {
+            System.err.println(e.getLocalizedMessage());
+            return -1;
+        }
+        return 0;
+    }
+
+    /**
      * Command line interface.
      * 
      * @param args the command line arguments
      */
     public static void main(String[] args) {
 
-        PrintStream err = System.err;
-        PrintStream out = System.out;
-        TocIndex tocIndex = new TocIndex();
-        File base = null;
-
-        for (int i = 0; i < args.length; i++) {
-            String arg = args[i];
-            if (!arg.startsWith("-")) {
-                if (base != null) {
-                    err.println("Too many bases");
-                    System.exit(-1);
-                }
-                base = new File(arg);
-            } else if ("".equals(arg)) {
-                // silently ignored
-            } else if ("-".equals(arg)) {
-                // silently ignored
-            } else if (arg.startsWith("-omit=")) {
-                tocIndex.omit(arg.substring(6));
-            } else if ("-output".startsWith(arg)) {
-                if (i < args.length) {
-                    arg = args[++i];
-                    try {
-                        out = new PrintStream(new FileOutputStream(arg));
-                    } catch (FileNotFoundException e) {
-                        err.print(arg);
-                        err.print(" ");
-                        err.println(e.getLocalizedMessage());
-                    }
-                }
-            } else {
-                err.println("Unknown argument: " + arg);
-                System.exit(-1);
-            }
-        }
-
-        try {
-            out.println("#");
-            out.print("# Created ");
-            out.println(new Date().toString());
-            out.println("#");
-            tocIndex.collect(base != null ? base : new File("."), out);
-        } catch (MalformedURLException e) {
-            e.printStackTrace(err);
-            System.exit(-1);
-        }
-        System.exit(0);
+        System.exit(commandLine(args));
     }
 
     /**
      * The field <tt>omit</tt> contains the omit patterns.
      */
-    private List<String> omit = new ArrayList<String>();
+    private List<Pattern> omit = new ArrayList<Pattern>();
+
+    /**
+     * The field <tt>base</tt> contains the base directory.
+     */
+    private File base = null;
+
+    /**
+     * The field <tt>outFile</tt> contains the name of the output file.
+     */
+    private String outFile = null;
+
+    /**
+     * Creates a new object.
+     */
+    public TocIndex() {
+
+        super();
+    }
 
     /**
      * Recursive procedure to traverse a directory tree and collect the files
@@ -103,42 +98,174 @@ public class TocIndex {
      * 
      * @param file the current file to consider
      * @param out the output stream
-     * 
-     * @throws MalformedURLException just in case
+     * @param strip
      */
-    private void collect(File file, PrintStream out)
-            throws MalformedURLException {
+    private void collect(File file, PrintStream out, int strip) {
 
-        String f = file.toURL().getFile();
-        f = f.substring(f.indexOf("/./") + 3);
+        String f = file.toString().substring(strip).replace('\\', '/');
 
-        int n = omit.size();
-        for (int i = 0; i < n; i++) {
-            if (f.matches(omit.get(i))) {
+        for (Pattern p : omit) {
+            if (p.matcher(f).matches()) {
                 return;
             }
         }
 
         if (file.isDirectory()) {
-            File[] list = file.listFiles();
-            for (int i = 0; i < list.length; i++) {
-                collect(list[i], out);
+            for (File fi : file.listFiles()) {
+                collect(fi, out, strip);
             }
         } else if (file.isFile()) {
-            out.print(file.getName());
+            printQuoted(out, file.getName());
             out.print("=");
-            out.println(f);
+            printQuoted(out, f);
+            out.println();
         }
+    }
+
+    /**
+     * Get a message for an exception.
+     * 
+     * @param tag the tag
+     * @param args the arguments for the positional parameters
+     * 
+     * @return the formatted string
+     */
+    private String getMessage(String tag, Object... args) {
+
+        ResourceBundle bundle = ResourceBundle.getBundle(getClass().getName());
+        return MessageFormat.format(bundle.getString(tag), args);
     }
 
     /**
      * Add a pattern to the omit patterns.
      * 
      * @param arg the pattern to omit
+     * 
+     * @throws PatternSyntaxException in case of an error in the regular
+     *         expression
      */
-    private void omit(String arg) {
+    private void omit(String arg) throws PatternSyntaxException {
 
-        omit.add(arg);
+        omit.add(Pattern.compile(arg));
+    }
+
+    /**
+     * Print a string with quoted characters for properties files.
+     * 
+     * @param out the print stream
+     * @param s the string to print
+     */
+    private void printQuoted(PrintStream out, String s) {
+
+        out.print(s.replaceAll("\\([#=:\\\\]\\)", "\\$1"));
+    }
+
+    /**
+     * Process the list of string as command line parameters.
+     * 
+     * @param argv the command line parameters
+     * 
+     * @throws Exception in case of an error
+     */
+    public void processCommandLine(String[] argv) throws Exception {
+
+        for (int i = 0; i < argv.length; i++) {
+            String arg = argv[i];
+            if ("".equals(arg)) {
+                // silently ignored
+            } else if (!arg.startsWith("-")) {
+                setBase(new File(arg));
+            } else if ("-".equals(arg) || "--".equals(arg)) {
+                if (++i >= argv.length) {
+                    throw new Exception(getMessage("MissingArgument", arg));
+                }
+                setBase(new File(argv[i]));
+            } else if (arg.startsWith("-omit=") || arg.startsWith("--omit=")) {
+                try {
+                    omit(arg.substring(6));
+                } catch (PatternSyntaxException e) {
+                    throw new Exception(getMessage("PatternProblem", arg
+                        .substring(6)));
+                }
+            } else if ("-output".startsWith(arg) || "--output".startsWith(arg)) {
+                setOutFile(++i >= argv.length ? null : argv[i]);
+            } else if (arg.startsWith("-output=")) {
+                setOutFile(arg.substring(8));
+            } else if (arg.startsWith("--output=")) {
+                setOutFile(arg.substring(9));
+            } else if ("-help".startsWith(arg) || "--help".startsWith(arg)) {
+                System.err.println(getMessage("usage"));
+                return;
+            } else {
+                throw new Exception(getMessage("UnknownArgument", arg));
+            }
+        }
+
+        run();
+    }
+
+    /**
+     * Initialize the creation of the index and perform the creation itself.
+     * 
+     * @throws Exception in case of an error
+     */
+    public void run() throws Exception {
+
+        if (base != null && !base.isDirectory()) {
+            throw new Exception(getMessage("IllegalBase", base));
+        }
+
+        if ("".equals(outFile)) {
+            outFile = null;
+        }
+        PrintStream out;
+        try {
+            out = (outFile == null //
+                    ? System.out
+                    : new PrintStream(new FileOutputStream(outFile)));
+        } catch (FileNotFoundException e) {
+            throw new Exception(getMessage("OutFileNotFound", outFile));
+        }
+        try {
+            out.println("#");
+            out.print("# Created ");
+            out.println(new Date().toString());
+            out.println("#");
+            File file = base != null ? base : new File(".");
+            collect(file, out, file.toString().length());
+        } finally {
+            if (outFile != null) {
+                out.close();
+            }
+        }
+    }
+
+    /**
+     * Setter for base.
+     * 
+     * @param base the base to set
+     */
+    public void setBase(File base) {
+
+        if (this.base != null) {
+            throw new RuntimeException(getMessage("TooManyBases"));
+        }
+        this.base = base;
+    }
+
+    /**
+     * Setter for outFile.
+     * 
+     * @param outFile the outFile to set
+     * 
+     * @throws RuntimeException in case the argument is <code>null</code>
+     */
+    public void setOutFile(String outFile) throws RuntimeException {
+
+        if (outFile == null) {
+            throw new RuntimeException(getMessage("MissingArgument", "-output"));
+        }
+        this.outFile = outFile;
     }
 
 }
---
+++ ExTeX-resource/src/test/java/org/extex/resource/tool/TocIndexTest.java
@@ -0,0 +1,302 @@
+/*
+ * Copyright (C) 2008 The ExTeX Group and individual authors listed below
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package org.extex.resource.tool;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.PrintStream;
+import java.util.Locale;
+
+import org.junit.Test;
+
+/**
+ * This is a test suite for {@link TocIndex}.
+ * 
+ * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
+ * @version $Revision$
+ */
+public class TocIndexTest {
+
+    /**
+     * Run a test and compare the result.
+     * 
+     * @param expectedOut the expected output
+     * @param expectedErr the expected error
+     * @param args the command line arguments
+     */
+    private void runTest(String expectedOut, String expectedErr, String... args) {
+
+        PrintStream err = System.err;
+        ByteArrayOutputStream errStream = new ByteArrayOutputStream();
+        PrintStream out = System.out;
+        ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+        try {
+            System.setOut(new PrintStream(outStream));
+            System.setErr(new PrintStream(errStream));
+            TocIndex.commandLine(args);
+        } finally {
+            System.err.close();
+            System.setErr(err);
+            System.out.close();
+            System.setOut(out);
+        }
+        if (expectedErr != null) {
+            assertEquals(expectedErr, errStream.toString().replaceAll("\r", ""));
+        }
+        if ("".equals(expectedOut)) {
+            String s = outStream.toString().replaceAll("\r", "");
+            assertEquals(expectedOut, s.replaceAll("\r", ""));
+        } else if (expectedOut != null) {
+            String s = outStream.toString().replaceAll("\r", "");
+            int i = s.indexOf('\n');
+            assertTrue(i >= 0);
+            assertEquals("#", s.substring(0, i));
+            s = s.substring(i + 1);
+
+            i = s.indexOf('\n');
+            assertTrue(i >= 0);
+            assertEquals("# Created ", s.substring(0, 10));
+            s = s.substring(i + 1);
+
+            assertEquals(expectedOut, s.replaceAll("\r", ""));
+        }
+    }
+
+    /**
+     * Run a test with the English locale and compare the result.
+     * 
+     * @param expectedOut the expected output
+     * @param expectedErr the expected error
+     * @param args the command line arguments
+     */
+    private void runTestEn(String expectedOut, String expectedErr,
+            String... args) {
+
+        Locale.setDefault(Locale.ENGLISH);
+        runTest(expectedOut, expectedErr, args);
+    }
+
+    /**
+     * <testcase>Test that a empty directory is correctly indexed. </testcase>
+     */
+    @Test
+    public final void test1() {
+
+        File dir = new File("target/empty");
+        dir.delete();
+        dir.mkdir();
+        runTestEn("#\n", "", "target/empty");
+    }
+
+    /**
+     * <testcase>Test that an unknown command line argument is reported.
+     * </testcase>
+     */
+    @Test
+    public final void testError01() {
+
+        runTestEn("", "Unknown argument `--undef'\n", "--undef");
+    }
+
+    /**
+     * <testcase>Test that too many base directories are reported. </testcase>
+     */
+    @Test
+    public final void testError02() {
+
+        runTestEn("", "Too many base directories\n", "a", "b");
+    }
+
+    /**
+     * <testcase>Test that too many base directories are reported. </testcase>
+     */
+    @Test
+    public final void testError03() {
+
+        runTestEn("", "Too many base directories\n", "a", "-", "b");
+    }
+
+    /**
+     * <testcase>Test that too many base directories are reported. </testcase>
+     */
+    @Test
+    public final void testError04() {
+
+        runTestEn("", "Too many base directories\n", "-", "a", "-", "b");
+    }
+
+    /**
+     * <testcase>Test that a missing base directory for flag - is reported.
+     * </testcase>
+     */
+    @Test
+    public final void testError05() {
+
+        runTestEn("", "Missing argument for `-'\n", "-");
+    }
+
+    /**
+     * <testcase>Test that a non-existent base directory is reported.
+     * </testcase>
+     */
+    @Test
+    public final void testError06() {
+
+        String sep = System.getProperty("file.separator");
+        runTestEn("", "The base directory `non-existent" + sep + "base" + sep
+                + "directory\' can not be read\n",
+            "non-existent/base/directory");
+    }
+
+    /**
+     * <testcase>Test that a missing base directory for flag -out is reported.
+     * </testcase>
+     */
+    @Test
+    public final void testError07() {
+
+        runTestEn("", "Missing argument for `-output'\n", "-out");
+    }
+
+    /**
+     * <testcase>Test that an unwritable output file is reported. </testcase>
+     */
+    @Test
+    public final void testError08() {
+
+        File dir = new File("target/empty");
+        dir.delete();
+        dir.mkdir();
+        runTestEn(
+            "",
+            "The output file `file/which/is/not/writable\' can not be written\n",
+            "target/empty", "-out", "file/which/is/not/writable");
+    }
+
+    /**
+     * <testcase>Test that an unwritable output file is reported. </testcase>
+     */
+    @Test
+    public final void testError09() {
+
+        File dir = new File("target/empty");
+        dir.delete();
+        dir.mkdir();
+        runTestEn(
+            "",
+            "The output file `file/which/is/not/writable\' can not be written\n",
+            "target/empty", "-output=file/which/is/not/writable");
+    }
+
+    /**
+     * <testcase>Test that an unwritable output file is reported. </testcase>
+     */
+    @Test
+    public final void testError10() {
+
+        File dir = new File("target/empty");
+        dir.delete();
+        dir.mkdir();
+        runTestEn(
+            "",
+            "The output file `file/which/is/not/writable\' can not be written\n",
+            "target/empty", "--output=file/which/is/not/writable");
+    }
+
+    /**
+     * <testcase>Test that -help works. </testcase>
+     */
+    @Test
+    public final void testHelp1() {
+
+        runTestEn("", "tocIndex [options] [base directory]\n", "-help");
+    }
+
+    /**
+     * <testcase>Test that --help works. </testcase>
+     */
+    @Test
+    public final void testHelp2() {
+
+        runTestEn("", "tocIndex [options] [base directory]\n", "--help");
+    }
+
+    /**
+     * <testcase>Test that a empty directory is correctly indexed when some
+     * directories are omitted. </testcase>
+     */
+    @Test
+    public final void testOmit1() {
+
+        runTestEn("#\n", "",
+            "src/test/resources/org/extex/framework/resource/tool/empty",
+            "-omit=.*/\\.svn$");
+    }
+
+    /**
+     * <testcase>Test that a empty directory is correctly indexed when some
+     * directories are omitted. </testcase>
+     */
+    @Test
+    public final void testOmit2() {
+
+        runTestEn("#\n" + "x.sam=/a/x.sam\n" + "y.sam=/y.sam\n", "",
+            "src/test/resources/org/extex/framework/resource/tool/toc",
+            "-omit=.*/\\.svn$");
+    }
+
+    /**
+     * <testcase>Test that an illegal regex is reported. </testcase>
+     */
+    @Test
+    public final void testOmit3() {
+
+        runTestEn("", "Illegal regular expression: \\\n",
+            "src/test/resources/org/extex/framework/resource/tool/toc",
+            "-omit=\\");
+    }
+
+    /**
+     * <testcase>Test that a empty directory is correctly indexed when some
+     * directories are omitted. </testcase>
+     */
+    @Test
+    public final void testOut1() {
+
+        runTestEn("", "",
+            "src/test/resources/org/extex/framework/resource/tool/toc",
+            "-omit=.*/\\.svn$", "--output=target/toc");
+    }
+
+    /**
+     * <testcase>Test that an empty output file redirects to stdout. </testcase>
+     */
+    @Test
+    public final void testOut2() {
+
+        runTestEn("#\n" + "x.sam=/a/x.sam\n" + "y.sam=/y.sam\n", "",
+            "src/test/resources/org/extex/framework/resource/tool/toc",
+            "-omit=.*/\\.svn$", "--output=");
+    }
+
+}
