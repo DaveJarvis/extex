4d5c4d596f9e79e7bf621995da2f5aff9e660638
Gerd Neugebauer <gene@gerd-neugebauer.de>
2008-09-14 15:08:37 +0000

    refactoring

--- ExBib/ExBib-groovy/src/test/java/org/extex/exbib/bsf/GroovyTest.java
+++ ExBib/ExBib-groovy/src/test/java/org/extex/exbib/bsf/GroovyTest.java
@@ -1,29 +1,31 @@
 /*
  * Copyright (C) 2008 The ExTeX Group and individual authors listed below
- *
- * This library is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published by the
- * Free Software Foundation; either version 2.1 of the License, or (at your
- * option) any later version.
- *
+ * 
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
  * This library is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
- * for more details.
- *
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
  * You should have received a copy of the GNU Lesser General Public License
- * along with this library; if not, write to the Free Software Foundation,
- * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- *
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
 
 package org.extex.exbib.bsf;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.io.PrintWriter;
 import java.io.Writer;
 import java.util.Properties;
@@ -61,7 +63,7 @@ public class GroovyTest {
     }
 
     /**
-     * <test> TODO gene: missing JavaDoc </test>
+     * <test> trivial test case </test>
      * 
      * @throws Exception in case of an error
      */
@@ -76,7 +78,10 @@ public class GroovyTest {
         String aux = "target/test.aux";
         makeFile(aux, "\\citation{*}\n\\bibstyle{" + gy
                 + "}\n\\bibdata{target/test.bib}\n");
+        PrintStream out = System.out;
         try {
+            ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+            System.setOut(new PrintStream(outStream));
             Properties prop = new Properties();
             prop.put(ExBib.PROP_PROCESSOR, "groovy");
             prop.put(ExBib.PROP_FILE, aux);
@@ -85,7 +90,10 @@ public class GroovyTest {
             logger.setLevel(Level.WARNING);
             exBib.setLogger(logger);
             assertTrue(exBib.run());
+            assertEquals("[@book{abc,...}]\n", outStream.toString().replaceAll(
+                "\r", ""));
         } finally {
+            System.setOut(out);
             new File(aux).delete();
             new File(aux.replaceAll(".aux$", ".bbl")).delete();
             new File(gy).delete();
