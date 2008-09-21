0d3b6eed4d15258cee018fb512a2d96d6f5a1e63
Gerd Neugebauer <gene@gerd-neugebauer.de>
2008-09-21 22:00:08 +0000

    error detection improved

---
+++ ExBib/ExBib-core/src/main/java/org/extex/exbib/core/bst/token/impl/TEmptyField.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2008 The ExTeX Group and individual authors listed below
+ * 
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+package org.extex.exbib.core.bst.token.impl;
+
+import org.extex.exbib.core.Processor;
+import org.extex.exbib.core.bst.exception.ExBibUndefinedFieldException;
+import org.extex.exbib.core.exceptions.ExBibException;
+import org.extex.exbib.core.io.Locator;
+
+/**
+ * This class represents a field which does not exist. This class throws an
+ * exception when expanded.
+ * 
+ * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
+ * @version $Revision$
+ */
+public class TEmptyField extends TString {
+
+    /**
+     * The field <tt>entryName</tt> contains the name of the entry.
+     */
+    private String entryName;
+
+    /**
+     * Creates a new object.
+     * 
+     * @param name the name of the field
+     * @param entryName the name of the entry
+     * @param locator the locator
+     */
+    public TEmptyField(String name, String entryName, Locator locator) {
+
+        super(name, locator);
+        this.entryName = entryName;
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see org.extex.exbib.core.bst.token.AbstractToken#expand(org.extex.exbib.core.Processor)
+     */
+    @Override
+    public String expand(Processor processor) throws ExBibException {
+
+        throw new ExBibUndefinedFieldException(getValue(), entryName,
+            getLocator());
+    }
+
+}
--- ExBib/ExBib-core/src/main/java/org/extex/exbib/core/bst/token/impl/TField.java
+++ ExBib/ExBib-core/src/main/java/org/extex/exbib/core/bst/token/impl/TField.java
@@ -1,20 +1,19 @@
 /*
  * Copyright (C) 2003-2008 The ExTeX Group and individual authors listed below
- *
- * This library is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published by the
- * Free Software Foundation; either version 2.1 of the License, or (at your
- * option) any later version.
- *
+ * 
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
  * This library is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
- * for more details.
- *
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
  * You should have received a copy of the GNU Lesser General Public License
- * along with this library; if not, write to the Free Software Foundation,
- * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- *
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
 
 package org.extex.exbib.core.bst.token.impl;
@@ -53,7 +52,7 @@ public class TField extends AbstractToken {
     /**
      * {@inheritDoc}
      * 
-     * @see org.extex.exbib.core.bst.token.Token#execute( BstProcessor,
+     * @see org.extex.exbib.core.bst.token.Token#execute(BstProcessor,
      *      org.extex.exbib.core.db.Entry, org.extex.exbib.core.io.Locator)
      */
     public void execute(BstProcessor processor, Entry entry, Locator locator)
@@ -66,8 +65,11 @@ public class TField extends AbstractToken {
         String field = getValue();
         String result = entry.getExpandedValue(field, processor.getDB());
 
-        // TODO: eliminate brain-dead compatibility code
-        if (field.equals("crossref") && result != null) {
+        if (result == null) {
+            processor.push(new TEmptyField(field, entry.getKey(), locator));
+            return;
+        } else if (field.equals("crossref")) {
+            // TODO: eliminate brain-dead compatibility code
             result = result.toLowerCase(Locale.ENGLISH);
         }
 
@@ -77,8 +79,7 @@ public class TField extends AbstractToken {
     /**
      * {@inheritDoc}
      * 
-     * @see org.extex.exbib.core.bst.token.Token#visit(
-     *      org.extex.exbib.core.bst.token.TokenVisitor)
+     * @see org.extex.exbib.core.bst.token.Token#visit(org.extex.exbib.core.bst.token.TokenVisitor)
      */
     public void visit(TokenVisitor visitor) throws IOException {
 
