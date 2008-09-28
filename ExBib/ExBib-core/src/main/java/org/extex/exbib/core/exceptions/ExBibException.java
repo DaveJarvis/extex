730b71ddde360b40a7ea5c6bdfb6a16d483f0c9b
Gerd Neugebauer <gene@gerd-neugebauer.de>
2008-09-28 20:15:19 +0000

    extended

--- ExBib/ExBib-core/src/main/java/org/extex/exbib/core/exceptions/ExBibException.java
+++ ExBib/ExBib-core/src/main/java/org/extex/exbib/core/exceptions/ExBibException.java
@@ -1,20 +1,19 @@
 /*
  * Copyright (C) 2003-2008 The ExTeX Group and individual authors listed below
- *
- * This library is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published by the
- * Free Software Foundation; either version 2.1 of the License, or (at your
- * option) any later version.
- *
+ * 
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
  * This library is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
- * for more details.
- *
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
  * You should have received a copy of the GNU Lesser General Public License
- * along with this library; if not, write to the Free Software Foundation,
- * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- *
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
 
 package org.extex.exbib.core.exceptions;
@@ -70,6 +69,17 @@ public class ExBibException extends Exception {
 
         super(message);
         this.locator = locator;
+    }
+
+    /**
+     * Create a new object.
+     * 
+     * @param message the message
+     * @param cause the cause
+     */
+    public ExBibException(String message, Throwable cause) {
+
+        super(message, cause);
     }
 
     /**
