0d36a6d1f8b995bfb24d30ad0a1f6884d57d18c9
Gerd Neugebauer <gene@gerd-neugebauer.de>
2008-11-16 21:15:20 +0000

    new

---
+++ ExBib/Bst2Groovy/src/test/java/org/extex/exbib/bst2groovy/io/CodeWriterTest.java
@@ -0,0 +1,119 @@
+/*
+ * Copyright (C) 2008 The ExTeX Group and individual authors listed below
+ * 
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+package org.extex.exbib.bst2groovy.io;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.io.StringWriter;
+
+import org.junit.Test;
+
+/**
+ * This is a test suite for the code writer.
+ * 
+ * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
+ * @version $Revision$
+ */
+public class CodeWriterTest {
+
+    /**
+     * TODO gene: missing JavaDoc
+     * 
+     * @param in
+     * @param expected
+     * 
+     * @throws IOException in case of an I/O error
+     */
+    private void run(String in, String expected) throws IOException {
+
+        StringWriter w = new StringWriter();
+        CodeWriter cw = new CodeWriter(w);
+        cw.write(in);
+        cw.close();
+        assertEquals(expected, w.toString());
+    }
+
+    /**
+     * TODO gene: missing JavaDoc
+     * 
+     * @throws IOException in case of an I/O error
+     */
+    @Test
+    public void test01() throws IOException {
+
+        run("", "");
+    }
+
+    /**
+     * TODO gene: missing JavaDoc
+     * 
+     * @throws IOException in case of an I/O error
+     */
+    @Test
+    public void test02() throws IOException {
+
+        run("abc", "abc");
+    }
+
+    /**
+     * TODO gene: missing JavaDoc
+     * 
+     * @throws IOException in case of an I/O error
+     */
+    @Test
+    public void test03() throws IOException {
+
+        run("ab\ncd", "ab\ncd");
+    }
+
+    /**
+     * TODO gene: missing JavaDoc
+     * 
+     * @throws IOException in case of an I/O error
+     */
+    @Test
+    public void test04() throws IOException {
+
+        run("ab\n\ncd", "ab\n\ncd");
+    }
+
+    /**
+     * TODO gene: missing JavaDoc
+     * 
+     * @throws IOException in case of an I/O error
+     */
+    @Test
+    public void test05() throws IOException {
+
+        run("\ncd", "cd");
+    }
+
+    /**
+     * TODO gene: missing JavaDoc
+     * 
+     * @throws IOException in case of an I/O error
+     */
+    @Test
+    public void test06() throws IOException {
+
+        run("ab\n\n\ncd", "ab\n\ncd");
+    }
+
+}
