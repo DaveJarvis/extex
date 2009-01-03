8827c48b5e41c03c2dff23abd15db4ff01b9ffb6
Gerd Neugebauer <gene@gerd-neugebauer.de>
2009-01-03 00:54:37 +0000

    minor documentation fixes

--- ExBib/ExBib-core/src/main/java/org/extex/exbib/core/io/Locator.java
+++ ExBib/ExBib-core/src/main/java/org/extex/exbib/core/io/Locator.java
@@ -1,20 +1,19 @@
 /*
- * Copyright (C) 2003-2008 The ExTeX Group and individual authors listed below
- *
- * This library is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published by the
- * Free Software Foundation; either version 2.1 of the License, or (at your
- * option) any later version.
- *
+ * Copyright (C) 2003-2009 The ExTeX Group and individual authors listed below
+ * 
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
  * This library is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
- * for more details.
- *
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
  * You should have received a copy of the GNU Lesser General Public License
- * along with this library; if not, write to the Free Software Foundation,
- * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- *
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
 
 package org.extex.exbib.core.io;
@@ -39,8 +38,7 @@ public class Locator {
     private String resource;
 
     /**
-     * The field <tt>line</tt> contains the number of the line in the
-     * resource.
+     * The field <tt>line</tt> contains the number of the line in the resource.
      */
     private int line;
 
@@ -77,7 +75,7 @@ public class Locator {
     }
 
     /**
-     * Getter for the line number
+     * Getter for the line number.
      * 
      * @return the line number
      */
@@ -97,7 +95,7 @@ public class Locator {
     }
 
     /**
-     * Getter for the resource name
+     * Getter for the resource name.
      * 
      * @return the resource name
      */
