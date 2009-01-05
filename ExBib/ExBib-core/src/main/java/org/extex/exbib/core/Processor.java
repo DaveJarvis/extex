fa1192f5ac1194006ab9245a3e9c699b3d946463
Gerd Neugebauer <gene@gerd-neugebauer.de>
2009-01-05 11:10:04 +0000

    typo fixed

--- ExBib/ExBib-core/src/main/java/org/extex/exbib/core/Processor.java
+++ ExBib/ExBib-core/src/main/java/org/extex/exbib/core/Processor.java
@@ -1,20 +1,19 @@
 /*
  * Copyright (C) 2003-2008 The ExTeX Group and individual authors listed below
- *
- * This library is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published by the
- * Free Software Foundation; either version 2.1 of the License, or (at your
- * option) any later version.
- *
+ * 
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
  * This library is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
- * for more details.
- *
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
  * You should have received a copy of the GNU Lesser General Public License
- * along with this library; if not, write to the Free Software Foundation,
- * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- *
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
 
 package org.extex.exbib.core;
@@ -89,13 +88,13 @@ public interface Processor extends Bibliography, Configurable {
      * 
      * @param name the name of the macro to search for
      * 
-     * @return the expanded value of the macro or <code>null</code> if none
-     *         has been found.
+     * @return the expanded value of the macro or <code>null</code> if none has
+     *         been found.
      */
     String getMacro(String name);
 
     /**
-     * Getter for the list of all defined macro names
+     * Getter for the list of all defined macro names.
      * 
      * @return the list of macro names
      */
