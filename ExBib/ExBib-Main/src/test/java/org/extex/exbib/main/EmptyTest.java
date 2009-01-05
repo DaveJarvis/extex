9c8a4e2c560db27cdd9df0434a79b9b0d00a95e7
Gerd Neugebauer <gene@gerd-neugebauer.de>
2009-01-05 15:24:49 +0000

    naming fixed

--- ExBib/ExBib-Main/src/test/java/org/extex/exbib/main/EmptyTest.java
+++ ExBib/ExBib-Main/src/test/java/org/extex/exbib/main/EmptyTest.java
@@ -1,20 +1,19 @@
 /*
- * Copyright (C) 2003-2008 The ExTeX Group and individual authors listed below
- *
- * This library is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published by the
- * Free Software Foundation; either version 2.1 of the License, or (at your
- * option) any later version.
- *
+ * Copyright (C) 2003-2009 The ExTeX Group and individual authors listed below
+ * 
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
  * This library is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
- * for more details.
- *
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
  * You should have received a copy of the GNU Lesser General Public License
- * along with this library; if not, write to the Free Software Foundation,
- * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- *
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
 
 package org.extex.exbib.main;
@@ -30,21 +29,13 @@ import org.junit.Test;
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
  * @version $Revision$
  */
-public class TestEmpty {
+public class EmptyTest {
 
     /**
      * The field <tt>DATA_DIR</tt> contains the directory containing database,
      * styles and results.
      */
     private static final String DATA_DIR = "src/test/resources/bibtex/empty";
-
-    /**
-     * Create a new object.
-     */
-    public TestEmpty() {
-
-        super();
-    }
 
     /**
      * Run a test case.
