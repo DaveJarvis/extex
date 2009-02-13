05ac33c24ded443fb949fdfeed5ccee50f551343
Gerd Neugebauer <gene@gerd-neugebauer.de>
2009-02-13 22:53:12 +0000

    documentation improved

--- tools/LaTeX-mojo/src/main/java/org/extex/maven/latex/make/exception/MakeException.java
+++ tools/LaTeX-mojo/src/main/java/org/extex/maven/latex/make/exception/MakeException.java
@@ -1,26 +1,25 @@
 /*
- * Copyright (C) 2008 The ExTeX Group and individual authors listed below
- *
- * This library is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published by the
- * Free Software Foundation; either version 2.1 of the License, or (at your
- * option) any later version.
- *
+ * Copyright (C) 2008-2009 The ExTeX Group and individual authors listed below
+ * 
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
  * This library is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
- * for more details.
- *
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
  * You should have received a copy of the GNU Lesser General Public License
- * along with this library; if not, write to the Free Software Foundation,
- * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- *
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
 
 package org.extex.maven.latex.make.exception;
 
 /**
- * TODO gene: missing JavaDoc.
+ * This is a base exception for the make.
  * 
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
  * @version $Revision$
@@ -45,7 +44,7 @@ public class MakeException extends Exception {
     /**
      * Creates a new object.
      * 
-     * @param message
+     * @param message the message
      */
     public MakeException(String message) {
 
@@ -55,8 +54,8 @@ public class MakeException extends Exception {
     /**
      * Creates a new object.
      * 
-     * @param message
-     * @param cause
+     * @param message the message
+     * @param cause the cause
      */
     public MakeException(String message, Throwable cause) {
 
@@ -66,7 +65,7 @@ public class MakeException extends Exception {
     /**
      * Creates a new object.
      * 
-     * @param cause
+     * @param cause the cause
      */
     public MakeException(Throwable cause) {
 
