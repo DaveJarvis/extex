0d0365ff9594b48ec1c7cce299717d7040a23dfc
Gerd Neugebauer <gene@gerd-neugebauer.de>
2009-08-23 21:19:23 +0000

    update

--- ExIndex/ExIndex-makeindex/src/test/java/org/extex/exindex/makeindex/main/MakeindexExBibTest.java
+++ ExIndex/ExIndex-makeindex/src/test/java/org/extex/exindex/makeindex/main/MakeindexExBibTest.java
@@ -1,16 +1,16 @@
 /*
  * Copyright (C) 2009 The ExTeX Group and individual authors listed below
- * 
+ *
  * This library is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 2.1 of the License, or (at your option)
  * any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this library; if not, write to the Free Software Foundation, Inc.,
  * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
@@ -32,15 +32,15 @@ import org.junit.Test;
 
 /**
  * This is a real life test suite for {@link Makeindex}.
- * 
+ *
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
- * @version $Revision: 7856 $
+ * @version $Revision$
  */
 public class MakeindexExBibTest extends AbstractTester {
 
     /**
      * {@inheritDoc}
-     * 
+     *
      * @see org.extex.exindex.makeindex.main.AbstractTester#makeInstance()
      */
     @Override
@@ -51,7 +51,7 @@ public class MakeindexExBibTest extends AbstractTester {
 
     /**
      * <testcase> An index can be sorted. </testcase>
-     * 
+     *
      * @throws IOException in case of an error
      */
     @Test
@@ -70,7 +70,7 @@ public class MakeindexExBibTest extends AbstractTester {
                         + "Scanning input file\n"
                         + "src/test/resources/makeindex/exbib-manual.idx...done (714 entries accepted, 0\n"
                         + "rejected)\n"
-                        + "Sorting...done (6681 comparisons).\n"
+                        + "Sorting...done (6672 comparisons).\n"
                         + "Generating output file target/exbib-manual.ind...done (0 entries written, 0\n"
                         + "warnings).\n"
                         + "Output written in target/exbib-manual.ind.\n"
