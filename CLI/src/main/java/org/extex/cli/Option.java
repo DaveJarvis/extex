b8c029d3b6512f21b49d0b56a52cefb26a4eaeb0
Gerd Neugebauer <gene@gerd-neugebauer.de>
2010-10-31 08:56:07 +0000

    simplified

--- CLI/src/main/java/org/extex/cli/Option.java
+++ CLI/src/main/java/org/extex/cli/Option.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2008 The ExTeX Group and individual authors listed below
+ * Copyright (C) 2008-2010 The ExTeX Group and individual authors listed below
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
@@ -47,7 +47,6 @@ public abstract class Option {
      */
     public Option(String tag) {
 
-        super();
         this.tag = tag;
     }
 
