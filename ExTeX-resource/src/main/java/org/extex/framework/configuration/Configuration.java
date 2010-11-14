00510c1afe6c1483df53634407e09e3118a9944c
Gerd Neugebauer <gene@gerd-neugebauer.de>
2010-11-14 22:27:19 +0000

    minor improvement

--- ExTeX-resource/src/main/java/org/extex/framework/configuration/Configuration.java
+++ ExTeX-resource/src/main/java/org/extex/framework/configuration/Configuration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2003-2007 The ExTeX Group and individual authors listed below
+ * Copyright (C) 2003-2010 The ExTeX Group and individual authors listed below
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
@@ -31,20 +31,21 @@ import org.extex.framework.configuration.exception.ConfigurationSyntaxException;
 /**
  * This is the interface for the configuration engine.
  * <p>
- * The configuration of a piece of software is in general independent of the
- * way the configuration information is stored. Only a few methods are required
- * to describe all features needed.
+ * The configuration of a piece of software is in general independent of the way
+ * the configuration information is stored. Only a few methods are required to
+ * describe all features needed.
  * </p>
  * <p>
  * This interface is independent from the external representation of the
- * configuration information. This means that the configuration information
- * can be stored in an XML file, a properties file, a database, or even be
+ * configuration information. This means that the configuration information can
+ * be stored in an XML file, a properties file, a database, or even be
  * hard-coded in the source of an implementation.
  * </p>
  * <p>
  * For the illustration the external representation as a special form of XML is
  * used. The following example illustrates an XML configuration file.
  * </p>
+ * 
  * <pre>
  *   &lt;?xml version="1.0" encoding="ISO-8859-1"?&gt;
  *   &lt;cfg&gt;
@@ -55,7 +56,7 @@ import org.extex.framework.configuration.exception.ConfigurationSyntaxException;
  *     . . .
  *   &lt;/cfg&gt;
  * </pre>
- *
+ * 
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
  * @version $Revision$
  */
@@ -64,9 +65,10 @@ public interface Configuration extends Iterable<Configuration> {
     /**
      * Extract a sub-configuration with a given name.
      * <p>
-     * Consider the following example with the configuration currently rooted
-     * at cfg:
+     * Consider the following example with the configuration currently rooted at
+     * cfg:
      * </p>
+     * 
      * <pre>
      *   &lt;cfg&gt;
      *     . . .
@@ -87,19 +89,19 @@ public interface Configuration extends Iterable<Configuration> {
      * If there are no tags with the given name then <code>null</code> is
      * returned.
      * </p>
-     *
+     * 
      * @param key the tag name of the sub-configuration
-     *
+     * 
      * @return the sub-configuration or <code>null</code> if none is found
-     *
+     * 
      * @throws ConfigurationInvalidResourceException in case that the given
-     *  resource name is <code>null</code> or empty
+     *         resource name is <code>null</code> or empty
      * @throws ConfigurationNotFoundException in case that the named path does
-     *  not lead to a resource
+     *         not lead to a resource
      * @throws ConfigurationSyntaxException in case that the resource contains
-     *  syntax errors
-     * @throws ConfigurationIOException in case of an IO exception while
-     *  reading the resource
+     *         syntax errors
+     * @throws ConfigurationIOException in case of an IO exception while reading
+     *         the resource
      */
     Configuration findConfiguration(String key)
             throws ConfigurationInvalidResourceException,
@@ -110,9 +112,10 @@ public interface Configuration extends Iterable<Configuration> {
     /**
      * Extract a sub-configuration with a given name and a given attribute.
      * <p>
-     * Consider the following example with the configuration currently rooted
-     * at cfg:
+     * Consider the following example with the configuration currently rooted at
+     * cfg:
      * </p>
+     * 
      * <pre>
      *   &lt;cfg&gt;
      *     . . .
@@ -137,12 +140,12 @@ public interface Configuration extends Iterable<Configuration> {
      * If there are no tags with the given name then <code>null</code> is
      * returned.
      * </p>
-     *
+     * 
      * @param key the tag name of the sub-configuration
      * @param attribute the value of the attribute name
-     *
+     * 
      * @return the sub-configuration
-     *
+     * 
      * @throws ConfigurationException in case of other errors.
      */
     Configuration findConfiguration(String key, String attribute)
@@ -150,20 +153,21 @@ public interface Configuration extends Iterable<Configuration> {
 
     /**
      * Getter for an attribute with a given name.
-     *
+     * 
      * @param name the tag name of the attribute
-     *
+     * 
      * @return the value of the attribute or <code>null</code> if such an
-     * attribute is not present
+     *         attribute is not present
      */
     String getAttribute(String name);
 
     /**
      * Extract a sub-configuration with a given name.
      * <p>
-     * Consider the following example with the configuration currently rooted
-     * at cfg:
+     * Consider the following example with the configuration currently rooted at
+     * cfg:
      * </p>
+     * 
      * <pre>
      *   &lt;cfg&gt;
      *     . . .
@@ -183,24 +187,24 @@ public interface Configuration extends Iterable<Configuration> {
      * <p>
      * If there are no tags with the given name then an exception is thrown.
      * </p>
-     *
+     * 
      * @param key the tag name of the sub-configuration
-     *
+     * 
      * @return the sub-configuration
-     *
-     * @throws ConfigurationException in case of other errors. Especially
-     *  <br />
-     *  ConfigurationNotFoundException in case that the given name does
-     * not correspond to one of the tags in the current configuration
+     * 
+     * @throws ConfigurationException in case of other errors. Especially <br />
+     *         ConfigurationNotFoundException in case that the given name does
+     *         not correspond to one of the tags in the current configuration
      */
     Configuration getConfiguration(String key) throws ConfigurationException;
 
     /**
      * Extract a sub-configuration with a given name and a given attribute.
      * <p>
-     * Consider the following example with the configuration currently rooted
-     * at cfg:
+     * Consider the following example with the configuration currently rooted at
+     * cfg:
      * </p>
+     * 
      * <pre>
      *   &lt;cfg&gt;
      *     . . .
@@ -224,37 +228,36 @@ public interface Configuration extends Iterable<Configuration> {
      * <p>
      * If there are no tags with the given name then an exception is thrown.
      * </p>
-     *
+     * 
      * @param key the tag name of the sub-configuration
      * @param attribute the value of the attribute name
-     *
+     * 
      * @return the sub-configuration
-     *
-     * @throws ConfigurationException in case of other errors. Especially
-     * <br/>
-     * ConfigurationNotFoundException in case that the given name does
-     * not correspond to one of the tags in the current configuration
+     * 
+     * @throws ConfigurationException in case of other errors. Especially <br/>
+     *         ConfigurationNotFoundException in case that the given name does
+     *         not correspond to one of the tags in the current configuration
      */
     Configuration getConfiguration(String key, String attribute)
             throws ConfigurationException;
 
     /**
      * Getter for the textual value of this configuration.
-     *
+     * 
      * @return the text stored directly in this configuration
-     *
+     * 
      * @throws ConfigurationException in case that something went wrong
      */
     String getValue() throws ConfigurationException;
 
     /**
-     * Retrieve a value from the configuration as <i>String</i>.
-     * If the value could not be determined then the empty string is
-     * returned.
+     * Retrieve a value from the configuration as <i>String</i>. If the value
+     * could not be determined then the empty string is returned.
      * <p>
-     * Consider the following example with the configuration currently rooted
-     * at cfg:
+     * Consider the following example with the configuration currently rooted at
+     * cfg:
      * </p>
+     * 
      * <pre>
      *   &lt;cfg&gt;
      *     . . .
@@ -266,24 +269,24 @@ public interface Configuration extends Iterable<Configuration> {
      * <p>
      * Then <tt>getValue("two")</tt> returns the String "the second value".
      * </p>
-     *
+     * 
      * @param key the name of the desired value
-     *
+     * 
      * @return the value of key or <code>null</code>
-     *
+     * 
      * @throws ConfigurationException in case that something went wrong
      */
     String getValue(String key) throws ConfigurationException;
 
     /**
-     * Retrieve a value from the configuration as <i>int</i>. If the value
-     * could not be determined then a given default value is returned.
-     *
+     * Retrieve a value from the configuration as <i>int</i>. If the value could
+     * not be determined then a given default value is returned.
+     * 
      * @param key the name of the desired value
      * @param defaultValue the default value
-     *
+     * 
      * @return the value of key or the default value
-     *
+     * 
      * @throws ConfigurationException in case that something went wrong
      */
     int getValueAsInteger(String key, int defaultValue)
@@ -291,49 +294,50 @@ public interface Configuration extends Iterable<Configuration> {
 
     /**
      * Get the list of all values with the given tag name in the current
+     * configuration and append them to a given String List.
+     * 
+     * @param key the name of the tags. A value of <code>null</code> is legal. I
+     *        this case nothing is appended.
+     * @param list the list to append the values to
+     * 
+     * @throws IllegalArgumentException in case that list is <code>null</code>
+     */
+    void getValues(List<String> list, String key)
+            throws IllegalArgumentException;
+
+    /**
+     * Get the list of all values with the given tag name in the current
      * configuration.
-     *
+     * 
      * @param key the name of the tags
-     *
+     * 
      * @return the list of values
      */
     List<String> getValues(String key);
 
     /**
-     * Get the list of all values with the given tag name in the current
-     * configuration and append them to a given String List.
-     *
-     * @param key the name of the tags. A value of <code>null</code> is legal.
-     *  I this case nothing is appended.
-     * @param list the list to append the values to
-     *
-     * @throws IllegalArgumentException in case that list is null
-     */
-    void getValues(List<String> list, String key);
-
-    /**
      * Get an iterator for all sub-configurations.
-     *
+     * 
      * @return an iterator for all sub-configurations
-     *
+     * 
      * @throws ConfigurationException in case that something went wrong
      */
     Iterator<Configuration> iterator() throws ConfigurationException;
 
     /**
      * Retrieve an iterator over all items of a sub-configuration.
-     *
+     * 
      * @param key the name of the sub-configuration
-     *
+     * 
      * @return the iterator
-     *
+     * 
      * @throws ConfigurationException in case that something went wrong
      */
     Iterator<Configuration> iterator(String key) throws ConfigurationException;
 
     /**
      * Sets a loader for new external sources.
-     *
+     * 
      * @param loader the loader
      */
     void setConfigurationLoader(ConfigurationLoader loader);
