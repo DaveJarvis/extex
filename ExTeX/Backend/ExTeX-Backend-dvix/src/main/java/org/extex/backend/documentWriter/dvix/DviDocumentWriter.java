46bcc07cc8a852e6dc2888c729297d081cb034d0
Gerd Neugebauer <gene@gerd-neugebauer.de>
2011-02-15 19:50:57 +0000

    simplified

--- ExTeX/Backend/ExTeX-Backend-dvix/src/main/java/org/extex/backend/documentWriter/dvix/DviDocumentWriter.java
+++ ExTeX/Backend/ExTeX-Backend-dvix/src/main/java/org/extex/backend/documentWriter/dvix/DviDocumentWriter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2006-2007 The ExTeX Group and individual authors listed below
+ * Copyright (C) 2006-2011 The ExTeX Group and individual authors listed below
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
@@ -647,7 +647,6 @@ public class DviDocumentWriter
      */
     public DviDocumentWriter(DocumentWriterOptions options) {
 
-        super();
         this.options = options;
     }
 
--- ExTeX/Backend/ExTeX-Backend-dvix/src/main/java/org/extex/dviware/Dvi.java
+++ ExTeX/Backend/ExTeX-Backend-dvix/src/main/java/org/extex/dviware/Dvi.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2006-2007 The ExTeX Group and individual authors listed below
+ * Copyright (C) 2006-2011 The ExTeX Group and individual authors listed below
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
@@ -334,7 +334,6 @@ public class Dvi {
      */
     public Dvi(InputStream dvi) {
 
-        super();
         this.dvi = dvi;
     }
 
