eda7b2dcc9befdcb7966d15333b41b1a5f0ef919
Gerd Neugebauer <gene@gerd-neugebauer.de>
2011-02-15 21:22:26 +0000

    simplified

--- ExTeX/Scanner/ExTeX-Scanner32/src/main/java/org/extex/scanner/stream/impl32/TokenStreamImpl.java
+++ ExTeX/Scanner/ExTeX-Scanner32/src/main/java/org/extex/scanner/stream/impl32/TokenStreamImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2003-2006 The ExTeX Group and individual authors listed below
+ * Copyright (C) 2003-2011 The ExTeX Group and individual authors listed below
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
@@ -70,7 +70,6 @@ public class TokenStreamImpl extends TokenStreamBaseImpl implements TokenStream 
          */
         public State(String name) {
 
-            super();
             this.name = name;
         }
 
