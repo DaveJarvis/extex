19c30fd75698307dc23077cb928468dd56af48e0
Gerd Neugebauer <gene@gerd-neugebauer.de>
2011-02-15 19:46:39 +0000

    simplified

--- ExIndex/ExIndex-Main/src/main/java/org/extex/exindex/main/ExIndex.java
+++ ExIndex/ExIndex-Main/src/main/java/org/extex/exindex/main/ExIndex.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2007-2008 The ExTeX Group and individual authors listed below
+ * Copyright (C) 2007-2011 The ExTeX Group and individual authors listed below
  * 
  * This library is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
@@ -398,7 +398,6 @@ public class ExIndex extends Indexer {
                 IllegalAccessException,
                 InvocationTargetException {
 
-        super();
         logger = Logger.getLogger(ExIndex.class.getName());
         logger.setUseParentHandlers(false);
         logger.setLevel(Level.INFO);
