73f336a64fca7358c84723a7c8bd27b0620e96f1
Gerd Neugebauer <gene@gerd-neugebauer.de>
2011-02-15 21:43:47 +0000

    simplified

--- ExTeX/Unit/ExTeX-Unit-etex/src/test/java/org/extex/unit/etex/font/FontcharicTest.java
+++ ExTeX/Unit/ExTeX-Unit-etex/src/test/java/org/extex/unit/etex/font/FontcharicTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2005-2007 The ExTeX Group and individual authors listed below
+ * Copyright (C) 2005-2011 The ExTeX Group and individual authors listed below
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
@@ -35,7 +35,6 @@ public class FontcharicTest extends ExTeXLauncher {
      */
     public FontcharicTest() {
 
-        super();
         setConfig("etex-test");
     }
 
