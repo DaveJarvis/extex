1c72e0d57246ef0daeeb9a4e33c8101535cb85db
Gerd Neugebauer <gene@gerd-neugebauer.de>
2011-02-15 21:38:05 +0000

    simplified

--- ExTeX/Typesetter/ExTeX-Typesetter-core/src/main/java/org/extex/typesetter/core/paragraphBuilder/trivial/TrivialBuilder.java
+++ ExTeX/Typesetter/ExTeX-Typesetter-core/src/main/java/org/extex/typesetter/core/paragraphBuilder/trivial/TrivialBuilder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004-2007 The ExTeX Group and individual authors listed below
+ * Copyright (C) 2004-2011 The ExTeX Group and individual authors listed below
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
@@ -125,8 +125,6 @@ public class TrivialBuilder implements ParagraphBuilder, LogEnabled {
      * Creates a new object.
      */
     public TrivialBuilder() {
-
-        super();
 
         if (DEVELOP) {
             tracer = Logger.getLogger(ParagraphBuilder.class.getName());
