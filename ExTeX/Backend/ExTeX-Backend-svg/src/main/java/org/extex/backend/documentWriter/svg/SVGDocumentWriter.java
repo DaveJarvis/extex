fa6f1e969f09754671ff0798905fb71ef2441f22
Gerd Neugebauer <gene@gerd-neugebauer.de>
2011-02-15 19:58:33 +0000

    simplified

--- ExTeX/Backend/ExTeX-Backend-svg/src/main/java/org/extex/backend/documentWriter/svg/SVGDocumentWriter.java
+++ ExTeX/Backend/ExTeX-Backend-svg/src/main/java/org/extex/backend/documentWriter/svg/SVGDocumentWriter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2003-2007 The ExTeX Group and individual authors listed below
+ * Copyright (C) 2003-2011 The ExTeX Group and individual authors listed below
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
@@ -142,7 +142,6 @@ public class SVGDocumentWriter
      */
     public SVGDocumentWriter(Configuration cfg, DocumentWriterOptions options) {
 
-        super();
         docoptions = options;
         FORMAT.setGroupingUsed(false);
         FORMAT.setMaximumFractionDigits(2);
