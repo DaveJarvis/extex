d512c8350050419ef4865c2eefb170fa57075b05
Gerd Neugebauer <gene@gerd-neugebauer.de>
2011-05-22 16:02:08 +0000

    making the test running under Maven again

--- ExTeX/Unit/ExTeX-Unit-native/src/test/java/org/extex/unit/dynamic/java/JavadefTest.java
+++ ExTeX/Unit/ExTeX-Unit-native/src/test/java/org/extex/unit/dynamic/java/JavadefTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2005-2007 The ExTeX Group and individual authors listed below
+ * Copyright (C) 2005-2011 The ExTeX Group and individual authors listed below
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
@@ -19,7 +19,6 @@
 
 package org.extex.unit.dynamic.java;
 
-import org.extex.scanner.type.token.CodeToken;
 import org.extex.test.NoFlagsButGlobalPrimitiveTester;
 import org.junit.Test;
 
@@ -38,97 +37,6 @@ public class JavadefTest extends NoFlagsButGlobalPrimitiveTester {
 
         super("javadef", "\\t{org.extex.unit.base.Relax}");
         setConfig("native-test");
-    }
-
-    /**
-     * Creates a new object. This constructor is needed for a test case.
-     * 
-     * @param arg the (ignored) argument
-     */
-    public JavadefTest(CodeToken arg) {
-
-        super(null, null);
-    }
-
-    /**
-     * <testcase primitive="\javadef"> Test case checking that <tt>\javadef</tt>
-     * needs a token to assign. </testcase>
-     * 
-     * @throws Exception in case of an error
-     */
-    @Test
-    public void testError0() throws Exception {
-
-        assertFailure(
-        // --- input code ---
-            DEFINE_BRACES + "\\javadef",
-            // --- log message ---
-            "Missing control sequence inserted");
-    }
-
-    /**
-     * <testcase primitive="\javadef"> Test case checking that <tt>\javadef</tt>
-     * needs a token to assign. </testcase>
-     * 
-     * @throws Exception in case of an error
-     */
-    @Test
-    public void testError1() throws Exception {
-
-        assertFailure(
-        // --- input code ---
-            DEFINE_BRACES + "\\javadef a",
-            // --- log message ---
-            "Missing control sequence inserted");
-    }
-
-    /**
-     * <testcase primitive="\javadef"> Test case checking that <tt>\javadef</tt>
-     * needs a token to assign. </testcase>
-     * 
-     * @throws Exception in case of an error
-     */
-    @Test
-    public void testError2() throws Exception {
-
-        assertFailure(
-        // --- input code ---
-            DEFINE_BRACES + "\\javadef 2",
-            // --- log message ---
-            "Missing control sequence inserted");
-    }
-
-    /**
-     * <testcase primitive="\javadef"> Test case checking that <tt>\javadef</tt>
-     * needs a token to assign. </testcase>
-     * 
-     * @throws Exception in case of an error
-     */
-    @Test
-    public void testError3() throws Exception {
-
-        assertFailure(
-        // --- input code ---
-            DEFINE_BRACES + "\\javadef \\x",
-            // --- log message ---
-            "File ended while scanning text of \\javadef");
-    }
-
-    /**
-     * <testcase primitive="\javadef"> Test case checking that <tt>\javadef</tt>
-     * needs a Java class of type Code. </testcase>
-     * 
-     * @throws Exception in case of an error
-     */
-    @Test
-    public void testError10() throws Exception {
-
-        assertFailure(
-        // --- input code ---
-            DEFINE_BRACES + "\\javadef\\t{" + getClass().getName() + "}",
-            // --- log message ---
-            "The class " + getClass().getName() + " does not implement the\n"
-                    + "required interface org.extex.interpreter.type.Code.");
     }
 
     /**
@@ -164,6 +72,155 @@ public class JavadefTest extends NoFlagsButGlobalPrimitiveTester {
                     + " \\end",
             // --- log message ---
             "0" + TERM);
+    }
+
+    /**
+     * <testcase primitive="\javadef"> Test case checking that <tt>\javadef</tt>
+     * respects \afterassignment. </testcase>
+     * 
+     * @throws Exception in case of an error
+     */
+    @Test
+    public void testAfterassignment1() throws Exception {
+
+        assertFailure(
+        // --- input code ---
+            DEFINE_BRACES + "\\afterassignment\\x " + "\\begingroup"
+                    + "\\javadef\\t{org.extex.unit.tex.info.The}"
+                    + "\\endgroup",
+            // --- log message ---
+            "Undefined control sequence \\x");
+    }
+
+    /**
+     * <testcase primitive="\javadef"> Test case checking that <tt>\javadef</tt>
+     * needs a token to assign. </testcase>
+     * 
+     * @throws Exception in case of an error
+     */
+    @Test
+    public void testError0() throws Exception {
+
+        assertFailure(
+        // --- input code ---
+            DEFINE_BRACES + "\\javadef",
+            // --- log message ---
+            "Missing control sequence inserted");
+    }
+
+    /**
+     * <testcase primitive="\javadef"> Test case checking that <tt>\javadef</tt>
+     * needs a token to assign. </testcase>
+     * 
+     * @throws Exception in case of an error
+     */
+    @Test
+    public void testError01() throws Exception {
+
+        assertFailure(
+        // --- input code ---
+            DEFINE_BRACES + "\\javadef a",
+            // --- log message ---
+            "Missing control sequence inserted");
+    }
+
+    /**
+     * <testcase primitive="\javadef"> Test case checking that <tt>\javadef</tt>
+     * needs a token to assign. </testcase>
+     * 
+     * @throws Exception in case of an error
+     */
+    @Test
+    public void testError02() throws Exception {
+
+        assertFailure(
+        // --- input code ---
+            DEFINE_BRACES + "\\javadef 2",
+            // --- log message ---
+            "Missing control sequence inserted");
+    }
+
+    /**
+     * <testcase primitive="\javadef"> Test case checking that <tt>\javadef</tt>
+     * needs a token to assign. </testcase>
+     * 
+     * @throws Exception in case of an error
+     */
+    @Test
+    public void testError03() throws Exception {
+
+        assertFailure(
+        // --- input code ---
+            DEFINE_BRACES + "\\javadef \\x",
+            // --- log message ---
+            "File ended while scanning text of \\javadef");
+    }
+
+    /**
+     * <testcase primitive="\javadef"> Test case checking that <tt>\javadef</tt>
+     * needs a non-empty class name. </testcase>
+     * 
+     * @throws Exception in case of an error
+     */
+    @Test
+    public void testError04() throws Exception {
+
+        assertFailure(
+        // --- input code ---
+            DEFINE_BRACES + "\\javadef \\x {}",
+            // --- log message ---
+            "The string `' is no valid class name");
+    }
+
+    /**
+     * <testcase primitive="\javadef"> Test case checking that <tt>\javadef</tt>
+     * needs a proper class name. </testcase>
+     * 
+     * @throws Exception in case of an error
+     */
+    @Test
+    public void testError05() throws Exception {
+
+        assertFailure(
+        // --- input code ---
+            DEFINE_BRACES + "\\javadef \\x {a-b-c}",
+            // --- log message ---
+            "The string `a-b-c' is no valid class name");
+    }
+
+    /**
+     * <testcase primitive="\javadef"> Test case checking that <tt>\javadef</tt>
+     * needs a Java class With a proper constructor. </testcase>
+     * 
+     * @throws Exception in case of an error
+     */
+    @Test
+    public void testError10() throws Exception {
+
+        assertFailure(
+        // --- input code ---
+            DEFINE_BRACES + "\\javadef\\t{" + String.class.getName() + "}",
+            // --- log message ---
+            "Method not found:\njava.lang.String.<init>(org.extex.scanner.type.token.CodeToken)");
+    }
+
+    /**
+     * <testcase primitive="\javadef"> Test case checking that <tt>\javadef</tt>
+     * needs a Java class of type Code. </testcase>
+     * 
+     * @throws Exception in case of an error
+     */
+    @Test
+    public void testError11() throws Exception {
+
+        assertFailure(
+        // --- input code ---
+            DEFINE_BRACES + "\\javadef\\t{" + JavadefTestClass1.class.getName()
+                    + "}",
+            // --- log message ---
+            "The class " + JavadefTestClass1.class.getName()
+                    + " does not implement the\n"
+                    + "required interface org.extex.interpreter.type.Code.");
     }
 
     /**
@@ -218,24 +275,6 @@ public class JavadefTest extends NoFlagsButGlobalPrimitiveTester {
                     + "\\endgroup" + "\\t\\count42" + " \\end",
             // --- log message ---
             "0" + TERM);
-    }
-
-    /**
-     * <testcase primitive="\javadef"> Test case checking that <tt>\javadef</tt>
-     * respects \afterassignment. </testcase>
-     * 
-     * @throws Exception in case of an error
-     */
-    @Test
-    public void testAfterassignment1() throws Exception {
-
-        assertFailure(
-        // --- input code ---
-            DEFINE_BRACES + "\\afterassignment\\x " + "\\begingroup"
-                    + "\\javadef\\t{org.extex.unit.tex.info.The}"
-                    + "\\endgroup",
-            // --- log message ---
-            "Undefined control sequence \\x");
     }
 
 }
