5c6572c1d049c3710a248e8b2c49bc7d69e6f75a
Gerd Neugebauer <gene@gerd-neugebauer.de>
2011-05-25 16:34:10 +0000

    test case disabled

--- ExTeX/Unit/ExTeX-Unit-tex/src/test/java/org/extex/unit/tex/table/HalignTest.java
+++ ExTeX/Unit/ExTeX-Unit-tex/src/test/java/org/extex/unit/tex/table/HalignTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004-2007 The ExTeX Group and individual authors listed below
+ * Copyright (C) 2004-2011 The ExTeX Group and individual authors listed below
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
@@ -22,11 +22,12 @@ package org.extex.unit.tex.table;
 import java.util.Properties;
 
 import org.extex.test.NoFlagsPrimitiveTester;
+import org.junit.Ignore;
 import org.junit.Test;
 
 /**
  * This is a test suite for the primitive <tt>\halign</tt>.
- *
+ * 
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
  * @version $Revision: 4808 $
  */
@@ -42,155 +43,138 @@ public class HalignTest extends NoFlagsPrimitiveTester {
 
     /**
      * Test case checking that an outer macro in the preamble leads to an error.
-     *
-     * @throws Exception in case of an error
-     */
-    @Test
-    public void testMissingBrace1() throws Exception {
-
-        assertFailure(//--- input code ---
-                DEFINE_CATCODES
-                + "\\halign a"
-                + "\\end ",
-                //--- log message ---
-                "Missing `{' inserted");
-    }
-
-    /**
-     * Test case checking that an outer macro in the preamble leads to an error.
-     *
+     * 
      * @throws Exception in case of an error
      */
     @Test
     public void testEof1() throws Exception {
 
-        assertFailure(//--- input code ---
-                DEFINE_CATCODES
-                + "\\halign",
-                //--- log message ---
-                "Unexpected end of file while processing \\halign");
+        assertFailure(// --- input code ---
+            DEFINE_CATCODES + "\\halign",
+            // --- log message ---
+            "Unexpected end of file while processing \\halign");
+    }
+
+    /**
+     * Test case checking that ...
+     * 
+     * @throws Exception in case of an error
+     */
+    @Test
+    @Ignore("gene: expected result is missing")
+    public void testHalign1() throws Exception {
+
+        Properties properties = (Properties) System.getProperties().clone();
+        properties.setProperty("extex.output", "dump");
+
+        assertSuccess(properties,
+        // --- input code ---
+            DEFINE_CATCODES + "\\halign{a#b&c#d\\cr1&2\\cr}" + "\\end ",
+            // --- output channel ---
+            "???"); // TODO gene: check
     }
 
     /**
      * Test case checking that an outer macro in the preamble leads to an error.
-     *
+     * 
      * @throws Exception in case of an error
      */
     @Test
-    public void testOuter1() throws Exception {
+    public void testMissingBrace1() throws Exception {
 
-        assertFailure(//--- input code ---
-                DEFINE_CATCODES
-                + "\\outer\\def\\x{}"
-                + ""
-                + "\\halign{\\x#&\\cr}"
-                + "\\end ",
-                //--- log message ---
-                "Forbidden control sequence found while scanning preamble of \\halign");
+        assertFailure(// --- input code ---
+            DEFINE_CATCODES + "\\halign a" + "\\end ",
+            // --- log message ---
+            "Missing `{' inserted");
     }
 
     /**
-     * Test case checking that an outer macro in the preamble leads to an error.
-     *
-     * @throws Exception in case of an error
-     */
-    @Test
-    public void testOuter2() throws Exception {
-
-        assertFailure(//--- input code ---
-                DEFINE_CATCODES
-                + "\\outer\\def\\x{}"
-                + ""
-                + "\\halign{a#\\x&\\cr}"
-                + "\\end ",
-                //--- log message ---
-                "Forbidden control sequence found while scanning preamble of \\halign");
-    }
-
-    /**
-     * Test case checking that an outer macro in the preamble leads to an error.
-     *
-     * @throws Exception in case of an error
-     */
-    @Test
-    public void testOuter3() throws Exception {
-
-        assertFailure(//--- input code ---
-                DEFINE_CATCODES
-                + "\\outer\\def\\x{}"
-                + ""
-                + "\\halign{a#b&a#\\x&\\cr}"
-                + "\\end ",
-                //--- log message ---
-                "Forbidden control sequence found while scanning preamble of \\halign");
-    }
-
-    /**
-     * Test case checking that a missing sharp in the preamble leads to an error.
-     *
+     * Test case checking that a missing sharp in the preamble leads to an
+     * error.
+     * 
      * @throws Exception in case of an error
      */
     @Test
     public void testMissingSharp1() throws Exception {
 
-        assertFailure(//--- input code ---
-                DEFINE_CATCODES
-                + "\\halign{a&#\\cr}"
-                + "\\end ",
-                //--- log message ---
-                "Missing # inserted in alignment preamble");
+        assertFailure(// --- input code ---
+            DEFINE_CATCODES + "\\halign{a&#\\cr}" + "\\end ",
+            // --- log message ---
+            "Missing # inserted in alignment preamble");
     }
 
     /**
-     * Test case checking that a missing sharp in the preamble leads to an error.
-     *
+     * Test case checking that a missing sharp in the preamble leads to an
+     * error.
+     * 
      * @throws Exception in case of an error
      */
     @Test
     public void testMissingSharp2() throws Exception {
 
-        assertFailure(//--- input code ---
-                DEFINE_CATCODES
-                + "\\halign{#a&abc\\cr}"
-                + "\\end ",
-                //--- log message ---
-                "Missing # inserted in alignment preamble");
+        assertFailure(// --- input code ---
+            DEFINE_CATCODES + "\\halign{#a&abc\\cr}" + "\\end ",
+            // --- log message ---
+            "Missing # inserted in alignment preamble");
+    }
+
+    /**
+     * Test case checking that an outer macro in the preamble leads to an error.
+     * 
+     * @throws Exception in case of an error
+     */
+    @Test
+    public void testOuter1() throws Exception {
+
+        assertFailure(// --- input code ---
+            DEFINE_CATCODES + "\\outer\\def\\x{}" + "" + "\\halign{\\x#&\\cr}"
+                    + "\\end ",
+            // --- log message ---
+            "Forbidden control sequence found while scanning preamble of \\halign");
+    }
+
+    /**
+     * Test case checking that an outer macro in the preamble leads to an error.
+     * 
+     * @throws Exception in case of an error
+     */
+    @Test
+    public void testOuter2() throws Exception {
+
+        assertFailure(// --- input code ---
+            DEFINE_CATCODES + "\\outer\\def\\x{}" + "" + "\\halign{a#\\x&\\cr}"
+                    + "\\end ",
+            // --- log message ---
+            "Forbidden control sequence found while scanning preamble of \\halign");
+    }
+
+    /**
+     * Test case checking that an outer macro in the preamble leads to an error.
+     * 
+     * @throws Exception in case of an error
+     */
+    @Test
+    public void testOuter3() throws Exception {
+
+        assertFailure(// --- input code ---
+            DEFINE_CATCODES + "\\outer\\def\\x{}" + ""
+                    + "\\halign{a#b&a#\\x&\\cr}" + "\\end ",
+            // --- log message ---
+            "Forbidden control sequence found while scanning preamble of \\halign");
     }
 
     /**
      * Test case checking that several sharps in the preamble leads to an error.
-     *
+     * 
      * @throws Exception in case of an error
      */
     @Test
     public void testSharp1() throws Exception {
 
-        assertFailure(//--- input code ---
-                DEFINE_CATCODES
-                + "\\halign{#a#\\cr}"
-                + "\\end ",
-                //--- log message ---
-                "Only one # is allowed per tab");
-    }
-
-    /**
-     * Test case checking that ...
-     *
-     * @throws Exception in case of an error
-     */
-    @Test
-    public void testHalign1() throws Exception {
-
-        Properties properties = System.getProperties();
-        properties.setProperty("extex.output", "dump");
-
-        assertSuccess(properties,
-                //--- input code ---
-                DEFINE_CATCODES
-                + "\\halign{a#b&c#d\\cr1&2\\cr}"
-                + "\\end ",
-                //--- output channel ---
-                "???"); //TODO gene: check
+        assertFailure(// --- input code ---
+            DEFINE_CATCODES + "\\halign{#a#\\cr}" + "\\end ",
+            // --- log message ---
+            "Only one # is allowed per tab");
     }
 
 }
