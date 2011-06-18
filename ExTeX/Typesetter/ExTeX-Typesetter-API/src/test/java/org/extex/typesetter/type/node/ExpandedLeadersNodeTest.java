af825141dd5f3202a563d4120c71a320277da819
Gerd Neugebauer <gene@gerd-neugebauer.de>
2011-06-18 21:48:10 +0000

    imports fixed

--- ExTeX/Typesetter/ExTeX-Typesetter-API/src/test/java/org/extex/typesetter/type/node/ExpandedLeadersNodeTest.java
+++ ExTeX/Typesetter/ExTeX-Typesetter-API/src/test/java/org/extex/typesetter/type/node/ExpandedLeadersNodeTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2007 The ExTeX Group and individual authors listed below
+ * Copyright (C) 2007-2011 The ExTeX Group and individual authors listed below
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
@@ -19,7 +19,9 @@
 
 package org.extex.typesetter.type.node;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import org.extex.core.dimen.Dimen;
 import org.extex.core.exception.GeneralException;
@@ -59,8 +61,7 @@ public class ExpandedLeadersNodeTest extends LeadersNodeTester {
     /**
      * {@inheritDoc}
      * 
-     * @see org.extex.typesetter.type.node.LeadersNodeTester#makeNode(
-     *      org.extex.typesetter.type.OrientedNode,
+     * @see org.extex.typesetter.type.node.LeadersNodeTester#makeNode(org.extex.typesetter.type.OrientedNode,
      *      org.extex.core.glue.FixedGlue)
      */
     @Override
@@ -78,22 +79,6 @@ public class ExpandedLeadersNodeTest extends LeadersNodeTester {
     public final void testAtShippingH1() throws GeneralException {
 
         AbstractLeadersNode node = makeNode(H_LIST, FixedGlue.S_S);
-        assertNotNull(node);
-        node.setWidth(Dimen.ONE_PT);
-        Node n = node.atShipping(null, null, null, null);
-        assertNotNull(n);
-        assertEquals(node, n);
-    }
-
-    /**
-     * <testcase>Not enough space left vertically </testcase>
-     * 
-     * @throws GeneralException in case of an error
-     */
-    @Test
-    public final void testAtShippingV1() throws GeneralException {
-
-        AbstractLeadersNode node = makeNode(V_LIST, FixedGlue.S_S);
         assertNotNull(node);
         node.setWidth(Dimen.ONE_PT);
         Node n = node.atShipping(null, null, null, null);
@@ -121,32 +106,6 @@ public class ExpandedLeadersNodeTest extends LeadersNodeTester {
             n.getHeight().eq(Dimen.ONE_PT));
         assertTrue("depth " + n.getDepth().toString(), //
             n.getDepth().eq(Dimen.ONE_SP));
-        NodeList nl = (NodeList) n;
-        int size = nl.size();
-        assertTrue("size " + Integer.toString(size), 1 == size);
-    }
-
-    /**
-     * <testcase>Exactly one instance fits vertically </testcase>
-     * 
-     * @throws GeneralException in case of an error
-     */
-    @Test
-    public final void testAtShippingV2() throws GeneralException {
-
-        AbstractLeadersNode node = makeNode(V_LIST, FixedGlue.S_S);
-        assertNotNull(node);
-        Dimen ht = new Dimen(Dimen.ONE_INCH.getValue() + 1);
-        node.setHeight(ht);
-        Node n = node.atShipping(null, null, null, null);
-        assertNotNull(n);
-        assertTrue(n.getClass().getName(), n instanceof VerticalListNode);
-        assertTrue("width " + n.getWidth().toString(), //
-            n.getWidth().eq(Dimen.ONE_PT));
-        assertTrue("height " + n.getHeight().toString(), //
-            n.getHeight().eq(ht));
-        assertTrue("depth " + n.getDepth().toString(), //
-            n.getDepth().eq(Dimen.ZERO_PT));
         NodeList nl = (NodeList) n;
         int size = nl.size();
         assertTrue("size " + Integer.toString(size), 1 == size);
@@ -183,6 +142,110 @@ public class ExpandedLeadersNodeTest extends LeadersNodeTester {
     }
 
     /**
+     * <testcase>Exactly two instances fits horizontally </testcase>
+     * 
+     * @throws GeneralException in case of an error
+     */
+    @Test
+    public final void testAtShippingH4() throws GeneralException {
+
+        AbstractLeadersNode node = makeNode(H_LIST, FixedGlue.S_S);
+        assertNotNull(node);
+        Dimen wd = new Dimen(Dimen.ONE_INCH.getValue() * 2);
+        node.setWidth(wd);
+        Node n = node.atShipping(null, null, null, null);
+        assertNotNull(n);
+        assertTrue(n instanceof HorizontalListNode);
+        assertTrue("width " + n.getWidth().toString(), //
+            n.getWidth().eq(wd));
+        assertTrue("height " + n.getHeight().toString(), //
+            n.getHeight().eq(Dimen.ONE_PT));
+        assertTrue("depth " + n.getDepth().toString(), //
+            n.getDepth().eq(Dimen.ONE_SP));
+        NodeList nl = (NodeList) n;
+        int size = nl.size();
+        assertTrue("size " + Integer.toString(size), 2 == size);
+        assertTrue("0:", nl.get(0) == H_LIST);
+        assertTrue("1:", nl.get(1) == H_LIST);
+    }
+
+    /**
+     * <testcase>One instance plus some glue fits horizontally </testcase>
+     * 
+     * @throws GeneralException in case of an error
+     */
+    @Test
+    public final void testAtShippingH5() throws GeneralException {
+
+        AbstractLeadersNode node = makeNode(H_LIST, FixedGlue.S_S);
+        assertNotNull(node);
+        Dimen wd = new Dimen(2 * Dimen.ONE_INCH.getValue() + 12);
+        node.setWidth(wd);
+        Node n = node.atShipping(null, null, null, null);
+        assertNotNull(n);
+        assertTrue(n instanceof HorizontalListNode);
+        NodeList nl = (NodeList) n;
+        assertTrue("width " + n.getWidth().toString(), //
+            n.getWidth().eq(wd));
+        assertTrue("height " + n.getHeight().toString(), //
+            n.getHeight().eq(Dimen.ONE_PT));
+        assertTrue("depth " + n.getDepth().toString(), //
+            n.getDepth().eq(Dimen.ONE_SP));
+        int size = nl.size();
+        assertTrue("size " + Integer.toString(size), 4 == size);
+        Node node0 = nl.get(0);
+        assertTrue("0:", node0 instanceof ImplicitKernNode);
+        assertTrue("0: width", node0.getWidth().getValue() == 4);
+        assertTrue("1:", nl.get(1) == H_LIST);
+        Node node2 = nl.get(2);
+        assertTrue("2:", node2 instanceof ImplicitKernNode);
+        assertTrue("2: width", node0.getWidth().getValue() == 4);
+        assertTrue("3:", nl.get(3) == H_LIST);
+    }
+
+    /**
+     * <testcase>Not enough space left vertically </testcase>
+     * 
+     * @throws GeneralException in case of an error
+     */
+    @Test
+    public final void testAtShippingV1() throws GeneralException {
+
+        AbstractLeadersNode node = makeNode(V_LIST, FixedGlue.S_S);
+        assertNotNull(node);
+        node.setWidth(Dimen.ONE_PT);
+        Node n = node.atShipping(null, null, null, null);
+        assertNotNull(n);
+        assertEquals(node, n);
+    }
+
+    /**
+     * <testcase>Exactly one instance fits vertically </testcase>
+     * 
+     * @throws GeneralException in case of an error
+     */
+    @Test
+    public final void testAtShippingV2() throws GeneralException {
+
+        AbstractLeadersNode node = makeNode(V_LIST, FixedGlue.S_S);
+        assertNotNull(node);
+        Dimen ht = new Dimen(Dimen.ONE_INCH.getValue() + 1);
+        node.setHeight(ht);
+        Node n = node.atShipping(null, null, null, null);
+        assertNotNull(n);
+        assertTrue(n.getClass().getName(), n instanceof VerticalListNode);
+        assertTrue("width " + n.getWidth().toString(), //
+            n.getWidth().eq(Dimen.ONE_PT));
+        assertTrue("height " + n.getHeight().toString(), //
+            n.getHeight().eq(ht));
+        assertTrue("depth " + n.getDepth().toString(), //
+            n.getDepth().eq(Dimen.ZERO_PT));
+        NodeList nl = (NodeList) n;
+        int size = nl.size();
+        assertTrue("size " + Integer.toString(size), 1 == size);
+    }
+
+    /**
      * <testcase>One instance plus some glue fits vertically </testcase>
      * 
      * @throws GeneralException in case of an error
@@ -213,34 +276,6 @@ public class ExpandedLeadersNodeTest extends LeadersNodeTester {
     }
 
     /**
-     * <testcase>Exactly two instances fits horizontally </testcase>
-     * 
-     * @throws GeneralException in case of an error
-     */
-    @Test
-    public final void testAtShippingH4() throws GeneralException {
-
-        AbstractLeadersNode node = makeNode(H_LIST, FixedGlue.S_S);
-        assertNotNull(node);
-        Dimen wd = new Dimen(Dimen.ONE_INCH.getValue()*2);
-        node.setWidth(wd);
-        Node n = node.atShipping(null, null, null, null);
-        assertNotNull(n);
-        assertTrue(n instanceof HorizontalListNode);
-        assertTrue("width " + n.getWidth().toString(), //
-            n.getWidth().eq(wd));
-        assertTrue("height " + n.getHeight().toString(), //
-            n.getHeight().eq(Dimen.ONE_PT));
-        assertTrue("depth " + n.getDepth().toString(), //
-            n.getDepth().eq(Dimen.ONE_SP));
-        NodeList nl = (NodeList) n;
-        int size = nl.size();
-        assertTrue("size " + Integer.toString(size), 2 == size);
-        assertTrue("0:", nl.get(0) == H_LIST);
-        assertTrue("1:", nl.get(1) == H_LIST);
-    }
-
-    /**
      * <testcase>Exactly one instance fits vertically </testcase>
      * 
      * @throws GeneralException in case of an error
@@ -250,7 +285,7 @@ public class ExpandedLeadersNodeTest extends LeadersNodeTester {
 
         AbstractLeadersNode node = makeNode(V_LIST, FixedGlue.S_S);
         assertNotNull(node);
-        Dimen ht = new Dimen(2*(Dimen.ONE_INCH.getValue() + 1));
+        Dimen ht = new Dimen(2 * (Dimen.ONE_INCH.getValue() + 1));
         node.setHeight(ht);
         Node n = node.atShipping(null, null, null, null);
         assertNotNull(n);
@@ -269,40 +304,6 @@ public class ExpandedLeadersNodeTest extends LeadersNodeTester {
     }
 
     /**
-     * <testcase>One instance plus some glue fits horizontally </testcase>
-     * 
-     * @throws GeneralException in case of an error
-     */
-    @Test
-    public final void testAtShippingH5() throws GeneralException {
-
-        AbstractLeadersNode node = makeNode(H_LIST, FixedGlue.S_S);
-        assertNotNull(node);
-        Dimen wd = new Dimen(2*Dimen.ONE_INCH.getValue() + 12);
-        node.setWidth(wd);
-        Node n = node.atShipping(null, null, null, null);
-        assertNotNull(n);
-        assertTrue(n instanceof HorizontalListNode);
-        NodeList nl = (NodeList) n;
-        assertTrue("width " + n.getWidth().toString(), //
-            n.getWidth().eq(wd));
-        assertTrue("height " + n.getHeight().toString(), //
-            n.getHeight().eq(Dimen.ONE_PT));
-        assertTrue("depth " + n.getDepth().toString(), //
-            n.getDepth().eq(Dimen.ONE_SP));
-        int size = nl.size();
-        assertTrue("size " + Integer.toString(size), 4 == size);
-        Node node0 = nl.get(0);
-        assertTrue("0:", node0 instanceof ImplicitKernNode);
-        assertTrue("0: width", node0.getWidth().getValue() == 4);
-        assertTrue("1:", nl.get(1) == H_LIST);
-        Node node2 = nl.get(2);
-        assertTrue("2:", node2 instanceof ImplicitKernNode);
-        assertTrue("2: width", node0.getWidth().getValue() == 4);
-        assertTrue("3:", nl.get(3) == H_LIST);
-    }
-
-    /**
      * <testcase>Two instances plus some glue fits vertically </testcase>
      * 
      * @throws GeneralException in case of an error
@@ -312,7 +313,7 @@ public class ExpandedLeadersNodeTest extends LeadersNodeTester {
 
         AbstractLeadersNode node = makeNode(V_LIST, FixedGlue.S_S);
         assertNotNull(node);
-        Dimen ht = new Dimen(2*(Dimen.ONE_INCH.getValue() + 1) + 12);
+        Dimen ht = new Dimen(2 * (Dimen.ONE_INCH.getValue() + 1) + 12);
         node.setHeight(ht);
         Node n = node.atShipping(null, null, null, null);
         assertNotNull(n);
